9ea50718f775d45d1aafc6cdf9192132ea9eb8e2
Larry Shaffer <larrys@dakotacarto.com>
2013-11-15 13:27:54 -0700

    openjpeg: for gdal 1.10 (needs work - installed header file moved)

---
+++ Formula/openjpeg.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Openjpeg < Formula
+  homepage 'http://www.openjpeg.org/'
+  url 'http://openjpeg.googlecode.com/files/openjpeg-2.0.0.tar.gz'
+  sha1 '0af78ab2283b43421458f80373422d8029a9f7a7'
+
+  head 'http://openjpeg.googlecode.com/svn/trunk/'
+
+  depends_on 'cmake' => :build
+  depends_on 'little-cms2'
+  depends_on 'libtiff'
+  depends_on :libpng
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make install"
+  end
+end
