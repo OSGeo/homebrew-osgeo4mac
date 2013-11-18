1575c467b01723000666607f9d1a0d2bf7bc242b
Larry Shaffer <larrys@dakotacarto.com>
2013-11-18 11:58:59 -0700

    pyspatialite: fix homepage and add head url

--- Formula/pyspatialite.rb
+++ Formula/pyspatialite.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class Pyspatialite < Formula
-  homepage 'https://launchpad.net/charm-tools'
+  homepage 'http://code.google.com/p/pyspatialite/'
   url 'https://pypi.python.org/packages/source/p/pyspatialite/pyspatialite-3.0.1.tar.gz'
   sha1 'bcb3fdbc902a1b2f4451f2dad84dbbfba157ed4e'
+
+  head url 'https://code.google.com/p/pyspatialite/', :using => :hg
 
   depends_on :python
   depends_on 'geos'
@@ -15,7 +17,8 @@ class Pyspatialite < Formula
     # Patch to work with libspatialite 4.x, drop amalgamation support, dynamically
     # link to libspatialite and sqlite3, and fix redeclaration build error
     # Reported upstream: http://code.google.com/p/pyspatialite/issues/detail?id=15
-    "https://gist.github.com/dakcarto/7510460/raw"
+    # (not tested/supported with HEAD builds)
+    "https://gist.github.com/dakcarto/7510460/raw" if build.stable?
   end
 
   def install
