166fc7316428a3d15c5f6f671ba084006229b1e1
Larry Shaffer <larrys@dakotacarto.com>
2013-11-19 19:39:45 -0700

    pyspatialite: update to match PR

--- Formula/pyspatialite.rb
+++ Formula/pyspatialite.rb
@@ -5,7 +5,7 @@ class Pyspatialite < Formula
   url 'https://pypi.python.org/packages/source/p/pyspatialite/pyspatialite-3.0.1.tar.gz'
   sha1 'bcb3fdbc902a1b2f4451f2dad84dbbfba157ed4e'
 
-  head url 'https://code.google.com/p/pyspatialite/', :using => :hg
+  head 'https://code.google.com/p/pyspatialite/', :using => :hg
 
   depends_on :python
   depends_on 'geos'
@@ -15,7 +15,7 @@ class Pyspatialite < Formula
 
   def patches
     # Patch to work with libspatialite 4.x, drop amalgamation support, dynamically
-    # link to libspatialite and sqlite3, and fix redeclaration build error
+    # link libspatialite and sqlite3, and fix redeclaration build error
     # Reported upstream: http://code.google.com/p/pyspatialite/issues/detail?id=15
     # (not tested/supported with HEAD builds)
     "https://gist.github.com/dakcarto/7510460/raw" if build.stable?
@@ -25,8 +25,8 @@ class Pyspatialite < Formula
     # write setup.cfg
     (buildpath/"setup.cfg").write <<-EOS.undent
       [build_ext]
-      include_dirs=/usr/local/include/:/usr/local/opt/sqlite/include/
-      library_dirs=/usr/local/lib:/usr/local/opt/sqlite/lib
+      include_dirs=#{HOMEBREW_PREFIX}/include/:#{HOMEBREW_PREFIX}/opt/sqlite/include/
+      library_dirs=#{HOMEBREW_PREFIX}/lib:#{HOMEBREW_PREFIX}/opt/sqlite/lib
     EOS
 
     python do
