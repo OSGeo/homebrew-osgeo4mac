c941649e5f99d5576aa9577261c3a1502cff58d9
Larry Shaffer <larrys@dakotacarto.com>
2013-11-24 11:53:11 -0700

    libnoise: switch home; add caveat; fix share inreplace

--- Formula/libnoise.rb
+++ Formula/libnoise.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Libnoise < Formula
-  homepage 'http://libnoise.sourceforge.net/'
+  homepage 'https://github.com/qknight/libnoise'
   url 'https://github.com/qknight/libnoise/archive/master.zip'
   sha1 '9b5572e4bff04a12aa97b044607e65f118948124'
 
@@ -13,7 +13,7 @@ class Libnoise < Formula
   depends_on 'doxygen' if build.include? 'with-docs'
 
   def install
-    inreplace "doc/CMakeLists.txt", "/usr/share", "#{share}" if build.include? 'with-docs'
+    inreplace "doc/CMakeLists.txt", "/usr/share", share if build.include? 'with-docs'
 
     args = std_cmake_args
     args << "-DBUILD_LIBNOISE_DOCUMENTATION=ON" if build.include? 'with-docs'
@@ -22,5 +22,14 @@ class Libnoise < Formula
       system 'cmake', '..', *args
       system 'make install'
     end
+  end
+
+  def caveats; <<-EOS.undent
+    This formula is installed from a fork of the main project, which offers a
+    a CMake-based install. Original project is located here:
+
+      `http://libnoise.sourceforge.net`
+
+    EOS
   end
 end
