fa7f9ba9f6baa03cbb31c49bab7e8191a813f48a
Larry Shaffer <larrys@dakotacarto.com>
2013-11-25 23:51:37 -0700

    saga-gis: update; still no joy on gui

--- Formula/saga-gis.rb
+++ Formula/saga-gis.rb
@@ -2,12 +2,16 @@ require 'formula'
 
 class SagaGis < Formula
   homepage 'http://www.saga-gis.org'
-  url 'http://sourceforge.net/projects/saga-gis/files/SAGA%20-%202.1/SAGA%202.1.0/saga_2.1.0.tar.gz/download'
+  url 'http://downloads.sourceforge.net/project/saga-gis/SAGA%20-%202.1/SAGA%202.1.0/saga_2.1.0.tar.gz'
   sha1 '1da4d7aed8ceb9efab9698b2c3bdb2670e65c5dd'
 
   option 'disable-gui', "Build the command line version of saga-gis (saga_cmd) only"
   option 'enable-python', "Build pythong bindings for saga-gis"
 
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
+  
   depends_on 'geos'
   depends_on 'gdal'
   depends_on 'proj'
@@ -15,12 +19,9 @@ class SagaGis < Formula
   depends_on 'fftw'
   depends_on 'unixodbc'
   depends_on 'wxmac'
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
 
   def patches
-    {:p0 => "https://gist.github.com/jctull/6f85de4211b499f1d117/raw"}
+    {:p0 => "https://gist.github.com/jctull/6f85de4211b499f1d117/raw/28de4ba6f1a99ecc207d31927d48646f65296dec/saga-gis.patch"}
   end
 
   def install
@@ -33,5 +34,9 @@ class SagaGis < Formula
     args << "--enable-python" if build.include? 'enable-python'
     system "./configure", *args
     system "make", "install"
+  end
+  
+  test do
+    system "#{bin}/saga_cmd", "-v"
   end
 end
