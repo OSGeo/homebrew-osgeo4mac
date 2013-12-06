ff7f62b563b53265da88e5f984ceb32baa8d1f50
Larry Shaffer <larrys@dakotacarto.com>
2013-12-05 18:21:48 -0700

    qgis-20: add isolation and env var setup
    - Redo sip/pyqt requirements, again
    - Env var setup currently in post_install, making it easier to debug

--- Formula/qgis-20.rb
+++ Formula/qgis-20.rb
@@ -39,32 +39,37 @@ class PythonUnlinked < Requirement
   end
 end
 
-class PyQtImportable < Requirement
-  fatal true
-  satisfy { quiet_system 'python', '-c', 'from PyQt4 import QtCore' }
-
-  def message
-    <<-EOS.undent
-      Python could not import the PyQt4 module. This will cause the QGIS build to fail.
-      The most common reason for this failure is that the PYTHONPATH needs to be adjusted.
-      The `pyqt` caveats explain this adjustment and may be reviewed using:
-
-          brew info pyqt
-
-      Ensure `pyqt` formula is installed and linked.
-
-    EOS
-  end
-end
-
 class SipBinary < Requirement
   fatal true
+  default_formula 'sip'
   satisfy { which 'sip' }
 
   def message
     <<-EOS.undent
       The `sip` binary is missing. It is needed to generate the Python bindings for QGIS.
       Ensure `sip` formula is installed and linked.
+
+    EOS
+  end
+end
+
+class PyQtConfig < Requirement
+  fatal true
+  default_formula 'pyqt'
+  # pyqtconfig is not created with PyQt4 >= 4.10.x when using configure-ng.
+  # Homebrew's `pyqt` formula corrects this. Remains an issue until QGIS project
+  # adjusts FindPyQt.py in CMake setup to work with configure-ng.
+  satisfy { quiet_system 'python', '-c', 'from PyQt4 import pyqtconfig' }
+
+  def message
+    <<-EOS.undent
+      Python could not import the PyQt4.pyqtconfig module. This will cause the QGIS build to fail.
+      The most common reason for this failure is that the PYTHONPATH needs to be adjusted.
+      The `pyqt` caveats explain this adjustment and may be reviewed using:
+
+          brew info pyqt
+
+      Ensure `pyqt` formula is installed and linked, and that it includes the `pyqtconfig` module.
 
     EOS
   end
@@ -77,6 +82,7 @@ class Qgis20 < Formula
 
   head 'https://github.com/qgis/QGIS.git', :branch => 'master'
 
+  option 'enable-isolation', "Isolate .app's environment to HOMEBRE_PREFIX, to coexist with other QGIS installs"
   option 'with-debug', 'Enable debug build/output (default for --HEAD installs)'
   option 'without-brewed-python', "Prefer system Python (default is Homebrew's, if linked)"
   option 'without-server', 'Build without QGIS Server (qgis_mapserv.fcgi)'
@@ -89,7 +95,7 @@ class Qgis20 < Formula
   option 'with-saga-gis', 'Build extra Saga GIS for Processing plugin'
   option 'with-qt-mysql', 'Build extra Qt MySQL plugin for eVis plugin'
   option 'with-api-docs', 'Build the API documentation with Doxygen and Graphviz'
-#   option 'persistent-build', 'Maintain the build directory in HOMEBREW_TEMP (--HEAD only)'
+  #option 'persistent-build', 'Maintain the build directory in HOMEBREW_TEMP (--HEAD only)'
 
   # core qgis
   depends_on 'cmake' => :build
@@ -99,18 +105,16 @@ class Qgis20 < Formula
     depends_on 'doxygen' => [:build, 'with-dot'] # with graphviz support
   end
   # while QGIS can be built without Python support, it is ON by default here
-  if build.with? 'brewed-python'
+  if build.with?('brewed-python') || build.include?('enable-isolation')
     depends_on BrewedPython
   else
     depends_on PythonUnlinked
   end
   depends_on :python
   depends_on 'qt'
-  depends_on 'pyqt'
-  depends_on 'qscintilla2' # will probably be a C++ lib deps in near future
-  depends_on PyQtImportable
   depends_on SipBinary
-
+  depends_on PyQtConfig
+  depends_on 'qscintilla2' # will probably be a C++ lib deps in near future
   depends_on 'qwt60' # keg_only, max of 6.0.2 works with embedded QwtPolar in QGIS 2.0.1
   # TODO: add QwtPolar 1.1 formula for HEAD builds (then set external CMake options)
   depends_on 'gsl'
@@ -137,7 +141,7 @@ class Qgis20 < Formula
   depends_on 'osgearth' if build.with? 'globe'
   depends_on 'qt-mysql' => :optional # for eVis plugin (non-functional in 2.0.1?)
 
-  # processing plugin extras
+  # core processing plugin extras
   # see `postgis` and `grass` above
   depends_on 'orfeo' => :optional
   depends_on 'r' => :optional
@@ -186,7 +190,7 @@ class Qgis20 < Formula
       end
     end
 
-    if build.with? 'debug' || build.head?
+    if build.with?('debug') || build.head?
       args << '-DCMAKE_BUILD_TYPE=RelWithDebInfo'
     else
       args << '-DCMAKE_BUILD_TYPE=None'
@@ -229,29 +233,10 @@ class Qgis20 < Formula
 
     python do
       # TODO: update .app's bundle identifier for HEAD builds
+      # (convert to using `defaults`)
+      #/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier org.qgis.qgis-dev" "$APPTARGET/Contents/Info.plist"
 
       # TODO: keep persistent build directory for HEAD builds
-      ##tmpname = File.basename Dir.pwd
-      #src = Dir.pwd
-      #if build.include? 'persistent-build'
-      # cd '..' do
-      #   mkdir 'qgis-build' unless File.exists?('qgis-build')
-      #   ln_s '../qgis-build', src + '/build'
-      #   cd 'qgis-build' do
-      #     if File.exists?('CMakeCache.txt')
-      #       # swap out any old cache dir path and new temp src directory path
-      #       inreplace "CMakeCache.txt" do |s|
-      #         s.sub! /(CMAKE_CACHEFILE_DIR:INTERNAL=)(.+)$/, '\1' + Dir.pwd
-      #         oldsrc = /(CMAKE_HOME_DIRECTORY:INTERNAL=)(.+)$/.match(s)[2]
-      #         s.gsub! oldsrc, src
-      #       end
-      #     end
-      #   end
-      # end
-      #else
-      # mkdir 'build'
-      #end
-
       mkdir 'build'
 
       cd 'build' do
@@ -265,21 +250,6 @@ class Qgis20 < Formula
       qgis_modules = prefix/'QGIS.app/Contents/Resources/python/qgis'
       py_lib.mkpath
       ln_s qgis_modules, py_lib/'qgis'
-
-      # TODO: define default env vars, relative to whether or not to isolate
-
-      # TODO: write PYQGIS_STARTUP file pyqgis_startup.py, if isolating
-
-      # TODO: add Info.plist setup, add other env vars: GDAL, OSG, etc.
-
-      # TODO: rewrite this only for running QGIS.app/Contents/MacOS/QGIS directly
-      # Create script to launch QGIS app
-      (bin + 'qgis').write <<-EOS.undent
-        #!/bin/sh
-        # Ensure Python modules can be found when QGIS is running.
-        env PATH='#{HOMEBREW_PREFIX}/bin':$PATH PYTHONPATH='#{HOMEBREW_PREFIX}/lib/#{python.xy}/site-packages':$PYTHONPATH\\
-          open #{prefix}/QGIS.app
-      EOS
     end
 
     ln_s 'QGIS.app/Contents/MacOS/fcgi-bin', prefix/'fcgi-bin' if build.with? 'server'
@@ -288,11 +258,131 @@ class Qgis20 < Formula
     mv prefix/'QGIS.app/Contents/Resources/doc/api', doc/'api' if build.with? 'api-docs'
     ln_s prefix/'QGIS.app/Contents/Resources/doc', doc/'doc'
 
+    # copy PYQGIS_STARTUP file pyqgis_startup.py, even if not isolating (so tap can be untapped)
+    # only works with QGIS > 2.0.1
+    # TODO: change to gist resource?
+    cp HOMEBREW_PREFIX/'Library/Taps/dakcarto-osgeo4mac/enviro/python_startup.py', prefix/'pyqgis_startup.py'
+
+    bin.mkdir
+    touch "#{bin}/qgis" # so it will be linked into HOMEBREW_PREFIX
+  end
+
+  def post_install
+    # configure environment variables for .app and launching binary directly.
+    # having this in `post_intsall` allows it to be individually run *after* installation with:
+    #    `brew postinstall -v qgis-20 [--option --option ...]`
+
+    # define default isolation env vars
+    pthsep = File::PATH_SEPARATOR
+    pypth = "#{HOMEBREW_PREFIX}/lib/#{python.xy}/site-packages"
+    pths = %W[#{HOMEBREW_PREFIX/'bin'} /usr/bin /bin /usr/sbin /sbin /usr/X11/bin].join(pthsep)
+
+    unless build.include? 'enable-isolation'
+      pths = ORIGINAL_PATHS.join(pthsep)
+      unless pths.include? HOMEBREW_PREFIX/'bin'
+        pths = HOMEBREW_PREFIX/'bin' + pthsep + pths
+      end
+      pyenv = ENV['PYTHONPATH']
+      if pyenv
+        pypth = (pyenv.include?(pypth)) ? pyenv : pypth + pthsep + pyenv
+      end
+    end
+
+    envars = {
+      :PATH => "#{pths}",
+      :PYTHONPATH => "#{pypth}",
+      :GDAL_DRIVER_PATH => "#{HOMEBREW_PREFIX}/lib/gdalplugins"
+    }
+
+    if build.with? 'grass'
+      grass = Formula.factory('grass')
+      envars[:GRASS_PREFIX] = "#{grass.opt_prefix}/grass-#{grass.linked_keg.realpath.basename}"
+    end
+
+    if build.with? 'globe'
+      osg = Formula.factory('open-scene-graph')
+      envars[:OSG_PLUGINS_PATH] = "#{HOMEBREW_PREFIX}/lib/osgPlugins-#{osg.linked_keg.realpath.basename}"
+    end
+
+    if build.include? 'enable-isolation'
+      envars[:DYLD_FRAMEWORK_PATH] = "#{HOMEBREW_PREFIX}/Frameworks:/System/Library/Frameworks"
+      versioned = %W[
+        #{Formula.factory('sqlite').opt_prefix}/lib
+        #{Formula.factory('expat').opt_prefix}/lib
+        #{Formula.factory('libxml2').opt_prefix}/lib
+        #{Formula.factory('qwt60').opt_prefix}/lib
+        #{HOMEBREW_PREFIX}/lib
+      ]
+      envars[:DYLD_VERSIONED_LIBRARY_PATH] = versioned.join(pthsep)
+      # TODO: is PYTHONHOME necessary for isolation, or is it set by embedded interpreter?
+      #envars[:PYTHONHOME] = "#{python.framework}/Python.framework/Versions/Current"
+      envars[:PYQGIS_STARTUP] = opt_prefix/'pyqgis_startup.py' # only works with QGIS > 2.0.1
+    end
+
+    #envars.each { |key, value| puts "#{key.to_s}=#{value}" }
+    #exit
+
+    # add env vars to QGIS.app's Info.plist, in LSEnvironment section
+    app = prefix/'QGIS.app'
+    domn = app/'Contents/Info'
+    plst = app/'Contents/Info.plist'
+    # first delete any LSEnvironment setting, ignoring errors
+    # CAUTION!: may not be what you want, if .app already has LSEnvironment settings
+    system "defaults delete \"#{domn}\" LSEnvironment 2>/dev/null"
+    kv = '{ '
+    envars.each { |key, value| kv += "'#{key.to_s}' = '#{value}'; " }
+    kv += '}'
+    system "defaults write \"#{plst}\" \"LSEnvironment\" \"#{kv}\""
+    # leave the plist readable; convert from binary to XML format
+    system "plutil -convert xml1 -- \"#{plst}\""
+    # update modification date on app bundle, or changes won't take effect
+    system "touch \"#{app}\""
+
+    # add env vars to launch script for QGIS app's binary
+    qgis_bin = bin/'qgis'
+    rm_f qgis_bin if File.exists?(qgis_bin) # install generates empty file
+    bin_cmds = %W[#!/bin/sh\n]
+    # setup shell-prepended env vars (may result in duplication of paths)
+    envars[:PATH] = "#{HOMEBREW_PREFIX}/bin" + pthsep + '$PATH'
+    envars[:PYTHONPATH] = "#{HOMEBREW_PREFIX}/lib/#{python.xy}/site-packages" + pthsep + '$PYTHONPATH'
+    envars.each { |key, value| bin_cmds << "export #{key.to_s}=#{value}" }
+    bin_cmds << opt_prefix/'QGIS.app/Contents/MacOS/QGIS'
+    qgis_bin.write(bin_cmds.join("\n"))
+    qgis_bin.chmod 0755
   end
 
   def caveats
-    # TODO: complete rewrite, not using .MacOSX/environment.plist hack
-    s = ''
+    s = <<-EOS.undent
+      QGIS is built as an application bundle. Environment variables for the
+      Homebrew prefix have been embedded in QGIS.app:
+
+        #{opt_prefix}/QGIS.app
+
+      You may also symlink QGIS.app into ~/Applications:
+
+        brew linkapps
+
+      To run the `QGIS.app/Contents/MacOS/QGIS` binary use the wrapper script
+      pre-defined with Homebrew prefix environment variables:
+
+        #{opt_prefix}/bin/qgis
+
+      NOTE: Your current PATH and PYTHONPATH environment variables are honored
+            when launching via the wrapper script, while launching QGIS.app bundle will not.
+
+    EOS
+    s += python.standard_caveats if python
+
+    if build.include? 'enable-isolation'
+      s += <<-EOS.undent
+        QGIS built with isolation enabled. This allows it to coexist with other
+        types of installations of QGIS on your Mac. However, on versions >= 2.0.1,
+        this also means Python modules installed in the *system* Python will NOT
+        be available to Python processes within QGIS.app.
+
+      EOS
+    end
+
     # check for required run-time Python module dependencies
     # TODO: add 'pyspatialite' dep for DB Manager (currently being updated by developer)
     xm = []
@@ -317,23 +407,6 @@ class Qgis20 < Formula
 
       EOS
     end
-
-    s += <<-EOS.undent
-      QGIS has been built as an application bundle. To make it easily available, a
-      wrapper script has been written that launches the app with environment
-      variables set so that Python modules will be functional:
-
-        qgis
-
-      You may also symlink QGIS.app into ~/Applications:
-        brew linkapps
-        mkdir -p #{ENV['HOME']}/.MacOSX
-        defaults write #{ENV['HOME']}/.MacOSX/environment.plist PYTHONPATH -string "#{HOMEBREW_PREFIX}/lib/#{python.xy}/site-packages"
-
-      You will need to log out and log in again to make environment.plist effective.
-
-    EOS
-    s += python.standard_caveats if python
     s
   end
 end
