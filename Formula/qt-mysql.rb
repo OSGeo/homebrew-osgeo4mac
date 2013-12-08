d41c801d89d8bda19c5e20f5a71f592fcdd30b9e
Larry Shaffer <larrys@dakotacarto.com>
2013-12-08 16:43:31 -0700

    qt-mysql: update downloads’s  remote host path

--- Formula/qt-mysql.rb
+++ Formula/qt-mysql.rb
@@ -3,7 +3,7 @@ require 'formula'
 class QtMysql < Formula
   homepage 'http://qt-project.org/'
   # 15 KB, stripped down archive of just what's needed to compile driver
-  url 'http://qgis.dakotacarto.com/qt-4.8.5-mysql-driver.tar.gz'
+  url 'http://qgis.dakotacarto.com/osgeo4mac/qt-4.8.5-mysql-driver.tar.gz'
   sha1 '2aa4651510b6974c0c129cfc673b88c7f8dbeeb2'
   # 241.5 MB, if downloading full source
   # url 'http://download.qt-project.org/official_releases/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.tar.gz'
