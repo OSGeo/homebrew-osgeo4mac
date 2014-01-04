8926bdd467db740b77f3e70d444c7221a511fb3b
Larry Shaffer <larrys@dakotacarto.com>
2014-01-04 00:19:22 -0700

    requirement: move GdalThirdParty to new Requirements dir

---
+++ Requirements/gdal_third_party.rb
@@ -0,0 +1,17 @@
+class GdalThirdParty < Requirement
+  fatal true
+
+  satisfy do
+    envar = ENV['GDAL_THIRD_PARTY']
+    envar && File.exists?(envar)
+  end
+
+  def message; <<-EOS.undent
+    Define GDAL_THIRD_PARTY environment variable that points to a directory,
+    which contains the unaltered download archive of the third-party library:
+
+      `export GDAL_THIRD_PARTY=path/to/gdal/third-party/directory`
+
+  EOS
+  end
+end
