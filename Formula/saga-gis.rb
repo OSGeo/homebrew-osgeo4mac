557d5a47ddd501dc0716a10abbe49b20e7105926
Larry Shaffer <larrys@dakotacarto.com>
2014-01-16 18:50:45 -0700

    saga-gis: updated to match ongoing homebrew PR (works now as GUI.app and via QGIS Processing plugin)

--- Formula/saga-gis.rb
+++ Formula/saga-gis.rb
@@ -1,42 +1,150 @@
 require 'formula'
 
 class SagaGis < Formula
-  homepage 'http://www.saga-gis.org'
-  url 'http://downloads.sourceforge.net/project/saga-gis/SAGA%20-%202.1/SAGA%202.1.0/saga_2.1.0.tar.gz'
-  sha1 '1da4d7aed8ceb9efab9698b2c3bdb2670e65c5dd'
+  homepage 'http://saga-gis.org'
+  url 'http://downloads.sourceforge.net/project/saga-gis/SAGA%20-%202.1/SAGA%202.1.1/saga_2.1.1.tar.gz'
+  sha1 '40ac4d0646e04187aa6728181c3954993e51dcd5'
 
-  option 'disable-gui', "Build the command line version of saga-gis (saga_cmd) only"
-  option 'enable-python', "Build pythong bindings for saga-gis"
+  head 'svn://svn.code.sf.net/p/saga-gis/code-0/trunk/saga-gis'
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-  
-  depends_on 'geos'
+  option "build-app", "Build SAGA.app Package"
+
+  depends_on :automake
+  depends_on :autoconf
+  depends_on :libtool
   depends_on 'gdal'
-  depends_on 'proj'
   depends_on 'jasper'
-  depends_on 'fftw'
-  depends_on 'unixodbc'
+  depends_on 'proj'
   depends_on 'wxmac'
+  depends_on 'libharu' => :recommended
+  depends_on 'postgresql' => :optional
 
   def patches
-    {:p0 => "https://gist.github.com/jctull/6f85de4211b499f1d117/raw/28de4ba6f1a99ecc207d31927d48646f65296dec/saga-gis.patch"}
+    # Compiling on Mavericks with libc++ causes issues with LC_NUMERIC.
+    # https://sourceforge.net/p/saga-gis/patches/12/
+    DATA
+  end
+
+  resource 'app_icon' do
+    url 'http://web.fastermac.net/~MacPgmr/SAGA/saga_gui.icns'
+    sha1 '1ff67c6d600dd161684d3e8b33a1d138c65b00f4'
+  end
+
+  resource 'projects' do
+    url 'http://trac.osgeo.org/proj/export/2409/branches/4.8/proj/src/projects.h'
+    sha1 '867367a8ef097d5ff772b7f50713830d2d4bc09c'
+    version '4.8.0'
   end
 
   def install
-    cxxstdlib_check :skip
-    args = [
-      "--disable-openmp",
-      "--prefix=#{prefix}"
-    ]
-    args << "--disable-gui" if build.include? 'disable-gui'
-    args << "--enable-python" if build.include? 'enable-python'
-    system "./configure", *args
-    system "make", "install"
+    (buildpath/'src/modules_projection/pj_proj4/pj_proj4/').install resource('projects')
+
+    # Need to remove unsupported libraries from various Makefiles
+    # http://sourceforge.net/apps/trac/saga-gis/wiki/Compiling%20SAGA%20on%20Mac%20OS%20X
+    inreplace "src/saga_core/saga_gui/Makefile.am", "aui,base,", ""
+    inreplace "src/saga_core/saga_gui/Makefile.am", "propgrid,", ""
+
+    system "autoreconf", "-i"
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+
+    if build.include? "build-app"
+      # Based on original script by Phil Hess
+      # http://web.fastermac.net/~MacPgmr/
+
+      (buildpath).install resource('app_icon')
+      mkdir_p "#{buildpath}/SAGA.app/Contents/MacOS"
+      mkdir_p "#{buildpath}/SAGA.app/Contents/Resources"
+
+      Pathname(buildpath/'SAGA.app/Contents/PkgInfo').write 'APPLSAGA'
+      cp "#{buildpath}/saga_gui.icns", "#{buildpath}/SAGA.app/Contents/Resources/"
+      ln_s "#{bin}/saga_gui", "#{buildpath}/SAGA.app/Contents/MacOS/saga_gui"
+
+      config = <<-EOS.undent
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+          <key>CFBundleDevelopmentRegion</key>
+          <string>English</string>
+          <key>CFBundleExecutable</key>
+          <string>saga_gui</string>
+          <key>CFBundleIconFile</key>
+          <string>saga_gui.icns</string>
+          <key>CFBundleInfoDictionaryVersion</key>
+          <string>6.0</string>
+          <key>CFBundleName</key>
+          <string>SAGA</string>
+          <key>CFBundlePackageType</key>
+          <string>APPL</string>
+          <key>CFBundleSignature</key>
+          <string>SAGA</string>
+          <key>CFBundleVersion</key>
+          <string>1.0</string>
+          <key>CSResourcesFileMapped</key>
+          <true/>
+        </dict>
+        </plist>
+    EOS
+
+    Pathname(buildpath/'SAGA.app/Contents/Info.plist').write config
+    prefix.install "SAGA.app"
+
+    end
   end
-  
-  test do
-    system "#{bin}/saga_cmd", "-v"
+
+  def caveats
+    if build.include? "build-app" then <<-EOS.undent
+      SAGA.app was installed in:
+        #{prefix}
+
+      To symlink into ~/Applications, you can do:
+        brew linkapps
+
+      Note that the SAGA GUI does not work very well yet.
+      It has problems with creating a preferences file in the correct location and sometimes won't shut down (use Activity Monitor to force quit if necessary).
+      EOS
+    end
   end
 end
+
+__END__
+diff --git a/src/saga_core/saga_cmd/saga_cmd.cpp b/src/saga_core/saga_cmd/saga_cmd.cpp
+index 0ce6d36..9f554a8 100644
+--- a/src/saga_core/saga_cmd/saga_cmd.cpp
++++ b/src/saga_core/saga_cmd/saga_cmd.cpp
+@@ -67,6 +67,7 @@
+ #include "callback.h"
+ 
+ #include "module_library.h"
++#include <locale.h>
+ 
+ 
+ ///////////////////////////////////////////////////////////
+diff --git a/src/modules_io/grid/io_grid/xyz.cpp b/src/modules_io/grid/io_grid/xyz.cpp
+index 9b6dc3b..3efdaa1 100644
+--- a/src/modules_io/grid/io_grid/xyz.cpp
++++ b/src/modules_io/grid/io_grid/xyz.cpp
+@@ -173,6 +173,10 @@ bool CXYZ_Export::On_Execute(void)
+ 	return( false );
+ }
+ 
++//---------------------------------------------------------
++CXYZ_Export::~CXYZ_Export(void)
++{}
++
+ 
+ ///////////////////////////////////////////////////////////
+ //														 //
+diff --git a/src/modules_io/grid/io_grid/xyz.h b/src/modules_io/grid/io_grid/xyz.h
+index ffbd194..33b62fd 100644
+--- a/src/modules_io/grid/io_grid/xyz.h
++++ b/src/modules_io/grid/io_grid/xyz.h
+@@ -86,6 +86,7 @@ class CXYZ_Export : public CSG_Module_Grid
+ {
+ public:
+ 	CXYZ_Export(void);
++	virtual ~CXYZ_Export(void);
+ 
+ 	virtual CSG_String		Get_MenuPath	(void)			{	return( _TL("R:Export") );	}
+ 
