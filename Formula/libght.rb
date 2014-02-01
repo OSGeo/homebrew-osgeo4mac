15b739b56bf60b67674b4ca6fb80c8c7368a5d4e
Larry Shaffer <larrys@dakotacarto.com>
2014-01-31 21:19:30 -0700

    libght: new formula; hash tree for storing and accessing multi-dimensional point clouds
    - Note: tests calling ght_node_to_nodelist() fail with segfault

---
+++ Formula/libght.rb
@@ -0,0 +1,30 @@
+require "formula"
+
+class Libght < Formula
+  homepage "https://github.com/pramsey/libght/"
+  url "https://github.com/pramsey/libght/archive/v0.1.0.tar.gz"
+  sha1 "19104cdba21fabb8d5fad847af1a8e8bcde40b6a"
+
+  head "https://github.com/pramsey/libght.git", :branch => "master"
+
+  option "with-tests", "Run unit tests after build, prior to install"
+
+  depends_on "cmake" => :build
+  depends_on "proj"
+  depends_on "liblas"
+  depends_on "cunit" if build.with? "tests"
+
+  def install
+    ENV.libxml2
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      puts %x(test/cu_tester) if build.with? "tests"
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match "version #{version.to_s[0,3]}", %x(#{bin}/"las2ght")
+  end
+end
