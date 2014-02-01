2e727ac4dc15e3864743b4884f6b39cefe2cfff6
Larry Shaffer <larrys@dakotacarto.com>
2014-01-31 21:15:59 -0700

    pdal: (WIP) update in preparation for next release

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -1,83 +1,76 @@
-require 'formula'
+require "formula"
 
 class Pdal < Formula
-  homepage 'http://pointcloud.org'
-  url 'https://github.com/PDAL/PDAL/archive/0.9.8.tar.gz'
-  sha1 '6a6a76e6531473541746c19154630ff403099547'
+  homepage "http://pointcloud.org"
+  # TODO: remove. temp url in preparation for next release
+  url "https://github.com/PDAL/PDAL.git",
+      :revision => "9be105d72e7692150628d7fcde8bea8ac795e2be"
+  version "1.0.0-9be105d"
+  sha1 "9be105d72e7692150628d7fcde8bea8ac795e2be"
 
-  head 'https://github.com/PDAL/PDAL.git'
+  head "https://github.com/PDAL/PDAL.git", :branch => "master"
 
-  option "with-caris", "Build with proprietary CARIS format support"
+  option "with-external-boost", "Build with Homebrew Boost instead of internal"
+  option "with-swig", "Build with Python bindings"
+  option "with-soci", "Build with SOCI database access support"
   option "with-mrsid", "Build with proprietary MrSID format support"
   option "with-oracle", "Build with proprietary Oracle format support"
-  option "with-nitro", "Build with DoD LAS-inside-NITF containers support"
-  option "with-swig", "Build with Python bindings"
-  option "with-tests", "Run CTest after build, prior to install"
+  option "with-tests", "Run unit tests after build, prior to install"
+  option "with-doc", "Install API documentation and tutorial"
 
-  depends_on 'cmake' => :build
-  depends_on 'boost' # unit test errors when using pdal-internal boost
-  depends_on :python => :recommended
-  depends_on "swig" => :optional
-  depends_on "proj" => :recommended
-  depends_on "libgeotiff" => :recommended
-  depends_on "gdal" => :recommended
-  depends_on "laszip" => :recommended
-  #depends_on "points2grid" => :optional # TODO: circular dependency fixable?
-  depends_on "flann" => :recommended
+  depends_on "cmake" => :build
+  depends_on "boost" if build.with? "external-boost"
+  depends_on :python # for PLang filters
+  depends_on "swig" => [:build, :optional]
+  depends_on "libgeotiff"
+  depends_on "gdal"
   depends_on :postgresql => :recommended
+  depends_on "laszip" => :recommended
+  depends_on "msgpack" => :recommended
+  # TODO: nix tap dup once version points2grid 1.1.1 is pushed to main tap
+  #       why doesn't :recommended work for taps?
+  depends_on "dakcarto/osgeo4mac/points2grid" # => :recommended
   depends_on "hexer" => :recommended
-  depends_on "nitro" => :optional
+  depends_on "soci" => :optional
 
-  # TODO: add [CARIS,] MrSID and Oracle, via HOMEBREW_LOCAL formulae
+  # TODO: add MrSID and Oracle, via HOMEBREW_LOCAL formulae
 
-  # Fix swig rename error: "Cannot find source file: python/pdal.i"
-  # Fix swig build error: headers for pdal not found
-  # https://github.com/PDAL/PDAL/pull/239
-  def patches
-    if build.with? "swig"
-      "https://gist.github.com/dakcarto/8721545/raw/15316b8a3933e07f03738b86e7108c1a4ad8e941/pdal-swig.diff"
-    end
+  if build.with? "doc"
+    depends_on "doxygen"
+    depends_on "sphinx" => :python
+    depends_on "breathe" => :python
   end
 
   def install
     ENV.libxml2
-    args = std_cmake_args
-    # unit test errors when using pdal-internal boost with stable
-    args << "-DPDAL_EMBED_BOOST=FALSE" unless build.head?
-    args << "-DWITH_ICONV=TRUE"
-    args << "-DWITH_LIBXML2=TRUE"
+    args = std_cmake_args.concat %W[
+      -DWITH_PKGCONFIG=TRUE
+      -DWITH_NITRO=FALSE
+    ]
+    args << "-DPDAL_EMBED_BOOST=FALSE" if build.with? "external-boost"
+    args << "-DWITH_TESTS=FALSE" if build.without? "tests"
 
-    unless build.without? "python"
-      args << "-DWITH_PYTHON=TRUE"
-      if brewed_python?
-        args << "-DPYTHON_INCLUDE_DIR=#{brewed_python_framework}/Headers"
-        args << "-DPYTHON_LIBRARY=#{brewed_python_framework}/Python"
-      end
+    if brewed_python?
+      args << "-DPYTHON_INCLUDE_DIR=#{brewed_python_framework}/Headers"
+      args << "-DPYTHON_LIBRARY=#{brewed_python_framework}/Python"
     end
-    if build.with? "swig"
-      args << "-DWITH_SWIG_PYTHON=TRUE"
-      # Fix rename error: "Cannot find source file: python/pdal.i"
-      # https://github.com/PDAL/PDAL/pull/239
-      mv "python/libpc.i", "python/pdal.i"
-    end
+    args << "-DWITH_SWIG_PYTHON=TRUE" if build.with? "swig"
 
-    args << "-DWITH_GEOTIFF=TRUE" unless build.without? "libgeotiff"
-    args << "-DWITH_GDAL=TRUE" unless build.without? "gdal"
-    args << "-DWITH_LASZIP=TRUE" unless build.without? "laszip"
-    #args << "-DWITH_P2G=TRUE" # circular dependency
-    args << "-DWITH_FLANN=TRUE" unless build.without? "flann"
-    args << "-DWITH_PGPOINTCLOUD=TRUE" unless build.without? "postgres"
-    args << "-DWITH_HEXER=TRUE" unless build.without? "hexer"
-    args << "-DWITH_NITRO=TRUE" unless build.without? "nitro"
+    args << "-DWITH_PGPOINTCLOUD=FALSE" if build.without? "postgresql"
+    args << "-DWITH_LASZIP=FALSE" if build.without? "laszip"
+    args << "-DWITH_MSGPACK=FALSE" if build.without? "msgpack"
+    # TODO: re-add conditional once points2grid 1.1.1 is pushed to main tap
+    args << "-DWITH_P2G=TRUE" # if build.with? "points2grid"
+    args << "-DWITH_HEXER=TRUE" if build.with? "hexer"
+    args << "-DWITH_SQLITE=TRUE" if build.with? "soci"
 
     # proprietary formats
-    args << "-DWITH_CARIS=TRUE" if build.with? "caris"
     args << "-DWITH_MRSID=TRUE" if build.with? "mrsid"
-    args << "-DWITH_ORACLE=TRUE" if build.with? "oracle"
+    args << "-DWITH_ORACLE=FALSE" if build.without? "oracle"
 
     mkdir "build" do
       system "cmake", "..", *args
-      #system 'bbedit', 'CMakeCache.txt'
+      #system "bbedit", "CMakeCache.txt"
       system "make"
       puts %x(ctest -VV .) if build.with? "tests"
       system "make", "install"
@@ -89,20 +82,32 @@ class Pdal < Formula
         end
       end
     end
-  end
 
-  #def caveats; <<-EOS.undent
-  #    To use 'drivers.p2g.writer' in PDAL to output point cloud interpolation,
-  #    install the `points2grid` package, which depends on PDAL.
-  #  EOS
-  #end
+    if build.with? "doc"
+      cd "doc" do
+        # nix need for `doxygen --with-dot` (and lengthy diagram generation)
+        inreplace "doxygen/doxygen.conf" do |s|
+          s.sub! /(HAVE_DOT\s*=\s*)YES/, "\\1NO"
+        end
+        inreplace "conf.py" do |s|
+          s.sub! ",'rst2pdf.pdfbuilder'", "" # not needed for just html
+          s.sub! '"indexsidebar.html",', "" # don't load ohloh sidebar
+          s.sub! /^\s*'disqus_shortname':\s*'pdal',$/, "" # don't load discus
+        end
+        system "make", "doxygen"
+        system "make", "html"
+        doc.install "doxygen/html" => "api"
+        doc.install "build/html" => "html"
+      end
+    end
+  end
 
   private
   # python utils (deprecated in latest Homebrew)
   # see: https://github.com/Homebrew/homebrew/pull/24842
 
   def which_python
-    "python" + %x(python -c 'import sys;print(sys.version[:3])').strip
+    "python" + %x(python -c "import sys;print(sys.version[:3])").strip
   end
 
   def brewed_python_framework
