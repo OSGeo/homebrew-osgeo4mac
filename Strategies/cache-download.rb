24f7931112267b45ed8929f50b94cac99d4af884
Larry Shaffer <larrys@dakotacarto.com>
2014-02-05 17:00:11 -0700

    strategy: reinstate local strategy using cache

---
+++ Strategies/cache-download.rb
@@ -0,0 +1,18 @@
+class CacheDownloadStrategy < CurlDownloadStrategy
+  def fetch
+    archive = @url.sub(%r[^file://], "")
+    unless File.exists?(archive)
+      odie <<-EOS.undent
+        Formula expects to locate the following archive:
+          #{Pathname.new(archive).basename}
+
+        in the HOMEBREW_CACHE directory:
+          #{HOMEBREW_CACHE}
+
+        Copy archive to cache or create a symlink in cache to archive:
+          ln -sf /path/to/archive $(brew --cache)/
+      EOS
+    end
+    super
+  end
+end
