9c89a9cdd00f938ea975b66d3a862e1c5105d48a
Larry Shaffer <larrys@dakotacarto.com>
2014-02-11 03:39:01 -0700

    orfeo: installs on 10.9; add patches

--- Formula/orfeo.rb
+++ Formula/orfeo.rb
@@ -14,18 +14,14 @@ class Orfeo < Formula
   depends_on 'gdal'
   depends_on 'qt'
   depends_on "muparser"
-  depends_on "liblas" # support removed in v4.0
+  depends_on "liblas" # support removed in v4.0, but still functional here
   depends_on "libkml"
   depends_on "minizip"
   depends_on "tinyxml"
+  depends_on "fftw" => :optional # restricts built binaries to GPL license
+  depends_on "opencv" => :optional
   # external libs that may work in next release:
   #depends_on "open-scene-graph" # (for libOpenThreads, now internal to osg)
-
-  depends_on "fftw" => :optional # restricts built binaries to GPL license
-  # these are currently experimental
-  depends_on "gettext" => :optional # support removed in v4.0
-  depends_on "libpqxx" => :optional # support removed in v4.0
-  depends_on "opencv" => :optional
 
   option 'examples', 'Compile and install various examples'
   option 'java', 'Enable Java support'
@@ -39,34 +35,30 @@ class Orfeo < Formula
   end
 
   def patches
+    p = []
     # Fix some CMake modules
-    # Ensure external liblas_c and liblas are found on Mac
-    # Ensure external libOpenThreads is not used unless specified; otherwise it
-    # may use open-scene-graph's newer lib, which fails when linking with orfeo
-    %W[
-      https://gist.github.com/dakcarto/8890690/raw/ab16c6cbaf7d214b786583f456d8839585a04fa7/orfeo-cmake-fixes.diff
-    ]
-    # Fix for forward declaration (and other issues) with clang and libc++
-    #https://gist.github.com/dakcarto/8890703/raw/47a363b483794c595dff83d23e989b9248454de5/orfeo-clang-fixes.diff
-  end
-
-  fails_with :clang do
-    build 500
-    cause <<-EOS.undent
-      Due to disallowed forward declarations (and other issues).
-      See https://groups.google.com/forum/#!topic/otb-users/dRjdIxlDWfs
-      No fix (2014-02-08)
-    EOS
+    #   Ensure external liblas_c and liblas are found on Mac
+    #   Ensure external libOpenThreads is not used unless specified; otherwise it
+    #   may use open-scene-graph's newer lib, which fails when linking with orfeo
+    p << "https://gist.github.com/dakcarto/8890690/raw/ab16c6cbaf7d214b786583f456d8839585a04fa7/orfeo-cmake-fixes.diff"
+    if ENV.compiler == :clang and MacOS.version >= :mavericks
+      # Fix for forward declaration (and other issues) with clang and libc++
+      #   See https://groups.google.com/forum/#!topic/otb-users/dRjdIxlDWfs
+      p << "https://gist.github.com/dakcarto/8930966/raw/331cca49a8e8dd579c4c19c865b17090a7433cd6/orfeo-libc-fixes.diff"
+    end
   end
 
   def install
-    (share/"orfeo/default_geoid").install resource("geoid")
+    (libexec/"default_geoid").install resource("geoid")
 
+    # gettext, libpqxx support removed in v4.0, deprecated here
     args = std_cmake_args + %W[
       -DBUILD_APPLICATIONS=ON
       -DOTB_USE_EXTERNAL_FLTK=ON
       -DBUILD_TESTING=OFF
       -DOTB_USE_EXTERNAL_OPENTHREADS=OFF
+      -DOTB_USE_GETTEXT=OFF
+      -DOTB_USE_PQXX=OFF
       -DBUILD_SHARED_LIBS=ON
       -DOTB_WRAP_QT=ON
     ]
@@ -77,8 +69,6 @@ class Orfeo < Formula
     args << '-DOTB_USE_PATENTED=' + ((build.include? 'patented') ? 'ON' : 'OFF')
     args << '-DOTB_WRAP_PYTHON=OFF' if build.without? 'python'
     args << "-DUSE_FFTWF=" + ((build.with? "fftw") ? "ON" : "OFF")
-    args << "-DOTB_USE_GETTEXT=" + ((build.with? "gettext") ? "ON" : "OFF")
-    args << "-DOTB_USE_PQXX=" + ((build.with? "libpqxx") ? "ON" : "OFF")
     args << "-DOTB_USE_OPENCV=" + ((build.with? "opencv") ? "ON" : "OFF")
 
     mkdir 'build' do
