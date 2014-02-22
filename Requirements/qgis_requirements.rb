d1d6c35c4c9660b4cfc4d4adbe491d8ada0e1d36
Larry Shaffer <larrys@dakotacarto.com>
2014-02-22 14:12:03 -0700

    requirements: move all shared for qgis to single include

---
+++ Requirements/qgis_requirements.rb
@@ -0,0 +1,68 @@
+class UnlinkedQGIS < Requirement
+  fatal true
+
+  def qgis_list
+    # default: Homebrew/science's
+    %W[qgis]
+  end
+
+  def no_linked_qgis
+    qgis_list.each {|f| return false, f if Formula.factory(f).linked_keg.exist?}
+    return true, ""
+  end
+
+  satisfy(:build_env => false) { no_linked_qgis[0] }
+
+  def message
+    qgis_f = no_linked_qgis[1]
+    <<-EOS.undent
+
+      Another QGIS formula is linked: #{qgis_f}.
+      Do `brew unlink #{qgis_f}` then try installing this formula again.
+      You can leave #{qgis_f} unlinked and installed as keg-only;
+      however, launching it as keg-only may cause issues or crashes, since it
+      will be now be referencing Python modules of the newly linked QGIS.
+
+      Issue can be overcome by setting PYTHONPATH, e.g. in the app's Options.
+
+    EOS
+  end
+end
+
+class SipBinary < Requirement
+  fatal true
+  #noinspection RubyResolve
+  default_formula 'sip'
+  satisfy(:build_env => false) { which 'sip' }
+
+  def message
+    <<-EOS.undent
+      The `sip` binary is missing. It is needed to generate the Python bindings for QGIS.
+      Ensure `sip` formula is installed and linked.
+
+    EOS
+  end
+end
+
+class PyQtConfig < Requirement
+  fatal true
+  #noinspection RubyResolve
+  default_formula 'pyqt'
+  # pyqtconfig is not created with PyQt4 >= 4.10.x when using configure-ng.
+  # Homebrew's `pyqt` formula corrects this. Remains an issue until QGIS project
+  # adjusts FindPyQt.py in CMake setup to work with configure-ng.
+  satisfy(:build_env => false) { quiet_system 'python', '-c', 'from PyQt4 import pyqtconfig' }
+
+  def message
+    <<-EOS.undent
+      Python could not import the PyQt4.pyqtconfig module. This will cause the QGIS build to fail.
+      The most common reason for this failure is that the PYTHONPATH needs to be adjusted.
+      The `pyqt` caveats explain this adjustment and may be reviewed using:
+
+          brew info pyqt
+
+      Ensure `pyqt` formula is installed and linked, and that it includes the `pyqtconfig` module.
+
+    EOS
+  end
+end
