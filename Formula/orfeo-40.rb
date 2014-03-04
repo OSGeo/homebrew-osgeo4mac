5d9ec7fa33ab901ad6f8ab28f1020cc36421d217
Larry Shaffer <larrys@dakotacarto.com>
2014-03-03 21:35:38 -0700

    orfeo-40: release candidate testing formula

---
+++ Formula/orfeo-40.rb
@@ -0,0 +1,68 @@
+require 'formula'
+
+class Orfeo40 < Formula
+  homepage 'http://www.orfeo-toolbox.org/otb/'
+  url 'http://www.orfeo-toolbox.org/packages/OTB-4.0-rc1.tgz'
+  sha1 'bb45afa2483f968b89fb54e542d2a4a79f186637'
+
+  option "with-external-boost", "Build with brewed Boost"
+
+  depends_on 'cmake' => :build
+  depends_on "boost" if build.with? "external-boost"
+  depends_on :python => :optional
+  depends_on 'gdal'
+  depends_on 'qt'
+  depends_on "muparser"
+  depends_on "libkml"
+  depends_on "minizip"
+  depends_on "tinyxml"
+  depends_on "fftw" => :optional # restricts built binaries to GPL license
+  depends_on "opencv" => :optional
+  # external libs that may work in next release:
+  #depends_on "open-scene-graph" # (for libOpenThreads, now internal to osg)
+
+  option 'examples', 'Compile and install various examples'
+  option 'java', 'Enable Java support'
+  option 'patented', 'Enable patented algorithms'
+
+  conflicts_with "orfeo", "orfeo is in main tap"
+
+  resource "geoid" do
+    # geoid to use in elevation calculations, if no DEM defined or avialable
+    url "http://hg.orfeo-toolbox.org/OTB-Data/raw-file/dec1ce83a5f3/Input/DEM/egm96.grd"
+    sha1 "034ae375ff41b87d5e964f280fde0438c8fc8983"
+    version "4.0.0"
+  end
+
+  def patches
+  end
+
+  def install
+    (libexec/"default_geoid").install resource("geoid")
+
+    args = std_cmake_args + %W[
+      -DBUILD_APPLICATIONS=ON
+      -DOTB_USE_EXTERNAL_ITK=OFF
+      -DBUILD_TESTING=OFF
+      -DOTB_USE_EXTERNAL_OPENTHREADS=OFF
+      -DBUILD_SHARED_LIBS=ON
+      -DOTB_WRAP_QT=ON
+    ]
+
+    args << "-DOTB_USE_EXTERNAL_BOOST=" + ((build.with? "external-boost") ? 'ON' : 'OFF')
+    args << '-DBUILD_EXAMPLES=' + ((build.include? 'examples') ? 'ON' : 'OFF')
+    args << '-DOTB_WRAP_JAVA=' + ((build.include? 'java') ? 'ON' : 'OFF')
+    args << '-DOTB_USE_PATENTED=' + ((build.include? 'patented') ? 'ON' : 'OFF')
+    args << '-DOTB_WRAP_PYTHON=OFF' if build.without? 'python'
+    args << "-DUSE_FFTWF=" + ((build.with? "fftw") ? "ON" : "OFF")
+    args << "-DOTB_USE_OPENCV=" + ((build.with? "opencv") ? "ON" : "OFF")
+
+    mkdir 'build' do
+      system 'cmake', '..', *args
+      #system "bbedit", "CMakeCache.txt"
+      #raise
+      system 'make'
+      system 'make install'
+    end
+  end
+end
