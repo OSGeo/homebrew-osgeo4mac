27fe9b661b1ae4843bac94d70e8e6a149484edb4
Larry Shaffer <larrys@dakotacarto.com>
2014-05-08 18:44:02 -0600

    orfeo-32: archive to older version and update syntax

--- Formula/orfeo-32.rb
+++ Formula/orfeo-32.rb
@@ -1,11 +1,13 @@
 require 'formula'
 
-class Orfeo < Formula
+class Orfeo32 < Formula
   homepage 'http://www.orfeo-toolbox.org/otb/'
-  url 'http://downloads.sourceforge.net/project/orfeo-toolbox/OTB/OTB-3.20/OTB-3.20.0.tgz'
+  url 'https://downloads.sourceforge.net/project/orfeo-toolbox/OTB/OTB-3.20/OTB-3.20.0.tgz'
   sha1 '2af5b4eb857d0f1ecb1fd1107c6879f9d79dd0fc'
 
   option "with-external-boost", "Build with brewed Boost"
+
+  keg_only "Older version; orfeo is in main tap and installs same components"
 
   depends_on 'cmake' => :build
   depends_on "boost" if build.with? "external-boost"
@@ -20,8 +22,6 @@ class Orfeo < Formula
   depends_on "tinyxml"
   depends_on "fftw" => :optional # restricts built binaries to GPL license
   depends_on "opencv" => :optional
-  # external libs that may work in next release:
-  #depends_on "open-scene-graph" # (for libOpenThreads, now internal to osg)
 
   option 'examples', 'Compile and install various examples'
   option 'java', 'Enable Java support'
@@ -34,17 +34,21 @@ class Orfeo < Formula
     version "3.20.0"
   end
 
-  def patches
-    p = []
+  patch do
     # Fix some CMake modules
     #   Ensure external liblas_c and liblas are found on Mac
     #   Ensure external libOpenThreads is not used unless specified; otherwise it
     #   may use open-scene-graph's newer lib, which fails when linking with orfeo
-    p << "https://gist.github.com/dakcarto/8890690/raw/ab16c6cbaf7d214b786583f456d8839585a04fa7/orfeo-cmake-fixes.diff"
-    if ENV.compiler == :clang and MacOS.version >= :mavericks
+    url "https://gist.github.com/dakcarto/8890690/raw/ab16c6cbaf7d214b786583f456d8839585a04fa7/orfeo-cmake-fixes.diff"
+    sha1 "b8c0a835fbe0ad7dd209ef5f949516927f949c88"
+  end
+
+  patch do
+    if MacOS.version >= :mavericks
       # Fix for forward declaration (and other issues) with clang and libc++
       #   See https://groups.google.com/forum/#!topic/otb-users/dRjdIxlDWfs
-      p << "https://gist.github.com/dakcarto/8930966/raw/331cca49a8e8dd579c4c19c865b17090a7433cd6/orfeo-libc-fixes.diff"
+      url "https://gist.github.com/dakcarto/8930966/raw/331cca49a8e8dd579c4c19c865b17090a7433cd6/orfeo-libc-fixes.diff"
+      sha1 "5ca9e21d272665da83cdcb37a280144cc42ab62e"
     end
   end
 
