6bb753809c372543ced6ee1f5efefb9874628475
Larry Shaffer <larrys@dakotacarto.com>
2014-05-08 18:39:21 -0600

    gdal-oracle: update to gdal 1.11

--- Formula/gdal-oracle.rb
+++ Formula/gdal-oracle.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GdalOracle < Formula
   homepage "http://www.gdal.org/ogr/drv_oci.html"
-  url "http://download.osgeo.org/gdal/1.10.1/gdal-1.10.1.tar.gz"
-  sha1 "b4df76e2c0854625d2bedce70cc1eaf4205594ae"
+  url 'http://download.osgeo.org/gdal/1.11.0/gdal-1.11.0.tar.gz'
+  sha1 '25efd2bffdea2e841377ca8c1fd49d89d02ac87e'
 
   depends_on "oracle-client-sdk"
   depends_on "gdal"
