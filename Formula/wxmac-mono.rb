564ee6781987ff45432871bf5aae136976dd8d1b
Larry Shaffer <larrys@dakotacarto.com>
2014-07-03 19:59:56 -0600

    wxmac-mono: new formula; monolithic build

---
+++ Formula/wxmac-mono.rb
@@ -0,0 +1,58 @@
+require 'formula'
+
+class WxmacMono < Formula
+  homepage "http://www.wxwidgets.org"
+  url "https://downloads.sourceforge.net/project/wxwindows/3.0.1/wxWidgets-3.0.1.tar.bz2"
+  sha1 "73e58521d6871c9f4d1e7974c6e3a81629fddcf8"
+
+  keg_only "because wxmac (non-monolithic) is in main tap"
+
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+
+  def install
+    # need to set with-macosx-version-min to avoid configure defaulting to 10.5
+    # need to enable universal binary build in order to build all x86_64
+    # FIXME I don't believe this is the whole story, surely this can be fixed
+    # without building universal for users who don't need it. - Jack
+    # headers need to specify x86_64 and i386 or will try to build for ppc arch
+    # and fail on newer OSes
+    # https://trac.macports.org/browser/trunk/dports/graphics/wxWidgets30/Portfile#L80
+    ENV.universal_binary
+    args = [
+      "--disable-debug",
+      "--prefix=#{prefix}",
+      "--enable-shared",
+      "--enable-unicode",
+      "--enable-std_string",
+      "--enable-display",
+      "--with-opengl",
+      "--with-osx_cocoa",
+      "--with-libjpeg",
+      "--with-libtiff",
+      # Otherwise, even in superenv, the internal libtiff can pick
+      # up on a nonuniversal xz and fail
+      # https://github.com/Homebrew/homebrew/issues/22732
+      "--without-liblzma",
+      "--with-libpng",
+      "--with-zlib",
+      "--enable-dnd",
+      "--enable-clipboard",
+      "--enable-webkit",
+      "--enable-svg",
+      "--enable-mediactrl",
+      "--enable-graphics_ctx",
+      "--enable-controls",
+      "--enable-dataviewctrl",
+      "--with-expat",
+      "--with-macosx-version-min=#{MacOS.version}",
+      "--enable-universal_binary=#{Hardware::CPU.universal_archs.join(',')}",
+      "--disable-precomp-headers",
+      "--enable-monolithic"
+    ]
+
+    system "./configure", *args
+    system "make install"
+  end
+end
