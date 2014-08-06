e43bcde72320ba2247ea16e800a13234afc33e45
Larry Shaffer <larrys@dakotacarto.com>
2014-08-06 12:42:55 -0600

    qwtpolar: now in main Homebrew tap

--- boneyard/qwtpolar.rb
+++ boneyard/qwtpolar.rb
@@ -2,49 +2,44 @@ require "formula"
 
 class Qwtpolar < Formula
   homepage "http://qwtpolar.sourceforge.net/"
-  url "http://downloads.sf.net/project/qwtpolar/qwtpolar/1.0.1/qwtpolar-1.0.1.tar.bz2"
-  sha1 "8cff25b444037429321a0fdecacc102cc044f695"
+  url "http://downloads.sf.net/project/qwtpolar/qwtpolar/1.1.0/qwtpolar-1.1.0.tar.bz2"
+  sha1 "94d5f897e75e37f32c910e3bdf2a1ffbaaf76621"
 
-  devel do
-    url "http://downloads.sf.net/project/qwtpolar/qwtpolar-beta/1.1.0-rc1/qwtpolar-1.1.0-rc1.tar.bz2"
-    sha1 "b71d6f462c857fd57f295ad97e87efa88b3b1ada"
-  end
-
-  #option "enable-framework", "Build as framework"
+  option "with-examples", "Install source code for example apps"
 
   depends_on "qt"
-  depends_on "osgeo/osgeo4mac/qwt"
-
-  # fix designer plugin linking, when in non-standard prefix
-  def patches
-    DATA
-  end
+  depends_on "qwt"
 
   def install
-
-    qwt = Formula.factory("qwt")
-    qwt_ver = qwt.linked_keg.realpath.basename.to_s.to_f
-    if build.stable? && qwt_ver >= 6.1
-      odie "QwtPolar 1.0.x is only compatible with Qwt <= 6.0.x. Use `--devel` build option."
-    elsif build.devel? && qwt_ver < 6.1
-      odie "QwtPolar `--devel` build requires Qwt >= 6.1.x."
+    cd "doc" do
+      doc.install "html"
+      man3.install Dir["man/man3/{q,Q}wt*"]
     end
+    rm_r "doc"
 
-    qwt_lib = qwt.opt_prefix/"lib"
+    libexec.install Dir["examples/*"] if build.with? "examples"
+
     inreplace "qwtpolarconfig.pri" do |s|
       # change_make_var won't work because there are leading spaces
-      s.gsub! /^(\s*)QWT_POLAR_INSTALL_PREFIX\s*=\s*(.*)$/, "\\1QWT_POLAR_INSTALL_PREFIX=#{prefix}"
-      s << "\n" << "QWT_POLAR_CONFIG -= QwtPolarFramework" unless build.include? "enable-framework" or build.stable?
-      # add paths to installed qwt (built as lib for 6.0.x and framework for 6.1.x)
-      if build.devel?
-        s << "\n" << "INCLUDEPATH += #{qwt_lib}/qwt.framework/Headers"
-        s << "\n" << "QMAKE_LFLAGS += -F#{qwt_lib}/"
-        s << "\n" << "LIBS += -framework qwt"
-      else
-        s << "\n" << "INCLUDEPATH += #{qwt.opt_prefix}/include"
-        s << "\n" << "QMAKE_LFLAGS += -L#{qwt_lib}/"
-        s << "\n" << "LIBS += -lqwt"
-      end
+      s.gsub! /^(\s*)QWT_POLAR_INSTALL_PREFIX\s*=\s*(.*)$/,
+              "\\1QWT_POLAR_INSTALL_PREFIX=#{prefix}"
+      # don't build examples now, since linking flawed until qwtpolar installed
+      s.sub! /\+(=\s*QwtPolarExamples)/, "-\\1"
+    end
+
+    # update designer plugin linking back to qwtpolar framework/lib
+    inreplace "designer/designer.pro" do |s|
+      s.sub! /(INSTALLS \+= target)/, "\\1\n" + <<-EOS.undent
+        macx {
+            contains(QWT_POLAR_CONFIG, QwtPolarFramework) {
+                QWTP_LIB = qwtpolar.framework/Versions/$${QWT_POLAR_VER_MAJ}/qwtpolar
+            }
+            else {
+                QWTP_LIB = libqwtpolar.$${QWT_POLAR_VER_MAJ}.dylib
+            }
+            QMAKE_POST_LINK = install_name_tool -change $${QWTP_LIB} #{opt_prefix}/lib/$${QWTP_LIB} ${DESTDIR}/$(TARGET)
+        }
+      EOS
     end
 
     args = %W[-config release -spec]
@@ -55,31 +50,15 @@ class Qwtpolar < Formula
       args << "macx-g++"
     end
 
+    ENV["QMAKEFEATURES"] = "#{Formula["qwt"].opt_prefix}/features"
     system "qmake", *args
     system "make"
     system "make", "install"
 
-    # symlink QT Designer plugin (note: not removed on formula uninstall)
-    cd Formula.factory('qt').opt_prefix/"plugins/designer" do
+    # symlink Qt Designer plugin (note: not removed on qwtpolar formula uninstall)
+    cd Formula["qt"].opt_prefix/"plugins/designer" do
       ln_sf prefix/"plugins/designer/libqwt_polar_designer_plugin.dylib", "."
     end
   end
 
 end
-
-__END__
-diff --git a/designer/designer.pro b/designer/designer.pro
-index 4bca34c..208c428 100644
---- a/designer/designer.pro
-+++ b/designer/designer.pro
-@@ -58,6 +58,10 @@ contains(QWT_POLAR_CONFIG, QwtPolarDesigner) {
-
-     target.path = $${QWT_POLAR_INSTALL_PLUGINS}
-     INSTALLS += target
-+    LIBS     = -L../lib $${LIBS}
-+    macx {
-+        QMAKE_POST_LINK = install_name_tool -change libqwtpolar.1.dylib $${QWT_POLAR_INSTALL_PREFIX}/lib/libqwtpolar.1.dylib ${DESTDIR}/$(TARGET)
-+    }
- }
- else {
-     TEMPLATE        = subdirs # do nothing
