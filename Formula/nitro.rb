8223968f3bbf44aff7de8fb96a4ae990f81f6434
Howard Butler <howard@hobu.co>
2014-09-19 11:21:46 -0500

    Upgrade to latest Nitro HEAD

--- Formula/nitro.rb
+++ Formula/nitro.rb
@@ -3,9 +3,9 @@ require "formula"
 class Nitro < Formula
   homepage "https://github.com/hobu/nitro"
   # TODO: request a tagged release
-  url "https://github.com/hobu/nitro.git", :revision => "c0c3fbba2638a68d3300191cd0a204542b78fc78"
-  version "1.0-c0c3fbb"
-  sha1 "c0c3fbba2638a68d3300191cd0a204542b78fc78"
+  url "https://github.com/hobu/nitro.git", :revision => "a3539c63128d9190fbd0043c1652d1b9397f8fcd"
+  version "1.0-a3539c6"
+  sha1 "a3539c63128d9190fbd0043c1652d1b9397f8fcd"
 
   head "https://github.com/hobu/nitro.git", :branch => "master"
 
