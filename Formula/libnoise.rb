80105e890a76dc68fc5a387b873b19b11c0102c9
Larry Shaffer <larrys@dakotacarto.com>
2014-11-21 17:25:22 -0700

    libnoise: update formula and fix #61

--- Formula/libnoise.rb
+++ Formula/libnoise.rb
@@ -1,26 +1,28 @@
-require 'formula'
+require "formula"
 
 class Libnoise < Formula
-  homepage 'https://github.com/qknight/libnoise'
-  url 'https://github.com/qknight/libnoise/archive/master.zip'
-  sha1 '9b5572e4bff04a12aa97b044607e65f118948124'
+  homepage "https://github.com/qknight/libnoise"
+  url "https://github.com/qknight/libnoise.git",
+      :revision => "ea2e5174ccbc4b30ccdb23e9685a18f3fff66596"
+  version "1.0.0-cmake"
+  revision 1
 
-  version '1.0.0-cmake'
+  option "with-docs", "Install documentation"
 
-  option 'with-docs', 'Install documentation'
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build if build.with? "docs"
 
-  depends_on 'cmake' => :build
-  depends_on 'doxygen' if build.include? 'with-docs'
 
   def install
-    inreplace 'doc/CMakeLists.txt', '/usr/share', share if build.include? 'with-docs'
+    inreplace "doc/CMakeLists.txt", "/usr/share", share if build.with? "docs"
 
     args = std_cmake_args
-    args << '-DBUILD_LIBNOISE_DOCUMENTATION=ON' if build.include? 'with-docs'
+    args << "-DBUILD_LIBNOISE_DOCUMENTATION=ON" if build.with? "docs"
 
-    mkdir 'build' do
-      system 'cmake', '..', *args
-      system 'make install'
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
     end
   end
 
