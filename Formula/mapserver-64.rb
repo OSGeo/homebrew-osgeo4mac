e8c2fbe6c8f591fb8c7db4fb998eca3655483057
Larry Shaffer <larrys@dakotacarto.com>
2014-12-07 01:03:40 -0700

    mapserver-64: convert quotes

--- Formula/mapserver-64.rb
+++ Formula/mapserver-64.rb
@@ -1,4 +1,4 @@
-require 'formula'
+require "formula"
 
 class JavaJDK < Requirement
   fatal true
@@ -6,7 +6,7 @@ class JavaJDK < Requirement
   def self.home
     [
         `/usr/libexec/java_home`.chomp!,
-        ENV['JAVA_HOME']
+        ENV["JAVA_HOME"]
     ].find { |dir| dir && File.exist?("#{dir}/bin/javac") &&
         (File.exist?("#{dir}/include" || File.exist?("#{dir}/bundle"))) }
   end
@@ -28,9 +28,9 @@ class JavaJDK < Requirement
 end
 
 class Mapserver64 < Formula
-  homepage 'http://mapserver.org/'
-  url 'http://download.osgeo.org/mapserver/mapserver-6.4.1.tar.gz'
-  sha1 'f7d2e7f44cd9a4ff5d9483d21bb71c1cc28e09ab'
+  homepage "http://mapserver.org/"
+  url "http://download.osgeo.org/mapserver/mapserver-6.4.1.tar.gz"
+  sha1 "f7d2e7f44cd9a4ff5d9483d21bb71c1cc28e09ab"
 
   bottle do
     root_url "http://qgis.dakotacarto.com/osgeo4mac/bottles"
@@ -38,54 +38,54 @@ class Mapserver64 < Formula
   end
 
   head do
-    url 'https://github.com/mapserver/mapserver.git', :branch => 'master'
-    depends_on 'harfbuzz'
-    depends_on 'v8' => :optional
+    url "https://github.com/mapserver/mapserver.git", :branch => "master"
+    depends_on "harfbuzz"
+    depends_on "v8" => :optional
   end
 
-  conflicts_with 'mapserver', :because => 'mapserver is in main tap'
+  conflicts_with "mapserver", :because => "mapserver is in main tap"
 
-  option 'without-php', 'Build PHP MapScript module'
-  option 'without-rpath', "Don't embed rpath to installed libmapserver in modules"
-  option 'without-geos', 'Build without GEOS spatial operations support'
-  option 'without-postgresql', 'Build without PostgreSQL data source support'
-  option 'without-xml-mapfile', 'Build with native XML mapfile support'
-  option 'with-java', 'Build Java MapScript module'
-  option 'with-gd', 'Build with GD support (deprecated)' unless build.head?
-  option 'with-librsvg', 'Build with SVG symbology support'
-  option 'with-docs', 'Download and generate HTML documentation'
+  option "without-php", "Build PHP MapScript module"
+  option "without-rpath", "Don't embed rpath to installed libmapserver in modules"
+  option "without-geos", "Build without GEOS spatial operations support"
+  option "without-postgresql", "Build without PostgreSQL data source support"
+  option "without-xml-mapfile", "Build with native XML mapfile support"
+  option "with-java", "Build Java MapScript module"
+  option "with-gd", "Build with GD support (deprecated)" unless build.head?
+  option "with-librsvg", "Build with SVG symbology support"
+  option "with-docs", "Download and generate HTML documentation"
   option "with-unit-tests", "Download and install full unit test suite"
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
   depends_on :freetype
   depends_on :libpng
   depends_on :python
-  depends_on 'swig' => :build
-  depends_on JavaJDK if build.with? 'java'
-  depends_on 'giflib'
-  depends_on 'gd' => :optional unless build.head?
-  depends_on 'proj'
-  depends_on 'geos' => :recommended
-  depends_on 'gdal'
+  depends_on "swig" => :build
+  depends_on JavaJDK if build.with? "java"
+  depends_on "giflib"
+  depends_on "gd" => :optional unless build.head?
+  depends_on "proj"
+  depends_on "geos" => :recommended
+  depends_on "gdal"
   depends_on :postgresql => :recommended
   depends_on :mysql => :optional
-  depends_on 'fcgi' => :recommended
-  depends_on 'cairo' => :recommended
-  depends_on 'libxml2' if build.with? 'xml-mapfile' or MacOS.version < :mountain_lion
-  depends_on 'libxslt' if build.with? 'xml-mapfile'
-  depends_on 'librsvg' => :optional
-  depends_on 'fribidi'
-  depends_on :python => %w[sphinx] if build.with? 'docs'
+  depends_on "fcgi" => :recommended
+  depends_on "cairo" => :recommended
+  depends_on "libxml2" if build.with? "xml-mapfile" or MacOS.version < :mountain_lion
+  depends_on "libxslt" if build.with? "xml-mapfile"
+  depends_on "librsvg" => :optional
+  depends_on "fribidi"
+  depends_on :python => %w[sphinx] if build.with? "docs"
 
   resource "sphinx" do
     url "https://pypi.python.org/packages/source/S/Sphinx/Sphinx-1.2.2.tar.gz"
     sha1 "9e424b03fe1f68e0326f3905738adcf27782f677"
   end
 
-  resource 'docs' do
+  resource "docs" do
     # NOTE: seems to be no tagged releases for `docs`, just active branches
-    url 'https://github.com/mapserver/docs.git', :branch => 'branch-6-4'
-    version '6.4'
+    url "https://github.com/mapserver/docs.git", :branch => "branch-6-4"
+    version "6.4"
   end
 
   resource "unittests" do
@@ -95,12 +95,12 @@ class Mapserver64 < Formula
   end
 
   def png_prefix
-    png = Formula['libpng']
+    png = Formula["libpng"]
     (png.installed? or MacOS.version >= :mountain_lion) ? png.opt_prefix : MacOS::X11.prefix
   end
 
   def freetype_prefix
-    ft = Formula['freetype']
+    ft = Formula["freetype"]
     (ft.installed? or MacOS.version >= :mountain_lion) ? ft.opt_prefix : MacOS::X11.prefix
   end
 
@@ -108,14 +108,14 @@ class Mapserver64 < Formula
     # install unit tests
     (prefix/"msautotest").install resource("unittests") if build.with? "unit-tests"
 
-    ENV.prepend_path 'CMAKE_PREFIX_PATH', freetype_prefix
-    ENV.prepend_path 'CMAKE_PREFIX_PATH', png_prefix
+    ENV.prepend_path "CMAKE_PREFIX_PATH", freetype_prefix
+    ENV.prepend_path "CMAKE_PREFIX_PATH", png_prefix
 
     args = std_cmake_args
     if MacOS.prefer_64_bit?
       args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.arch_64_bit}"
     else
-      args << '-DCMAKE_OSX_ARCHITECTURES=i386'
+      args << "-DCMAKE_OSX_ARCHITECTURES=i386"
     end
 
     # defaults different than CMakeLists.txt (they don't incur extra dependencies)
@@ -127,81 +127,81 @@ class Mapserver64 < Formula
       -DWITH_SOS=ON
     ]
 
-    args << '-DWITH_XMLMAPFILE=ON' if build.with? 'xml-mapfile'
-    args << '-DWITH_MYSQL=ON' if build.with? 'mysql'
-    args << '-DWITH_GD=ON' if build.with? 'gd' && !build.head?
-    args << '-DWITH_RSVG=ON' if build.with? 'librsvg'
+    args << "-DWITH_XMLMAPFILE=ON" if build.with? "xml-mapfile"
+    args << "-DWITH_MYSQL=ON" if build.with? "mysql"
+    args << "-DWITH_GD=ON" if build.with? "gd" && !build.head?
+    args << "-DWITH_RSVG=ON" if build.with? "librsvg"
 
-    mapscr_dir = prefix/'mapscript'
+    mapscr_dir = prefix/"mapscript"
     mapscr_dir.mkpath
-    rpath = %Q{-Wl,-rpath,"#{opt_prefix/'lib'}"}
-    use_rpath = build.with? 'rpath'
-    cd 'mapscript' do
-      args << '-DWITH_PYTHON=ON'
-      inreplace 'python/CMakeLists.txt' do |s|
-        s.gsub! '${PYTHON_SITE_PACKAGES}', %Q{"#{lib/which_python/'site-packages'}"}
-        s.sub! '${MAPSERVER_LIBMAPSERVER}',
+    rpath = %Q{-Wl,-rpath,"#{opt_prefix/"lib"}"}
+    use_rpath = build.with? "rpath"
+    cd "mapscript" do
+      args << "-DWITH_PYTHON=ON"
+      inreplace "python/CMakeLists.txt" do |s|
+        s.gsub! "${PYTHON_SITE_PACKAGES}", %Q{"#{lib/which_python/"site-packages"}"}
+        s.sub! "${MAPSERVER_LIBMAPSERVER}",
                "#{rpath} ${MAPSERVER_LIBMAPSERVER}" if use_rpath
       end
 
-      # override language extension install locations, e.g. install to prefix/'mapscript/lang'
-      args << '-DWITH_RUBY=ON'
-      (mapscr_dir/'ruby').mkpath
-      inreplace 'ruby/CMakeLists.txt' do |s|
-        s.gsub! '${RUBY_SITEARCHDIR}', %Q{"#{mapscr_dir}/ruby"}
-        s.sub! '${MAPSERVER_LIBMAPSERVER}',
+      # override language extension install locations, e.g. install to prefix/"mapscript/lang"
+      args << "-DWITH_RUBY=ON"
+      (mapscr_dir/"ruby").mkpath
+      inreplace "ruby/CMakeLists.txt" do |s|
+        s.gsub! "${RUBY_SITEARCHDIR}", %Q{"#{mapscr_dir}/ruby"}
+        s.sub! "${MAPSERVER_LIBMAPSERVER}",
                "#{rpath} ${MAPSERVER_LIBMAPSERVER}" if use_rpath
       end
 
-      if build.with? 'php'
-        args << '-DWITH_PHP=ON'
-        (mapscr_dir/'php').mkpath
-        inreplace 'php/CMakeLists.txt' do |s|
-          s.gsub! '${PHP5_EXTENSION_DIR}', %Q{"#{mapscr_dir}/php"}
-          s.sub! '${MAPSERVER_LIBMAPSERVER}',
+      if build.with? "php"
+        args << "-DWITH_PHP=ON"
+        (mapscr_dir/"php").mkpath
+        inreplace "php/CMakeLists.txt" do |s|
+          s.gsub! "${PHP5_EXTENSION_DIR}", %Q{"#{mapscr_dir}/php"}
+          s.sub! "${MAPSERVER_LIBMAPSERVER}",
                  "#{rpath} ${MAPSERVER_LIBMAPSERVER}" if use_rpath
         end
       end
 
-      args << '-DWITH_PERL=ON'
-      (mapscr_dir/'perl').mkpath
+      args << "-DWITH_PERL=ON"
+      (mapscr_dir/"perl").mkpath
       args << "-DCUSTOM_PERL_SITE_ARCH_DIR=#{mapscr_dir}/perl"
-      inreplace 'perl/CMakeLists.txt', '${MAPSERVER_LIBMAPSERVER}',
+      inreplace "perl/CMakeLists.txt", "${MAPSERVER_LIBMAPSERVER}",
                 "#{rpath} ${MAPSERVER_LIBMAPSERVER}" if use_rpath
 
-      if build.with? 'java'
-        args << '-DWITH_JAVA=ON'
-        ENV['JAVA_HOME'] = JavaJDK.home
-        (mapscr_dir/'java').mkpath
-        inreplace 'java/CMakeLists.txt' do |s|
-          s.gsub! 'DESTINATION ${CMAKE_INSTALL_LIBDIR}',
+      if build.with? "java"
+        args << "-DWITH_JAVA=ON"
+        ENV["JAVA_HOME"] = JavaJDK.home
+        (mapscr_dir/"java").mkpath
+        inreplace "java/CMakeLists.txt" do |s|
+          s.gsub! "DESTINATION ${CMAKE_INSTALL_LIBDIR}",
                   %Q|${CMAKE_CURRENT_BINARY_DIR}/mapscript.jar DESTINATION "#{mapscr_dir}/java"|
-          s.sub! '${MAPSERVER_LIBMAPSERVER}',
+          s.sub! "${MAPSERVER_LIBMAPSERVER}",
                  "#{rpath} ${MAPSERVER_LIBMAPSERVER}" if use_rpath
         end
       end
     end
 
-    mkdir 'build' do
-      system 'cmake', '..', *args
-      system 'make', 'install'
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
     end
 
     # install devel headers
-    (include/'mapserver').install Dir['*.h']
+    (include/"mapserver").install Dir["*.h"]
 
-    prefix.install 'tests'
+    prefix.install "tests"
     (mapscr_dir/"python").install "mapscript/python/tests"
-    cd 'mapscript' do
+    cd "mapscript" do
       %w[python ruby perl].each {|x|(mapscr_dir/"#{x}").install "#{x}/examples"}
-      (mapscr_dir/'php').install 'php/examples' if build.with? 'php'
-      (mapscr_dir/'java').install 'java/examples' if build.with? 'java'
+      (mapscr_dir/"php").install "php/examples" if build.with? "php"
+      (mapscr_dir/"java").install "java/examples" if build.with? "java"
     end
 
     # write install instructions for modules
-    s = ''
-    mapscr_opt_dir = opt_prefix/'mapscript'
-    if build.with? 'php'
+    s = ""
+    mapscr_opt_dir = opt_prefix/"mapscript"
+    if build.with? "php"
       s += <<-EOS.undent
         Using the built PHP module:
           * Add the following line to php.ini:
@@ -212,18 +212,18 @@ class Mapserver64 < Formula
       EOS
     end
     %w[ruby perl java].each do |m|
-      if m != 'java' or build.with? m
+      if m != "java" or build.with? m
         cmd = []
         case m
-        when 'ruby'
+        when "ruby"
           ruby_site = %x[ruby -r rbconfig -e 'puts RbConfig::CONFIG["sitearchdir"]'].chomp
           cmd << "sudo cp -f mapscript.bundle #{ruby_site}/"
-        when 'perl'
+        when "perl"
           perl_site = %x[perl -MConfig -e 'print $Config{"sitearch"};'].chomp
           cmd << "sudo cp -f mapscript.pm #{perl_site}/"
           cmd << "sudo cp -fR auto/mapscript #{perl_site}/auto/"
-        when 'java'
-          cmd << 'sudo cp -f libjavamapscript.jnilib mapscript.jar /Library/Java/Extensions/'
+        when "java"
+          cmd << "sudo cp -f libjavamapscript.jnilib mapscript.jar /Library/Java/Extensions/"
         else
         end
         s += <<-EOS.undent
@@ -234,9 +234,9 @@ class Mapserver64 < Formula
         EOS
       end
     end
-    (mapscr_dir/'Install_Modules.txt').write s
+    (mapscr_dir/"Install_Modules.txt").write s
 
-    if build.with? 'docs'
+    if build.with? "docs"
       unless which("sphinx-build")
         # vendor a local sphinx install
         sphinx_site = libexec/"lib/python2.7/site-packages"
@@ -244,11 +244,11 @@ class Mapserver64 < Formula
         resource("sphinx").stage {quiet_system "python2.7", "setup.py", "install", "--prefix=#{libexec}"}
         ENV.prepend_path "PATH", libexec/"bin"
       end
-      resource('docs').stage do
+      resource("docs").stage do
         # just build the en docs
         inreplace "Makefile", "$(TRANSLATIONS_I18N) $(TRANSLATIONS_STATIC)", ""
-        system 'make', 'html'
-        doc.install 'build/html' => 'html'
+        system "make", "html"
+        doc.install "build/html" => "html"
       end
     end
   end
@@ -263,19 +263,19 @@ class Mapserver64 < Formula
   end
 
   test do
-    mapscr_opt_dir = opt_prefix/'mapscript'
-    system "#{bin}/mapserv", '-v'
-    system 'python', '-c', '"import mapscript"'
-    system 'ruby', '-e', "\"require '#{mapscr_opt_dir}/ruby/mapscript'\""
-    system 'perl', "-I#{mapscr_opt_dir}/perl", '-e', '"use mapscript;"'
+    mapscr_opt_dir = opt_prefix/"mapscript"
+    system "#{bin}/mapserv", "-v"
+    system "python", "-c", '"import mapscript"'
+    system "ruby", "-e", "\"require '#{mapscr_opt_dir}/ruby/mapscript'\""
+    system "perl", "-I#{mapscr_opt_dir}/perl", "-e", '"use mapscript;"'
 
     cd "#{mapscr_opt_dir}/java/examples" do
       system "#{JavaJDK.home}/bin/javac",
-             '-classpath', '../', '-Djava.ext.dirs=../', 'RFC24.java'
+             "-classpath", "../", "-Djava.ext.dirs=../", "RFC24.java"
       system "#{JavaJDK.home}/bin/java",
-             '-classpath', '../', '-Djava.library.path=../', '-Djava.ext.dirs=../',
-             'RFC24', '../../../tests/test.map'
-    end if build.with? 'java'
+             "-classpath", "../", "-Djava.library.path=../", "-Djava.ext.dirs=../",
+             "RFC24", "../../../tests/test.map"
+    end if build.with? "java"
   end
 
   private
