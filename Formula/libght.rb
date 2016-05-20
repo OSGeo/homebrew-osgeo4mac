b51a3b5685ca1a683da144dad48e0f8268789296
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-05-20 16:34:37 -0600

    libght: 0.1.1; with-test still segfaults, but test block passes

--- Formula/libght.rb
+++ Formula/libght.rb
@@ -1,11 +1,16 @@
 class Libght < Formula
+  desc "GeoHashTree for storing and accessing multi-dimensional point clouds"
   homepage "https://github.com/pramsey/libght/"
-  url "https://github.com/pramsey/libght/archive/v0.1.0.tar.gz"
-  sha1 "19104cdba21fabb8d5fad847af1a8e8bcde40b6a"
+  url "https://github.com/pramsey/libght.git",
+      :branch => "master",
+      :revision => "e323c506b4180bb6de825c5d637f21f569da4cb4"
+  version "0.1.1"
 
   head "https://github.com/pramsey/libght.git", :branch => "master"
 
-  option "with-tests", "Run unit tests after build, prior to install"
+  option "with-test", "Run unit tests after build, prior to install"
+
+  deprecated_option "with-tests" => "with-test"
 
   depends_on "cmake" => :build
   depends_on "proj"
@@ -17,12 +22,12 @@ class Libght < Formula
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make"
-      puts %x(test/cu_tester) if build.with? "tests"
+      puts `test/cu_tester` if build.with? "test"
       system "make", "install"
     end
   end
 
   test do
-    assert_match "version #{version.to_s[0,3]}", %x(#{bin}/"las2ght")
+    assert_match "version #{version.to_s[0, 3]}", `#{bin}/"las2ght"`
   end
 end
