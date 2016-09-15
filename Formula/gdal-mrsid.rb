0da0643bf0a4813efb4897e477e791eded28c45e
Bruce Steedman <MatzFan@users.noreply.github.com>
2016-09-15 17:02:25 +0100

    Update gdal-mrsid.rb sha256
    re #148

--- Formula/gdal-mrsid.rb
+++ Formula/gdal-mrsid.rb
@@ -1,7 +1,7 @@
 class GdalMrsid < Formula
   homepage "http://www.gdal.org/frmt_mrsid.html"
   url "http://download.osgeo.org/gdal/1.11.0/gdal-1.11.0.tar.gz"
-  sha1 "25efd2bffdea2e841377ca8c1fd49d89d02ac87e"
+  sha256 "989db33ff411e2c888348e71edec5ad06c74ed68781ebfbc4e85179b9d65aafe"
 
   depends_on "mrsid-sdk"
   depends_on "gdal"
