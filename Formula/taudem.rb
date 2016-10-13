8f9281d0727712e85245db365341d2660383b9f2
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-10-12 21:10:15 -0600

    taudem: 5.3.6-dev; fix sha1, dload, cmake and audit

--- Formula/taudem.rb
+++ Formula/taudem.rb
@@ -1,54 +1,35 @@
 class Taudem < Formula
+  desc "Terrain Analysis Using Digital Elevation Models for hydrology"
   homepage "http://hydrology.usu.edu/taudem/taudem5/"
-  url "http://hydrology.usu.edu/taudem/taudem5/TauDEM5PCsrc_511.zip"
-  sha1 "7d357abcdc2bb4f28134a1a95f54b20426d6edc1"
+  url "https://github.com/dtarb/TauDEM/archive/a0335e826d579926013e2d1c33c53d413b7c04b3.tar.gz"
+  version "5.3.6-dev"
+  sha256 "8b82f5162af6aaa5dcc0d56d23f6a392cfe5463b317ae5286286dba5554160f3"
 
-  bottle do
-    root_url "http://qgis.dakotacarto.com/osgeo4mac/bottles"
-    cellar :any
-    sha1 "a810d7027dbe24ab04887f8d2f266a259882df24" => :mavericks
-  end
+  # bottle do
+  #   root_url "http://qgis.dakotacarto.com/osgeo4mac/bottles"
+  #   cellar :any
+  #   sha256 "" => :mavericks
+  # end
 
-  devel do
-    # multi-file (directory) support
-    url "https://github.com/dtarb/TauDEM.git", :branch => "master"
-    version "5.2"
-  end
+  head "https://github.com/dtarb/TauDEM.git", :branch => "master"
+
+  depends_on "cmake" => :build
+  depends_on :mpi => [:cc, :cxx]
+  depends_on "gdal"
 
   resource "logan" do
     url "http://hydrology.usu.edu/taudem/taudem5/LoganDemo.zip"
-    sha1 "12177f13e6d654b04be1b56713c8fdd6f96360d8"
-    version "5.1.1"
+    sha256 "3340f75a30d3043e7ad09b7a7324fa71374811b22fa913ad577840499a7dab83"
+    version "5.3.5"
   end
 
-  depends_on "cmake" => :build
-  depends_on :mpi => [:cc, :cx]
-
   def install
-
-    inreplace %W[commonLib.h linearpart.h] do |s|
-      s.gsub! '"mpi.h"', "<mpi.h>"
-    end
-
-    inreplace "Node.cpp",'"stdlib.h"', "<stdlib.h>"
-    inreplace "PeukerDouglas.cpp", '"ctime"', "<ctime>"
-
-    inreplace "tiffTest.cpp" do |s|
-      s.gsub! '"mpi.h"', "<mpi.h>"
-      s.gsub! '"stdint.h"', "<stdint.h>"
-    end
-
     args = std_cmake_args
-    mpi = Formula["open-mpi"]
-    args << "-DCMAKE_CXX_FLAGS=-I#{mpi.opt_prefix}/include"
-    args << "-DCMAKE_EXE_LINKER_FLAGS=-L#{mpi.opt_prefix}/lib -lmpi -lmpi_cxx"
-
-    cd "src" if build.devel?
-    mkdir "build" do
-      system "cmake", "..", *args
+    cd "src" do
+      system "cmake", ".", *args
+      system "make"
       system "make", "install"
     end
-
   end
 
   test do
