b42a24d1492c2ec61ca51a877869c9a1ab9da23d
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-10-12 20:17:53 -0600

    pointcloud: 1.1.0-dev; fix sha1, dload and install prefix

--- Formula/pointcloud.rb
+++ Formula/pointcloud.rb
@@ -1,27 +1,42 @@
 class Pointcloud < Formula
-  homepage "https://github.com/pramsey/pointcloud"
-  # functioning CMake build and fixes for Mountain Lion (not in 0.1.0 release)
-  url "https://github.com/pramsey/pointcloud.git",
-      :revision => "4c4bb5d394b904d3cd4e111c098dea1949b9ec90"
-  version "0.2.0-4c4bb5d"
-  sha1 "4c4bb5d394b904d3cd4e111c098dea1949b9ec90"
+  desc "PostgreSQL extension for storing point cloud (LIDAR) data"
+  homepage "https://github.com/pgpointcloud/pointcloud"
+  url "https://github.com/pgpointcloud/pointcloud/archive/3929653f51296f5dd5fe5997c8c4c5d46419cb50.tar.gz"
+  version "1.1.0-dev"
+  sha256 "d3190fb662912c1d889343a3cd221bb3995c9bbc69156fa184c5f4712b7b26b5"
 
-  head "https://github.com/pramsey/pointcloud.git", :branch => "master"
+  head "https://github.com/pgpointcloud/pointcloud.git", :branch => "master"
 
-  option "with-tests", "Run unit tests after build, prior to install"
+  option "with-test", "Run unit tests after build, prior to install"
+
+  deprecated_option "with-tests" => "with-test"
 
   depends_on "cmake" => :build
   depends_on :postgresql
   depends_on "libght"
-  depends_on "cunit" if build.with? "tests"
+  depends_on "libxml2"
+  # depends on "lazperf" => :optional
+  depends_on "cunit" if build.with? "test"
 
   def install
-    ENV.libxml2
+    mkdir lib/"postgresql"
+    mkdir_p pkgshare/"postgresql/extension/"
+    inreplace "pgsql/CMakeLists.txt", "${PGSQL_PKGLIBDIR}", "#{lib}/postgresql"
+    inreplace "pgsql/CMakeLists.txt", "${PGSQL_SHAREDIR}", "#{share}/postgresql"
+    inreplace "pgsql_postgis/CMakeLists.txt", "${PGSQL_SHAREDIR}", "#{share}/postgresql"
+
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make"
-      puts %x(lib/cunit/cu_tester) if build.with? "tests"
+      # system "/usr/local/bin/bbedit", "CMakeCache.txt"
+      # raise
+      # TODO: this fails with Segmentation fault: 11
+      # puts `lib/cunit/cu_tester` if build.with? "test"
       system "make", "install"
     end
+  end
+
+  test do
+    system "True"
   end
 end
