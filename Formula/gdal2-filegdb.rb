84018c536a8df5115f6c18c988f6a0a8417d5213
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-10-15 17:40:05 -0600

    gdal2-filegdb: 2.1.1; new formula for ESRI FileGDB plugin

---
+++ Formula/gdal2-filegdb.rb
@@ -0,0 +1,64 @@
+class Gdal2Filegdb < Formula
+  desc "GDAL/OGR 2.x plugin for ESRI FileGDB driver"
+  homepage "http://www.gdal.org/drv_filegdb.html"
+  url "http://download.osgeo.org/gdal/2.1.1/gdal-2.1.1.tar.gz"
+  sha256 "55fc6ffbe76e9d2e7e6cf637010e5d4bba6a966d065f40194ff798544198236b"
+
+  depends_on "filegdb-api"
+  depends_on "gdal2"
+
+  def gdal_majmin_ver
+    gdal_ver_list = Formula["gdal2"].version.to_s.split(".")
+    "#{gdal_ver_list[0]}.#{gdal_ver_list[1]}"
+  end
+
+  def install
+    filegdb_opt = Formula["filegdb-api"].opt_prefix
+
+    gdal = Formula["gdal2"]
+    gdal_plugins = lib/gdal.plugins_subdirectory.to_s
+    gdal_plugins.mkpath
+    (HOMEBREW_PREFIX/"lib/#{gdal.plugins_subdirectory}").mkpath
+
+    # cxx flags
+    args = %W[-Iport -Igcore -Iogr -Iogr/ogrsf_frmts -Iogr/ogrsf_frmts/generic
+              -Iogr/ogrsf_frmts/filegdb -I#{filegdb_opt}/include/filegdb]
+
+    # source files
+    Dir["ogr/ogrsf_frmts/filegdb/*.c*"].each do |src|
+      args.concat %W[#{src}]
+    end
+
+    # plugin dylib
+    dylib_name = "ogr_FileGDB.dylib"
+    args.concat %W[
+      -dynamiclib
+      -install_name #{opt_lib}/#{gdal.plugins_subdirectory}/#{dylib_name}
+      -current_version #{version}
+      -compatibility_version #{gdal_majmin_ver}.0
+      -o #{gdal_plugins}/#{dylib_name}
+      -undefined dynamic_lookup
+    ]
+
+    # ld flags
+    args.concat %W[-L#{filegdb_opt}/lib -lFileGDBAPI]
+
+    # build and install shared plugin
+    system ENV.cxx, *args
+  end
+
+  def caveats; <<-EOS.undent
+      This formula provides a plugin that allows GDAL or OGR to access geospatial
+      data stored in its format. In order to use the shared plugin, you may need
+      to set the following enviroment variable:
+
+        export GDAL_DRIVER_PATH=#{HOMEBREW_PREFIX}/lib/gdalplugins
+    EOS
+  end
+
+  test do
+    gdal_opt_bin = Formula["gdal2"].opt_bin
+    out = `#{gdal_opt_bin}/ogrinfo --formats`
+    assert_match "FileGDB -vector- (rw+)", out
+  end
+end
