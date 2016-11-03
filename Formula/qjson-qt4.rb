b8fc35eb9dea8466417cb669c04bde144839da11
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-11-02 20:30:38 -0600

    qjson-qt4: remove bottles

--- Formula/qjson-qt4.rb
+++ Formula/qjson-qt4.rb
@@ -6,14 +6,6 @@ class QjsonQt4 < Formula
   sha256 "cd4db5b956247c4991a9c3e95512da257cd2a6bd011357e363d02300afc814d9"
   head "https://github.com/flavio/qjson.git"
 
-  bottle do
-    cellar :any
-    revision 2
-    sha256 "4d47edace9872cd7cb267ca6e77c7f7d55400918cbdc57d45e4ee5d12087f4da" => :el_capitan
-    sha256 "a505ef97e0a0a3e05013ef1aa641af1104dedb363ff75c7d23b2c3dee5299649" => :yosemite
-    sha256 "2563c0f4d42e92136279434e9e73201b3117993d2eaa949359dab9c148d71710" => :mavericks
-  end
-
   depends_on "cmake" => :build
   depends_on "qt-4"
 
