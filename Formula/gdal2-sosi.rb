b386c45290e03f3d809c5ee65aa8ff9f2dd30017
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-11-02 21:39:21 -0600

    gdal2-sosi: 2.1.2; new formula for GDAL 2.x SOSI driver

---
+++ Formula/gdal2-sosi.rb
@@ -0,0 +1,68 @@
+class Gdal2Sosi < Formula
+  desc "GDAL/OGR 2.x plugin for SOSI driver"
+  homepage "http://trac.osgeo.org/gdal/wiki/SOSI"
+  url "http://download.osgeo.org/gdal/2.1.2/gdal-2.1.2.tar.gz"
+  sha256 "69761c38acac8c6d3ea71304341f6982b5d66125a1a80d9088b6bfd2019125c9"
+
+  depends_on "fyba"
+  depends_on "gdal2"
+
+  def gdal_majmin_ver
+    gdal_ver_list = Formula["gdal2"].version.to_s.split(".")
+    "#{gdal_ver_list[0]}.#{gdal_ver_list[1]}"
+  end
+
+  def gdal_plugins_subdirectory
+    "gdalplugins/#{gdal_majmin_ver}"
+  end
+
+  def install
+    fyba_opt = Formula["fyba"].opt_prefix
+
+    gdal_plugins = lib/gdal_plugins_subdirectory
+    gdal_plugins.mkpath
+    (HOMEBREW_PREFIX/"lib/#{gdal_plugins_subdirectory}").mkpath
+
+    # cxx flags
+    args = %W[-DLINUX -DUNIX -Iport -Igcore -Iogr
+              -Iogr/ogrsf_frmts -Iogr/ogrsf_frmts/generic
+              -Iogr/ogrsf_frmts/sosi -I#{fyba_opt}/include/fyba]
+
+    # source files
+    Dir["ogr/ogrsf_frmts/sosi/ogrsosi*.c*"].each do |src|
+      args.concat %W[#{src}]
+    end
+
+    # plugin dylib
+    dylib_name = "ogr_SOSI.dylib"
+    args.concat %W[
+      -dynamiclib
+      -install_name #{opt_lib}/#{gdal_plugins_subdirectory}/#{dylib_name}
+      -current_version #{version}
+      -compatibility_version #{gdal_majmin_ver}.0
+      -o #{gdal_plugins}/#{dylib_name}
+      -undefined dynamic_lookup
+    ]
+
+    # ld flags
+    args.concat %W[-L#{fyba_opt}/lib -lfyba -lfygm -lfyut]
+
+    # build and install shared plugin
+    system ENV.cxx, *args
+  end
+
+  def caveats; <<-EOS.undent
+      This formula provides a plugin that allows GDAL or OGR to access geospatial
+      data stored in its format. In order to use the shared plugin, you may need
+      to set the following enviroment variable:
+
+        export GDAL_DRIVER_PATH=#{HOMEBREW_PREFIX}/lib/gdalplugins
+    EOS
+  end
+
+  test do
+    gdal_opt_bin = Formula["gdal2"].opt_bin
+    out = `#{gdal_opt_bin}/ogrinfo --formats`
+    assert_match "SOSI -vector- (ro)", out
+  end
+end
