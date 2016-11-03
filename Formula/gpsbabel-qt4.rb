8f9065537b2f0cffd734e10bd03024608979abda
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-11-02 21:33:26 -0600

    gpsbabel-qt4: 1.5.3; new keg-only formula for qt-4 compatibility

---
+++ Formula/gpsbabel-qt4.rb
@@ -0,0 +1,35 @@
+class GpsbabelQt4 < Formula
+  desc "Converts/uploads GPS waypoints, tracks, and routes"
+  homepage "https://www.gpsbabel.org/"
+  url "https://github.com/gpsbabel/gpsbabel/archive/gpsbabel_1_5_3.tar.gz"
+  sha256 "10b7aaca44ce557fa1175fec37297b8df55611ab2c51cb199753a22dbf2d3997"
+
+  head "https://github.com/gpsbabel/gpsbabel.git"
+
+  keg_only "gpsbabel is in main tap and same-name bin utilities are installed"
+
+  depends_on "libusb" => :optional
+  depends_on "qt-4"
+
+  def install
+    args = ["--disable-debug", "--disable-dependency-tracking",
+            "--prefix=#{prefix}"]
+    args << "--without-libusb" if build.without? "libusb"
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.loc").write <<-EOS.undent
+      <?xml version="1.0"?>
+      <loc version="1.0">
+        <waypoint>
+          <name id="1 Infinite Loop"><![CDATA[Apple headquarters]]></name>
+          <coord lat="37.331695" lon="-122.030091"/>
+        </waypoint>
+      </loc>
+    EOS
+    system bin/"gpsbabel", "-i", "geo", "-f", "test.loc", "-o", "gpx", "-F", "test.gpx"
+    assert File.exist? "test.gpx"
+  end
+end
