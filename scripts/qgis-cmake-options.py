ca6b5d724d313f01cd556bb8af38bdf68f4438e7
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-12-13 21:51:18 -0700

    scripts: legacy commit of changes for qgis cmake options

--- scripts/qgis-cmake-options.py
+++ scripts/qgis-cmake-options.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
  OSGeo4Mac Python script for generating CMake option string for use in Qt
  Creator with dev builds and installs of QGIS when built off dependencies
- from homebrew-osgeo4mac tap
+ from Homebrew project
                               -------------------
-        begin    : January 2014
-        copyright: (C) 2014 Larry Shaffer
+        begin    : November 2016
+        copyright: (C) 2016 Larry Shaffer
         email    : larrys at dakotacarto dot com
  ***************************************************************************/
 
@@ -24,119 +24,85 @@
 import os
 import re
 import sys
+import subprocess
 from collections import OrderedDict
 import argparse
 
-GRASS_VERSION = '6.4.6'
-GRASS7_VERSION = '7.0.5'
-OSG_VERSION = '3.4.0'
-HOMEBREW_PREFIX = '/usr/local'  # default for Homebrew
+HOMEBREW_PREFIX = \
+    subprocess.run(["brew", "--prefix"],
+                   stdout=subprocess.PIPE).stdout.decode('UTF-8').strip()
 
+def cellar_version(pkg, hb):
+    try:
+        cellar_pth = os.readlink('{0}/opt/{1}'.format(hb, pkg))
+        return cellar_pth.split('/')[-1]
+    except (OSError, IndexError):
+        return ''
 
 def cmake_opts(qi, hb, ver):
-    # define versions of CMake option changes
-    lt27 = int(ver[0]) <= 2 and int(ver[1]) < 7
-    w_server = 'WITH_MAPSERVER' if lt27 else 'WITH_SERVER'
-
-    # ensure libintl.h can be found in gettext for grass
-    cxx_flags = "-I{hb}/opt/gettext/include"
-    if 'CXXFLAGS' in os.environ:
-        cxx_flags += " " + os.environ['CXXFLAGS']
-
-    # search Homebrew's Frameworks directory before those in /Library or /System
-    ld_flags = "-F{hb}/Frameworks"
-    if 'LDFLAGS' in os.environ:
-        ld_flags += " " + os.environ['LDFLAGS']
-
     # IMPORTANT: mulit-path options need the CMake list semicolon (;) delimiter,
     #            NOT the environment variable path list colon (:) separator
 
     # set CMAKE_PREFIX_PATH for keg-only installs, and HOMEBREW_PREFIX
 
     cm_opts = OrderedDict([
-        ('CMAKE_INSTALL_PREFIX', qi),
-        ('CMAKE_PREFIX_PATH', '"{hb}/opt/libxml2;{hb}/opt/expat;'
-                              '{hb}/opt/gettext;{hb}/opt/sqlite;'
-                              '{hb}/opt/gdal2;'
-                              '{hb}"'),
-        ('CMAKE_FRAMEWORK_PATH', '"{hb}/opt/qwt/lib;{hb}/opt/qwtpolar/lib"'),
-        ('CMAKE_BUILD_TYPE', 'RelWithDebInfo'),
-        ('CMAKE_FIND_FRAMEWORK', 'LAST'),
-        ('CMAKE_CXX_FLAGS', '"' + cxx_flags + '"'),
-        ('CMAKE_EXE_LINKER_FLAGS', '"' + ld_flags + '"'),
-        ('CMAKE_MODULE_LINKER_FLAGS', '"' + ld_flags + '"'),
-        ('CMAKE_SHARED_LINKER_FLAGS', '"' + ld_flags + '"'),
-        ('CXX_EXTRA_FLAGS', '"-Wno-inconsistent-missing-override '
-                            '-Wno-unused-private-field '
-                            '-Wno-deprecated-register"'),
-        ('BISON_EXECUTABLE', '{hb}/opt/bison/bin/bison'),
-        ('FLEX_EXECUTABLE', '{hb}/opt/flex/bin/flex'),
-        ('QT_QMAKE_EXECUTABLE', '{hb}/bin/qmake'),
-        ('SUPPRESS_QT_WARNINGS', 'TRUE'),
-        ('GITCOMMAND', '{hb}/bin/git'),
-        ('ENABLE_TESTS', 'TRUE'),
-        ('ENABLE_MODELTEST', 'TRUE'),
-        ('WITH_ASTYLE', 'TRUE'),
-        ('WITH_PYSPATIALITE', 'FALSE'),
-        ('WITH_QWTPOLAR', 'TRUE'),
-        ('WITH_INTERNAL_QWTPOLAR', 'FALSE'),
-        (w_server, 'TRUE'),
-        ('WITH_STAGED_PLUGINS', 'FALSE'),
-        ('WITH_PY_COMPILE', 'TRUE'),
-        ('WITH_APIDOC', 'FALSE'),
-        ('WITH_QSCIAPI', 'FALSE'),
-        ('QSCI_SIP_DIR', '{hb}/opt/qscintilla2/share/sip'),
-        ('GDAL_CONFIG', '{hb}/opt/gdal2/bin/gdal-config'),
-        ('POSTGRES_CONFIG', '{hb}/bin/pg_config'),
-        ('WITH_QSPATIALITE', 'TRUE'),
-        ('WITH_GRASS', 'FALSE'),
-        ('WITH_GRASS7', 'TRUE'),
-        ('GRASS_PREFIX7', '{hb}/opt/grass7/grass-' + GRASS7_VERSION),
-        ('WITH_GLOBE', 'FALSE'),
-        ('WITH_ORACLE', 'FALSE'),
-        ('FCGI_INCLUDE_DIR', '{hb}/opt/fcgi/include'),
-        ('GEOS_INCLUDE_DIR', '{hb}/opt/geos/include'),
-        ('GSL_INCLUDE_DIR', '{hb}/opt/gsl/include'),
-        ('PROJ_INCLUDE_DIR', '{hb}/opt/proj/include'),
-        ('QCA_INCLUDE_DIR', '{hb}/opt/qca/lib/qca.framework/Headers'),
-        ('QSCINTILLA_INCLUDE_DIR', '{hb}/opt/qscintilla2/include'),
-        ('QWT_INCLUDE_DIR', '{hb}/opt/qwt/lib/qwt.framework/Headers'),
-        ('SPATIALINDEX_INCLUDE_DIR', '{hb}/opt/spatialindex/include/spatialindex'),
-        ('SPATIALITE_INCLUDE_DIR', '{hb}/opt/libspatialite/include'),
-        ('SQLITE3_INCLUDE_DIR', '{hb}/opt/sqlite/include'),
-        ('QGIS_MACAPP_BUNDLE', '0')
+        ('CMAKE_INSTALL_PREFIX:PATH', qi),
+        ('CMAKE_PREFIX_PATH:INTERNAL',
+            '"{hb}/opt/qt5;{hb}/opt/qt5-webkit;'
+            '{hb}/opt/libxml2;{hb}/opt/expat;'
+            '{hb}/opt/bison;{hb}/opt/flex;'
+            '{hb}/opt/sqlite;{hb}/opt/gdal2;'
+            '{hb}"'),
+        ('CMAKE_BUILD_TYPE:STRING', 'RelWithDebInfo'),
+        ('CMAKE_FIND_FRAMEWORK:STRING', 'LAST'),
+        ('QT_QMAKE_EXECUTABLE:FILEPATH', '{hb}/opt/qt5/bin/qmake'),
+
+        ('CXX_EXTRA_FLAGS:STRING', ''),
+        ('DISABLE_DEPRECATED:BOOL', 'FALSE'),
+        ('ENABLE_COVERAGE:BOOL', 'FALSE'),
+        ('ENABLE_MODELTEST:BOOL', 'FALSE'),
+        ('ENABLE_MSSQLTEST:BOOL', 'FALSE'),
+        ('ENABLE_ORACLETEST:BOOL', 'FALSE'),
+        ('ENABLE_PGTEST:BOOL', 'FALSE'),
+        ('ENABLE_TESTS:BOOL', 'TRUE'),
+        ('GENERATE_COVERAGE_DOCS:BOOL', 'FALSE'),
+        ('PEDANTIC:BOOL', 'TRUE'),
+
+        ('GDAL_LIBRARY:FILEPATH', '{hb}/opt/gdal2/lib/libgdal.dylib'),
+        ('GDAL_CONFIG:FILEPATH', '{hb}/opt/gdal2/bin/gdal-config'),
+        ('GEOS_LIBRARY:FILEPATH', '{hb}/opt/geos/lib/libgeos_c.dylib'),
+        ('GSL_CONFIG:FILEPATH', '{hb}/opt/gsl/bin/gsl-config'),
+        ('GSL_INCLUDE_DIR:PATH', '{hb}/opt/gsl/include'),
+        ('GSL_LIBRARIES:STRING', '-L{hb}/opt/gsl/lib -lgsl -lgslcblas'),
+        ('POSTGRES_CONFIG:FILEPATH', '{hb}/bin/pg_config'),
+
+        ('WITH_GRASS7:BOOL', 'TRUE'),
+        ('GRASS_PREFIX7:PATH', '{hb}/opt/grass7/grass-base'),
+
+        ('WITH_APIDOC:BOOL', 'FALSE'),
+        ('WITH_ASTYLE:BOOL', 'TRUE'),
+        ('WITH_CUSTOM_WIDGETS:BOOL', 'TRUE'),
+        ('WITH_GLOBE:BOOL', 'FALSE'),
+        ('WITH_GRASS:BOOL', 'FALSE'),
+        ('WITH_INTERNAL_QWTPOLAR:BOOL', 'FALSE'),
+        ('WITH_ORACLE:BOOL', 'FALSE'),
+        ('WITH_PY_COMPILE:BOOL', 'FALSE'),
+        ('WITH_PYSPATIALITE:BOOL', 'FALSE'),
+        ('WITH_QSCIAPI:BOOL', 'FALSE'),
+        ('WITH_QSPATIALITE:BOOL', 'FALSE'),
+        ('WITH_QTWEBKIT:BOOL', 'TRUE'),
+        ('WITH_QWTPOLAR:BOOL', 'TRUE'),
+        ('WITH_SERVER:BOOL', 'TRUE'),
+        ('WITH_STAGED_PLUGINS:BOOL', 'TRUE'),
+
+        ('QGIS_MACAPP_BUNDLE:STRING', '0')
     ])
 
-    # ('CMAKE_FRAMEWORK_PATH', '""')
-
     # Supplemental
-
-    # ('OSG_DIR', '{hb}'),
-    # ('OSGEARTH_DIR', '{hb}'),
-    # ('OSG_PLUGINS_PATH', '{hb}/lib/osgPlugins-' + OSG_VERSION),
-
-    # ('GRASS_PREFIX', '{hb}/opt/grass6/grass-' + GRASS_VERSION),
-
-    # ('OCI_INCLUDE_DIR', '{hb}/opt/oracle-client-sdk/sdk/include'),
-    # ('OCI_LIBRARY', '{hb}/lib/libclntsh.dylib'),
-
-    # These should be found automatically now...
-    # ('SQLITE3_INCLUDE_DIR', '{hb}/opt/sqlite/include'),
-    # ('SQLITE3_LIBRARY', '{hb}/opt/sqlite/lib/libsqlite3.dylib'),
-    # ('QSCINTILLA_INCLUDE_DIR', '{hb}/opt/qscintilla2/include/Qsci'),
-    # ('QSCINTILLA_LIBRARY', '{hb}/opt/qscintilla2/lib/libqscintilla2.dylib'),
-    # ('QWT_LIBRARY', '{hb}/opt/qwt/lib/qwt.framework/qwt'),
-    # ('QWT_INCLUDE_DIR', '{hb}/opt/qwt/lib/qwt.framework/Headers'),
-    # ('QWTPOLAR_LIBRARY',
-    #  '{hb}/opt/qwtpolar/lib/qwtpolar.framework/qwt'),
-    # ('QWTPOLAR_INCLUDE_DIR',
-    #  '{hb}/opt/qwtpolar/lib/qwtpolar.framework/Headers'),
-    # ('WITH_INTERNAL_SPATIALITE', 'FALSE'),
+    #
 
     if os.path.exists(hb + '/Frameworks/Python.framework/Versions/2.7'):
         cm_opts['PYTHON_EXECUTABLE'] = '{hb}/bin/python'
-        cm_opts['PYTHON_CUSTOM_FRAMEWORK'] = \
-            '{hb}/Frameworks/Python.framework/Versions/2.7'
 
     return cm_opts
 
@@ -146,13 +112,13 @@ def arg_parser():
         description="""\
             Script for generating CMake option string for use in Qt Creator with
             dev builds and installs of QGIS when built off dependencies from
-            homebrew-osgeo4mac tap.
+            Homebrew.
             """
     )
     parser.add_argument(
         '-p', '--homebrew-prefix', dest='hb',
         metavar='homebrew_prefix',
-        help='homebrew prefix path, or set HOMEBREW_PREFIX (/usr/local default)'
+        help='homebrew prefix path, or set HOMEBREW_PREFIX'
     )
     parser.add_argument(
         'qs', metavar='qgis-src-dir',
