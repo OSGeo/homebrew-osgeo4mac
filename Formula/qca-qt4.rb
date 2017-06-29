b98933c97d5f6902f3b687df064409dca6b49174
Larry Shaffer <lshaffer@boundlessgeo.com>
2017-06-29 05:27:36 -0600

    qca-qt4: update download hashes for patch diffs
    [ci skip]

--- Formula/qca-qt4.rb
+++ Formula/qca-qt4.rb
@@ -10,13 +10,13 @@ class QcaQt4 < Formula
     # Fix for linking CoreFoundation and removing deprecated code; already in HEAD).
     patch do
       url "https://github.com/KDE/qca/commit/f223ce03d4b94ffbb093fc8be5adf8d968f54434.diff"
-      sha256 "75ef105b01658c3b4030b8c697338dbceddbcc654b022162b284e0fa8df582b5"
+      sha256 "78f0239c7007f7bf74c94c90f142f49d4b748a2c7a2d56eaed38dc5ee3fd6ee1"
     end
 
     # Fix for framework installation; already in HEAD).
     patch do
       url "https://github.com/KDE/qca/commit/9e4bf795434304bce32626fe0f6887c10fec0824.diff"
-      sha256 "5f4e575d2c9f55090c7e3358dc27b6e22cccecaaee264d1638aabac86421c314"
+      sha256 "952e1fc4f96db0ee11e7dca0013e81e512fe79ac0c4b3cf993ce8c7f0061a016"
     end
   end
 
