0a4206b31688f0509ad23b9bd5a5ba555c87d930
Larry Shaffer <lshaffer@boundlessgeo.com>
2017-08-09 08:45:54 -0600

    gdal2: add sierra bottle
    [ci skip]

--- Formula/gdal2.rb
+++ Formula/gdal2.rb
@@ -4,10 +4,10 @@ class Gdal2 < Formula
   url "http://download.osgeo.org/gdal/2.2.1/gdal-2.2.1.tar.gz"
   sha256 "61837706abfa3e493f3550236efc2c14bd6b24650232f9107db50a944abf8b2f"
 
-  # bottle do
-  #   root_url "http://qgis.dakotacarto.com/bottles"
-  #   sha256 "db8f421f6f0fa62769a26e76848c36d048d71bfc9b99014837130fef99bf7cd5" => :sierra
-  # end
+  bottle do
+    root_url "http://qgis.dakotacarto.com/bottles"
+    sha256 "63d30c9d78fdaa0c6ac52c3c3f5b5370d5f66dad426fbc04fd8e91baadca4d68" => :sierra
+  end
 
   head do
     url "https://svn.osgeo.org/gdal/trunk/gdal"
