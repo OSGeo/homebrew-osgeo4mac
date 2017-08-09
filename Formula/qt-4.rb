8669bd6f4306eb81a3f91021656f0f8f16ee3810
Larry Shaffer <lshaffer@boundlessgeo.com>
2017-08-08 23:25:02 -0600

    qt-4: fix formula for sandboxing restrictions and audit warnings
    [ci skip]

--- Formula/qt-4.rb
+++ Formula/qt-4.rb
@@ -14,12 +14,14 @@ class Qt4 < Formula
 
   # Backport of Qt5 commit to fix the fatal build error with Xcode 7, SDK 10.11.
   # https://code.qt.io/cgit/qt/qtbase.git/commit/?id=b06304e164ba47351fa292662c1e6383c081b5ca
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/480b7142c4e2ae07de6028f672695eb927a34875/qt/el-capitan.patch"
-    sha256 "c8a0fa819c8012a7cb70e902abb7133fc05235881ce230235d93719c47650c4e"
+  if MacOS.version >= :el_capitan
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/480b7142c4e2ae07de6028f672695eb927a34875/qt/el-capitan.patch"
+      sha256 "c8a0fa819c8012a7cb70e902abb7133fc05235881ce230235d93719c47650c4e"
+    end
   end
 
-  keg_only "Deprecated in homebrew-core for macOS >= Sierra (10.12)"
+  keg_only "deprecated in homebrew-core for macOS >= Sierra (10.12)"
 
   option "with-qt3support", "Build with deprecated Qt3Support module support"
   option "with-docs", "Build documentation"
@@ -107,7 +109,7 @@ class Qt4 < Formula
 
     system "./configure", *args
     system "make"
-    ENV.j1
+    ENV.deparallelize
     system "make", "install"
 
     # what are these anyway?
@@ -133,8 +135,6 @@ class Qt4 < Formula
            "#{bin}/Designer.app/Contents/Info.plist"
 
     Pathname.glob("#{bin}/*.app") { |app| mv app, prefix }
-
-    post_install
   end
 
   def post_install
