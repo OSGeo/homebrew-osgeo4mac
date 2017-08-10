dfb5dc6c12449e53f877767d8b7071ae890ce6d0
Larry Shaffer <lshaffer@boundlessgeo.com>
2017-08-10 17:44:22 -0600

    travis-cache-seed: add Travis CI Homebrew cache seeding formula

---
+++ Formula/travis-cache-seed.rb
@@ -0,0 +1,98 @@
+class TravisCacheSeed < Formula
+  desc "Trigger update of Travis CI cache with geospatial dependencies"
+  homepage "https://github.com/OSGeo/homebrew-osgeo4mac"
+  url "http://qgis.dakotacarto.com/osgeo4mac/dummy.tar.gz"
+  version "0.0.1"
+  sha256 "e7776e2ff278d6460300bd69a26d7383e6c5e2fbeb17ff12998255e7fc4c9511"
+
+  keg_only "because it doesn't really install anything"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "bison"
+  depends_on "boost"
+  depends_on "cairo"
+  depends_on "cmake" => :build
+  depends_on "expat"
+  depends_on "fcgi"
+  depends_on "fftw"
+  depends_on "flex"
+  depends_on "freetype"
+  depends_on "gdal2-python"
+  depends_on "gdal2"
+  depends_on "geos"
+  depends_on "gettext"
+  depends_on "ghostscript"
+  depends_on "glew"
+  depends_on "glfw"
+  depends_on "gpsbabel-qt4"
+  depends_on "gsl"
+  depends_on "homebrew/science/insighttoolkit"
+  # depends on "homebrew/science/osgearth"
+  depends_on "jpeg"
+  depends_on "lbzip2"
+  depends_on "libgeotiff"
+  depends_on "libharu"
+  depends_on "libkml"
+  depends_on "libpng"
+  depends_on "libsvm"
+  depends_on "libtiff"
+  depends_on "libtool" => :build
+  depends_on "minizip"
+  depends_on "muparser"
+  depends_on "mysql"
+  depends_on "numpy"
+  depends_on "open-scene-graph"
+  depends_on "openssl"
+  depends_on "ossim"
+  depends_on "pcre"
+  depends_on "pkg-config" => :build
+  depends_on "postgresql"
+  depends_on "proj"
+  depends_on "pyqt-qt4"
+  depends_on "pyspatialite"
+  depends_on "python"
+  depends_on "python3"
+  depends_on "qca-qt4"
+  depends_on "qhull"
+  depends_on "qjson-qt4"
+  depends_on "qscintilla2-qt4"
+  depends_on "qt-4"
+  depends_on "qwt-qt4"
+  depends_on "qwtpolar-qt4"
+  depends_on "readline"
+  depends_on "sip-qt4"
+  depends_on "spatialindex"
+  depends_on "sqlite"
+  depends_on "swig" => :build
+  depends_on "tinyxml"
+  depends_on "unixodbc"
+  depends_on "vtk"
+  depends_on "wxmac"
+  depends_on "wxpython"
+  depends_on "zlib"
+
+  def install
+    (share/"blank").write "blank"
+  end
+
+  def caveats; <<-EOS.undent
+    Formula does not install anything per se. Just updates cache at Travis CI
+    with geospatial dependencies to reduce build times of larger formulae.
+
+    Example `.travis.yml` settings:
+
+      cache:
+        directories:
+          - /usr/local
+        timeout: 900
+      before_cache:
+        - brew cleanup
+
+    EOS
+  end
+
+  test do
+    #
+  end
+end
