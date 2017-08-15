6e348ba3786d7d4b745dc1e7b51012633bc02245
Larry Shaffer <lshaffer@boundlessgeo.com>
2017-08-14 21:49:11 -0400

    qgis2: bump revision
    [ci skip]

--- Formula/qgis2.rb
+++ Formula/qgis2.rb
@@ -5,6 +5,7 @@ class Qgis2 < Formula
   desc "Open Source Geographic Information System"
   homepage "https://www.qgis.org"
 
+  revision 1
   head "https://github.com/qgis/QGIS.git", :branch => "release-2_18"
 
   stable do
