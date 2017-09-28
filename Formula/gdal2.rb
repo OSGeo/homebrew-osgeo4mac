ae230896a5e2ce74523d70d1ead7b6c8f4f16d93
Larry Shaffer <lshaffer@boundlessgeo.com>
2017-09-27 19:55:58 -0600

    gdal2: update sierra bottle
    [ci skip]

--- Formula/gdal2.rb
+++ Formula/gdal2.rb
@@ -4,10 +4,10 @@ class Gdal2 < Formula
   url "http://download.osgeo.org/gdal/2.2.2/gdal-2.2.2.tar.gz"
   sha256 "14c1f78a60f429ad51c08d75cbf49771f1e6b20e7385c6e8379b40e8dfa39544"
 
-  # bottle do
-  #   root_url "http://qgis.dakotacarto.com/bottles"
-  #   sha256 "26a1871aa163a5cb76e5d99b9025f4ed2e54e358178b91ab54fea5a8a4579a1f" => :sierra
-  # end
+  bottle do
+    root_url "http://qgis.dakotacarto.com/bottles"
+    sha256 "fe18e999b216099c5f9b8eedfd178ce2d351cf507583c3b4990bd54abe71ceb1" => :sierra
+  end
 
   head do
     url "https://svn.osgeo.org/gdal/trunk/gdal"
