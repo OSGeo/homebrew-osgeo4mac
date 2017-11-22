6e80d1413f23a077bfa788824a51c5ac61aec659
Larry Shaffer <lshaffer@boundlessgeo.com>
2017-11-21 18:37:51 -0700

    prepair: update to released 0.7.1 build (faux-version downgrade)
    [ci skip]

--- Formula/prepair.rb
+++ Formula/prepair.rb
@@ -1,9 +1,14 @@
 class Prepair < Formula
   desc "Automatic repair of single GIS polygons using constrained triangulation"
   homepage "https://github.com/tudelft-gist/prepair"
-  url "https://github.com/tudelft3d/prepair/archive/417f1bc9c4375bfe293e4c096cadc9911feb6266.tar.gz"
-  version "0.8-dev"
-  sha256 "cd19877f014ec98737ec0966cf8eacfb27d98627c6e42c7c27b19b97e7b139af"
+  url "https://github.com/tudelft3d/prepair/archive/v0.7.1.tar.gz"
+  sha256 "2abc69588880e595552af363580e38c1a4a63c9d51549f6450ab6f96ee1ad67f"
+
+  # bottle do
+  #   root_url "https://osgeo4mac.s3.amazonaws.com/bottles"
+  #   sha256 "" => :sierra
+  #   sha256 "" => :high_sierra
+  # end
 
   option "with-library", "Build library in addition to executable"
 
