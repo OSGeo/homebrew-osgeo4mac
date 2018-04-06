37d017688540901a251401beac47e2e5863951be
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-04-06 08:22:53 -0400

    vendoring pillow (credits to @fitnr)

--- Formula/gpkgtools.rb
+++ Formula/gpkgtools.rb
@@ -6,7 +6,11 @@ class Gpkgtools < Formula
 
   depends_on 'libspatialite'
   depends_on :python
-  depends_on 'PIL' => :python
+
+  resource "Pillow" do
+    url "https://files.pythonhosted.org/packages/0f/57/25be1a4c2d487942c3ed360f6eee7f41c5b9196a09ca71c54d1a33c968d9/Pillow-5.0.0.tar.gz"
+    sha256 "12f29d6c23424f704c66b5b68c02fe0b571504459605cfe36ab8158359b0e1bb"
+  end
 
   def install
     head_ext = "#{HOMEBREW_PREFIX}/Cellar/libspatialite/HEAD/lib/spatialite.dylib"
@@ -21,6 +25,9 @@ class Gpkgtools < Formula
                 `brew switch libspatialite <non-HEAD version>`
       EOS
     end
+
+    resource("Pillow").stage { system "python", *Language::Python.setup_install_args(libexec/"vendor") }
+
     cd 'gpkgtools' do
       inreplace %w[util_sqlite.py GeoPackage.py] do |s|
         s.sub! 'from pysqlite2', '#from pysqlite2'
