6d711d164e3be158fe2e37b5865b6bbb7c07e348
Nick Robison <nick@nickrobison.com>
2018-04-11 12:25:31 -0700

    Modified bottle building process to automatically update the Formulae with the new bottles.
    Currently builds both sierra and high sierra bottles and merges them into the formula.
    After updating all the formula, it adds them into a single commit and pushes the commit to master using the new deploy key.
    This means we get a single commit with all the changes bottles, but Travis skips the new commit so we only get a single build.
    If the build fails at any point, the bottles aren't built and no extra commits are made to the repo.
    
    Also updated gitignore to skip IntelliJ files.

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -18,6 +18,15 @@
 set -e
 
 cd ${TRAVIS_BUILD_DIR}
+
+# Setup Git configuration
+git config user.name "Travis CI (for $(git log --format='%an' $(TRAVIS_COMMIT)^!))"
+git config user.email "$(git log --format='%ae' $(TRAVIS_COMMIT)^!)"
+REPO=$(git config remote.origin.url)
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+
+# Build the bottles
 mkdir -p bottles
 
 pushd bottles
@@ -25,14 +34,39 @@ pushd bottles
     echo "Bottling changed formula ${f}..."
     brew bottle --verbose --json --root-url=https://osgeo4mac.s3.amazonaws.com/bottles \
       ${TRAVIS_REPO_SLUG}/${f}
-  done
 
-  # temporary duplication of 10.2-Xcode-8.x-built bottles to 10.3 bottles
-  for art in *.sierra.bottle.*; do
-    new_name=${art/.sierra./.high_sierra.}
-    cp -a ${art} ${new_name}
-  done
-  for json in *.high_sierra.bottle*.json; do
-    sed -i '' s@sierra@high_sierra@g ${json}
+#    temporary duplication of 10.2-Xcode-8.x-built bottles to 10.3 bottles
+#    Do the bottle duplication per formula, so we can merge the changes
+    for art in ${f}*.sierra.bottle.*; do
+      new_name=${art/.sierra./.high_sierra.}
+      cp -a ${art} ${new_name}
+    done
+    for json in *.high_sierra.bottle*.json; do
+      sed -i '' s@sierra@high_sierra@g ${json}
+    done
+
+#    Do Merge bottles with the formula
+#    Don't commit anything, we'll do that after updating all the formulae
+    brew bottle --merge --write --no-commit ${f}*.json
   done
 popd
+
+# Now do the commit and push
+
+git add -vA Formula/*.rb
+git commit -m "Updated bottles for: ${CHANGED_FORMULAE}
+
+[ci skip]"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./deploy_key.enc -out ./deploy_key -d
+chmod 600 ./deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push ${SSH_REPO} master
