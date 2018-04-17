5930642017175fdfc7837abcbf0ca2575ec0a323
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-04-17 13:29:31 -0400

    remove bottle from gpkg

--- Formula/gpkgtools.rb
+++ Formula/gpkgtools.rb
@@ -4,10 +4,6 @@ class Gpkgtools < Formula
   homepage 'https://launchpad.net/gpkgtools'
   head "lp:gpkgtools", :using => :bzr
 
-  bottle do
-
-  end
-
   depends_on 'libspatialite'
   depends_on "python@2"
 
