175b8ed149b4ec5ce660190baabb5e8a74c18722
Nick Robison <nickrobison@users.noreply.github.com>
2018-04-30 14:48:24 -0700

    Upgrade ossim and add alias (#360)
    * Upgrade ossim and add alias
    
    * Added patch to deal with missing nullptr in Xcode 8.3

--- Formula/ossim.rb
+++ Formula/ossim.rb
@@ -2,19 +2,17 @@ class Ossim < Formula
   desc "Geospatial libs and apps to process imagery, terrain, and vector data"
   homepage "https://trac.osgeo.org/ossim/"
 
-  # TODO: request a tagged release
-  # May 18, 2016 commit
-  url "https://github.com/ossimlabs/ossim.git",
-      :branch => "master",
-      :revision => "f01c951587eeb63623b913750bec5097ece86d9a"
-  version "1.9.0"
-  revision 5
+  stable do
+    url "https://github.com/ossimlabs/ossim/archive/Gasparilla-2.3.1.tar.gz"
+    sha256 "f928544b4dfc6a1c93c55afb244d04e9a33b368fd5e6c3fe552f43b5da4e7c6e"
+  end
 
   bottle do
-    root_url "https://osgeo4mac.s3.amazonaws.com/bottles"
-    sha256 "5377c220a9e7448445989e9b3f89e9dd38710e81bc8936291161d846303a4e58" => :sierra
-    sha256 "5377c220a9e7448445989e9b3f89e9dd38710e81bc8936291161d846303a4e58" => :high_sierra
   end
+
+  # This patch is required in order to build on XCode 8.3
+  # It's been submitted upstream as: https://github.com/ossimlabs/ossim/pull/199
+  patch :DATA
 
   option "with-curl-apps", "Build curl-dependent apps"
   option "without-framework", "Generate library instead of framework"
@@ -29,16 +27,10 @@ class Ossim < Formula
   depends_on "geos"
   depends_on "freetype"
   depends_on "zlib"
-  depends_on "mpi" => [:cc, :cxx, :optional]
+  depends_on "open-mpi" => :optional
 
   def install
     ENV.cxx11
-
-    # build setup expects the checkout to be in subdir named 'ossim'
-    cur_dir = Dir["*", ".git*"]
-    mkdir "ossim"
-    (buildpath/"ossim").install cur_dir
-    mkdir "build"
 
     ENV["OSSIM_DEV_HOME"] = buildpath.to_s
     ENV["OSSIM_BUILD_DIR"] = (buildpath/"build").to_s
@@ -46,7 +38,6 @@ class Ossim < Formula
 
     # TODO: add options and deps for plugins
     args = std_cmake_args + %W[
-      -DCMAKE_CXX_STANDARD=11
       -DOSSIM_DEV_HOME=#{ENV["OSSIM_DEV_HOME"]}
       -DINSTALL_LIBRARY_DIR=lib
       -DBUILD_OSSIM_APPS=ON
@@ -79,20 +70,10 @@ class Ossim < Formula
     args << "-DBUILD_OSSIM_CURL_APPS=" + (build.with?("curl-apps") ? "ON" : "OFF")
     args << "-DBUILD_OSSIM_GUI=" + (build.with?("gui") ? "ON" : "OFF")
 
-    # fix up include/ossim/base/ossimRefPtr.h
-    inreplace "ossim/include/ossim/base/ossimRefPtr.h" do |s|
-      s.sub! /(#include <stddef.h>)/, "\\1\n#include <cstddef>"
-      s.gsub! "nullptr_t", "std::nullptr_t"
-    end
-
-    cd "build" do
-      system "cmake", "../ossim/cmake", *args
-      # bbedit = "/usr/local/bin/bbedit"
-      # system bbedit, "CMakeCache.txt"
-      # raise
+    mkdir "build" do
+      system "cmake", "..", *args
       system "make"
       system "make", "install"
-      File.rename (prefix/"lib64").to_s, lib.to_s
     end
   end
 
@@ -100,3 +81,41 @@ class Ossim < Formula
     system bin/"ossim-cli", "--version"
   end
 end
+__END__
+diff --git a/include/ossim/base/ossimRefPtr.h b/include/ossim/base/ossimRefPtr.h
+index fef5824..dbe015a 100644
+--- a/include/ossim/base/ossimRefPtr.h
++++ b/include/ossim/base/ossimRefPtr.h
+@@ -8,6 +8,7 @@
+ #define ossimRefPtr_HEADER
+ #include <ossim/base/ossimConstants.h>
+ #include <stddef.h>
++#include <cstddef>
+ 
+ template<class T> class ossimRefPtr
+ {
+@@ -100,20 +101,20 @@ template<typename _Tp1, typename _Tp2> inline bool
+   operator==(const ossimRefPtr<_Tp1>& __a, const ossimRefPtr<_Tp2>& __b) noexcept
+   { return __a.get() == __b.get(); }
+ 
+-template<typename _Tp> inline bool operator==(const ossimRefPtr<_Tp>& __a, nullptr_t) noexcept
++template<typename _Tp> inline bool operator==(const ossimRefPtr<_Tp>& __a, std::nullptr_t) noexcept
+   { return !__a; }
+ 
+-template<typename _Tp> inline bool operator==(nullptr_t, const ossimRefPtr<_Tp>& __a) noexcept
++template<typename _Tp> inline bool operator==(std::nullptr_t, const ossimRefPtr<_Tp>& __a) noexcept
+   { return !__a; }
+ 
+ template<typename _Tp1, typename _Tp2>  inline bool
+   operator!=(const ossimRefPtr<_Tp1>& __a, const ossimRefPtr<_Tp2>& __b) noexcept
+   { return __a.get() != __b.get(); }
+ 
+-template<typename _Tp> inline bool operator!=(const ossimRefPtr<_Tp>& __a, nullptr_t) noexcept
++template<typename _Tp> inline bool operator!=(const ossimRefPtr<_Tp>& __a, std::nullptr_t) noexcept
+   { return (bool)__a; }
+ 
+-template<typename _Tp> inline bool operator!=(nullptr_t, const ossimRefPtr<_Tp>& __a) noexcept
++template<typename _Tp> inline bool operator!=(std::nullptr_t, const ossimRefPtr<_Tp>& __a) noexcept
+   { return (bool)__a; }
+ 
+ 
