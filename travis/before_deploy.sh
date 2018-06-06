15da55ed2713275e4245e24eedf40918f8dc7860
Nick Robison <nick@nickrobison.com>
2018-06-06 13:01:51 -0700

    Moved to homebrew osgeo organization.

--- travis/before_deploy.sh
+++ travis/before_deploy.sh
@@ -56,7 +56,7 @@ mkdir -p bottles
 pushd bottles
   # S3 bucket isn't currently working for the Travis pro repo, so we're switching to Bintray, for now.
   # BOTTLE_ROOT=https://osgeo4mac.s3.amazonaws.com/bottles
-  BOTTLE_ROOT=https://dl.bintray.com/nickrobison/homebrew-osgeo-bottles
+  BOTTLE_ROOT=https://dl.bintray.com/homebrew-osgeo/osgeo-bottles
   for f in ${CHANGED_FORMULAE};do
     echo "Bottling changed formula ${f}..."
     brew bottle --verbose --json --root-url=${BOTTLE_ROOT} ${TRAVIS_REPO_SLUG}/${f}
