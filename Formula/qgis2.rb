67640337464d874dc324b3ce41f5c5ab3b9fe009
Nick Robison <nick@nickrobison.com>
2018-06-11 14:16:24 -0700

    Upgrade QGIS to 2.18.20

--- Formula/qgis2.rb
+++ Formula/qgis2.rb
@@ -9,8 +9,8 @@ class Qgis2 < Formula
   head "https://github.com/qgis/QGIS.git", :branch => "release-2_18"
 
   stable do
-    url "https://github.com/qgis/QGIS/archive/final-2_18_19.tar.gz"
-    sha256 "65af7c234a75b45824a0b4b60d6c8a8c74b7542dcda694e85184df7af7bc8dda"
+    url "https://github.com/qgis/QGIS/archive/final-2_18_20.tar.gz"
+    sha256 "c3b064be173484dc2aa74eab142c9cf3f09f4c72a181553e0c31ebc3d5986c94"
 
     # patches that represent all backports to release-2_18 branch, since release tag
     # see: https://github.com/qgis/QGIS/commits/release-2_18
