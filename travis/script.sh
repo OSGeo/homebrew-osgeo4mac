8034a200c168a16b6b3369e1b3dbfc253fc95081
JF Perini <fjperini@gmail.com>
2018-06-26 02:31:37 -0300

    Test

--- travis/script.sh
+++ travis/script.sh
@@ -18,27 +18,6 @@
 set -e
 
 for f in ${CHANGED_FORMULAE};do
-#  if [[ $(brew list --versions ${f}) ]]; then
-#    echo "Clearing previously installed/cached formula ${f}..."
-#    brew uninstall --force --ignore-dependencies ${f} || true
-#  fi
   echo "Installing changed formula ${f}..."
-  brew install --build-bottle ${TRAVIS_REPO_SLUG}/${f}&
-  PID=$!
-  # add progress to ensure Travis doesn't complain about no output
-  while true; do
-    sleep 30
-    if jobs -rp | grep ${PID} >/dev/null; then
-      echo "."
-    else
-      echo
-      break
-    fi
-  done
-
-  echo "Testing changed formula ${f}..."
-  # does running postinstall mess up the bottle?
-  # (mentioned that it is skipped if installing with --build-bottle)
-  # brew postinstall ${f}
-  brew test ${TRAVIS_REPO_SLUG}/${f}
+  brew install ${f}
 done
