bbc2fe88838ad17273f8f5900f7e891885b02716
Nick Robison <nick@nickrobison.com>
2018-06-28 20:52:52 -0700

    Add sierra bottle.

--- Formula/qt5-webkit.rb
+++ Formula/qt5-webkit.rb
@@ -38,7 +38,7 @@ class Qt5Webkit < Formula
 
   bottle do
     root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
-    sha256 "26b681eed0c69cd3fe7b063d1017b78922d3c0b5e5d0051c24cf379186439cf1" => :sierra
+    sha256 "49dbdda9a08a7e3921ad14aa095190e51926e4cef6e11731430a07c6d0395de3" => :sierra
     sha256 "689dc916f223b50ca697600edb7dcb995dc92fa17740197b1fbb13582a3a21ad" => :high_sierra
   end
 
