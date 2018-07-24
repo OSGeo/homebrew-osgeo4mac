ef7eb7d45f2df9e941f091328997ff035b661650
Nick Robison <nick@nickrobison.com>
2018-07-23 17:13:07 -0700

    Add cxx11 support to saga lts.

--- Formula/saga-gis-lts.rb
+++ Formula/saga-gis-lts.rb
@@ -45,6 +45,7 @@ class SagaGisLts < Formula
     # SKIP liblas support until SAGA supports > 1.8.1, which should support GDAL 2;
     #      otherwise, SAGA binaries may lead to multiple GDAL versions being loaded
     # See: https://github.com/libLAS/libLAS/issues/106
+    ENV.cxx11
 
     cd "saga-gis"
 
