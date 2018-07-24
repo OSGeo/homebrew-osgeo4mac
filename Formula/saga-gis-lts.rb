83ba0906c32d0a55c4067c2ffe3ac9f04f7f663b
Nick Robison <nick@nickrobison.com>
2018-07-24 13:44:23 -0700

    Fix bad cxx typo

--- Formula/saga-gis-lts.rb
+++ Formula/saga-gis-lts.rb
@@ -45,7 +45,7 @@ class SagaGisLts < Formula
     # SKIP liblas support until SAGA supports > 1.8.1, which should support GDAL 2;
     #      otherwise, SAGA binaries may lead to multiple GDAL versions being loaded
     # See: https://github.com/libLAS/libLAS/issues/106
-    ENV.cxx1
+    ENV.cxx11
     # Disable narrowing warnings when compiling in C++11 mode.
     ENV.append "CXXFLAGS", "-Wno-c++11-narrowing"
 
