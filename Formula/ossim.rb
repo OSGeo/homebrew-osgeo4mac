ea34a9ec303c737f619f02bfaedbd9eef7dfd47d
FJ Perini <21315242+fjperini@users.noreply.github.com>
2018-10-28 20:25:33 -0300

    Upgrade OSSIM to 2.5.2

--- Formula/ossim.rb
+++ Formula/ossim.rb
@@ -3,8 +3,8 @@ class Ossim < Formula
   homepage "https://trac.osgeo.org/ossim/"
 
   stable do
-    url "https://github.com/ossimlabs/ossim/archive/Hollywood-2.4.2.tar.gz"
-    sha256 "1dd0cdc1533753b35922d9e49142012293b132f16bc7bc18823a5dfc541c44eb"
+    url "https://github.com/ossimlabs/ossim/archive/Islamorada-2.5.2.tar.gz"
+    sha256 "95e5ea2929e7036f2dc8baddf84eab29b9149d60074047dafa30f950a17b6edf"
   end
 
   bottle do
