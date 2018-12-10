8fe138ab55bb131736e6532983c5b9f2da28fde1
Nick Robison <nick@nickrobison.com>
2018-12-09 23:50:27 -0500

    Update encryption key
    Added brew taps to the dependency update job.

--- .circleci/before_install.sh
+++ .circleci/before_install.sh
@@ -33,10 +33,6 @@ for f in ${nix_f}; do
   brew uninstall --force --ignore-dependencies ${f} || true
 done
 
-# Add taps
-brew tap brewsci/science || true
-brew tap brewsci/bio || true
-
 brew update || brew update
 
 
