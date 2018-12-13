0512e5b251de2c7b67f7cb69357ea310964f545f
Nick Robison <nick@nickrobison.com>
2018-12-12 19:42:59 -0500

    Added ls for key debugging.

--- .circleci/before_deploy.sh
+++ .circleci/before_deploy.sh
@@ -74,6 +74,7 @@ popd
 
 # Set up the keys
 openssl aes-256-cbc -iv "${ENCRYPTION_IV}" -K "${ENCRYPTION_KEY}" -d -in ci_deploy_key.enc -out deploy_key
+ls .
 chmod 600 ./deploy.key
 ssh-add deploy_key
 
