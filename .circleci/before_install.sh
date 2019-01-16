a0c9fdd3f321a7813ec8ea6f70e46cf4bf7a3ca9
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-01-16 02:46:25 -0300

    fix Circle CI
    Error: python@2 2.7.15_1 is already installed

--- .circleci/before_install.sh
+++ .circleci/before_install.sh
@@ -33,11 +33,10 @@ for f in ${nix_f}; do
   brew uninstall --force --ignore-dependencies ${f} || true
 done
 
-brew update || brew update
+brew update > /dev/null
 
 # Install XQuartz
 brew cask install xquartz
-
 
 for f in ${CHANGED_FORMULAE};do
   echo "Homebrew setup for changed formula ${f}..."
@@ -49,7 +48,7 @@ for f in ${CHANGED_FORMULAE};do
   # https://discourse.brew.sh/t/brew-install-python3-fails/1756/3
   if [ "$(echo ${deps} | grep -c '[python|python3]')" != "0" ];then
     echo "Installing and configuring Homebrew Python3"
-    brew outdated python || brew upgrade python
+    brew upgrade python > /dev/null
 
     # Set up Python .pth files
     # get python short version (major.minor)
@@ -58,7 +57,7 @@ for f in ${CHANGED_FORMULAE};do
       echo $PY_VER
     fi
     mkdir -p ${CIRCLE_WORKING_DIRECTORY}/Library/Python/${PY_VER}/lib/python/site-packages
-   
+
     echo 'import site; site.addsitedir("${HOMEBREW_PREFIX}/lib/python${PY_VER}/site-packages")' \
          >> ${CIRCLE_WORKING_DIRECTORY}/Library/Python/${PY_VER}/lib/python/site-packages/homebrew.pth
     echo 'import site; site.addsitedir("${HOMEBREW_PREFIX}/opt/gdal2/lib/python${PY_VER}/site-packages")' \
@@ -72,13 +71,8 @@ for f in ${CHANGED_FORMULAE};do
 
   if [ "$(echo ${deps} | grep -c 'python@2')" != "0" ];then
     echo "Installing and configuring Homebrew Python2"
-    # If we just upgraded to Python3, install python2, otherwise, update it
-    if [ "$(echo ${deps} | grep -c '[python3|python]')" != "0" ];then
-      brew install python@2
-    else
-      brew outdated python@2 || brew upgrade python@2
+    brew upgrade python@2 > /dev/null
 
-    fi
     # Set up Python .pth files
     # get python short version (major.minor)
     PY_VER=$(${HOMEBREW_PREFIX}/bin/python2 -c "import sys;print('{0}.{1}'.format(sys.version_info[0],sys.version_info[1]).strip())")
@@ -96,10 +90,11 @@ for f in ${CHANGED_FORMULAE};do
       ${HOMEBREW_PREFIX}/bin/pip2 install numpy
     fi
   fi
+
   # Special handling of grass7, because it needs to be unlinked
   if [ "$(echo ${deps} | grep -c 'grass7')" != "0" ];then
     echo "Installing and unlinking grass7"
-#    GDAL gets its numpy installed via pip, but grass also has a dependency, so we need to force it.
+    # GDAL gets its numpy installed via pip, but grass also has a dependency, so we need to force it.
     brew install numpy || brew link --overwrite numpy
     brew install grass7
     brew unlink grass7
