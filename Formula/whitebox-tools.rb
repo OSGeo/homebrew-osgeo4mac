49ba9b09476ea3e72c21f4f1cffa6dcc3ba199b5
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-01-16 23:51:42 -0300

    Add WHITEBOKTOOLS formula

---
+++ Formula/whitebox-tools.rb
@@ -0,0 +1,24 @@
+class WhiteboxTools < Formula
+  desc "Geographic information system (GIS) and remote sensing package intended for advanced geospatial analysis and data visualization"
+  homepage "https://www.uoguelph.ca/~hydrogeo/WhiteboxTools"
+  url "https://github.com/jblindsay/whitebox-tools/releases/download/v0.13/WhiteboxTools_darwin_amd64.zip"
+  sha256 "fe79c3b797ba7ae23a525e11441b0d6406c339858b23271c143734c62af7ec27"
+  # url "https://github.com/jblindsay/whitebox-tools.git",
+  #   :branch => "master",
+  #   :commit => "f0cf6af792de8f60dd8afb10e3f0a26df1c702c4"
+  version "0.13.0"
+
+  # revision 1
+
+  # head "https://github.com/jblindsay/whitebox-tools.git", :branch => "master"
+
+  def install
+    cp_r "#{buildpath}", "#{prefix}"
+    mkdir "#{bin}"
+    ln_s "#{prefix}/WBT/whitebox_tools", "#{bin}/whitebox_tools"
+  end
+
+  test do
+    system "false"
+  end
+end
