f1dcd5048086d33371b124efb8ef509167b7fae1
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-01-18 17:17:07 -0300

    fix Circle CI

--- .circleci/after_script.sh
+++ .circleci/after_script.sh
@@ -18,33 +18,38 @@
 # Build the actual bottles
 # In Travis, this used to be part of the deploy phase, but now it needs to run as part of the original build process, but only on master.
 
-# mkdir -p bottles
+if [ "$CHANGED_FORMULAE" == "" ]; then
+  echo "Skipping CI, no changed formulae found";
+  exit 0;
+else
+  mkdir -p bottles
 
-pushd bottles
-  BOTTLE_ROOT=https://dl.bintray.com/homebrew-osgeo/osgeo-bottles
-  for f in ${CHANGED_FORMULAE};do
-    echo "Bottling changed formula ${f}..."
-    brew bottle --verbose --json --root-url=${BOTTLE_ROOT} osgeo/osgeo4mac/${f}
+  pushd bottles
+    BOTTLE_ROOT=https://dl.bintray.com/homebrew-osgeo/osgeo-bottles
+    for f in ${CHANGED_FORMULAE};do
+      echo "Bottling changed formula ${f}..."
+      brew bottle --verbose --json --root-url=${BOTTLE_ROOT} osgeo/osgeo4mac/${f}
 
-    for art in ${f}*.sierra.bottle.*; do
+      for art in ${f}*.sierra.bottle.*; do
+          # Remove double dashes introduced by the latest changes to Homebrew bottling.
+          # This may need to be reverted later, but this at least normalizes the bottle names with what's in the json files.
+          # Move the sierra bottle and json file
+          mv ${art} ${art/--/-}
+      done
+
+      # temporary duplication of 10.3-Xcode-10.x-built bottles to 10.4 bottles
+      # Do the bottle duplication per formula, so we can merge the changes
+      for art in ${f}*.high_sierra.bottle.*; do
+        new_name=${art/.high_sierra./.mojave.}
         # Remove double dashes introduced by the latest changes to Homebrew bottling.
         # This may need to be reverted later, but this at least normalizes the bottle names with what's in the json files.
-        # Move the sierra bottle and json file
+        cp -a ${art} ${new_name/--/-}
+        # Move the high_sierra bottle and json file
         mv ${art} ${art/--/-}
+      done
+      for json in ${f}*.mojave.bottle*.json; do
+        sed -i '' s@high_sierra@mojave@g ${json}
+      done
     done
-
-    # temporary duplication of 10.3-Xcode-10.x-built bottles to 10.4 bottles
-    # Do the bottle duplication per formula, so we can merge the changes
-    for art in ${f}*.high_sierra.bottle.*; do
-      new_name=${art/.high_sierra./.mojave.}
-      # Remove double dashes introduced by the latest changes to Homebrew bottling.
-      # This may need to be reverted later, but this at least normalizes the bottle names with what's in the json files.
-      cp -a ${art} ${new_name/--/-}
-      # Move the high_sierra bottle and json file
-      mv ${art} ${art/--/-}
-    done
-    for json in ${f}*.mojave.bottle*.json; do
-      sed -i '' s@high_sierra@mojave@g ${json}
-    done
-  done
-popd
+  popd
+fi
