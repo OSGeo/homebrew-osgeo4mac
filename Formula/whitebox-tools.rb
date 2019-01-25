f51d48170e349e7fa7a0d88b7ec982f75a046044
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-01-25 11:14:46 -0300

    test

--- Formula/whitebox-tools.rb
+++ Formula/whitebox-tools.rb
@@ -2,11 +2,11 @@ class WhiteboxTools < Formula
   desc "An advanced geospatial data analysis platform"
   homepage "https://www.uoguelph.ca/~hydrogeo/WhiteboxTools"
   url "https://github.com/jblindsay/whitebox-tools/releases/download/v0.13/WhiteboxTools_darwin_amd64.zip"
+  version "0.13.0"
   sha256 "fe79c3b797ba7ae23a525e11441b0d6406c339858b23271c143734c62af7ec27"
   # url "https://github.com/jblindsay/whitebox-tools.git",
   #   :branch => "master",
   #   :commit => "f0cf6af792de8f60dd8afb10e3f0a26df1c702c4"
-  version "0.13.0"
 
   bottle do
     root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
@@ -21,8 +21,8 @@ class WhiteboxTools < Formula
   # head "https://github.com/jblindsay/whitebox-tools.git", :branch => "master"
 
   def install
-    cp_r "#{buildpath}", "#{prefix}"
-    mkdir "#{bin}"
+    cp_r buildpath.to_s, prefix.to_s
+    mkdir bin.to_s
     ln_s "#{prefix}/WBT/whitebox_tools", "#{bin}/whitebox_tools"
   end
 
