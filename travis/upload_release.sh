979dc66604a3d71fb1640dd16c93dbda9ffce383
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-01-26 21:42:43 -0300

    delete old release tag

--- travis/upload_release.sh
+++ travis/upload_release.sh
@@ -4,6 +4,8 @@ brew install hub
 ls ./
 
 RELEASE_TAG=$(grep -Po "(\d+\.)+(\d+\.)+\d" ${HOMEBREW_REPOSITORY}/Library/Taps/${TRAVIS_REPO_SLUG}/Formula/${f}.rb | head -n 1)
+hub release delete "${RELEASE_TAG}"
+
 # tag_name="v${1}"
 
 asset_dir="./bottles"
