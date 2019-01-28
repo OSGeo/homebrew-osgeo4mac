5f314dfcff164527429c0bf12bedd36071b19d30
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-01-28 14:11:33 -0300

    qt-ifw-qt5: fix error - unknown version :mountain_lion (#634)

--- Formula/qt-ifw-qt5.rb
+++ Formula/qt-ifw-qt5.rb
@@ -10,7 +10,7 @@ class QtIfwQt5 < Formula
   # OS X 10.7 Lion is still supported in Qt 5.5, but is no longer a reference
   # configuration and thus untested in practice. Builds on OS X 10.7 have been
   # reported to fail: <https://github.com/Homebrew/homebrew/issues/45284>.
-  depends_on :macos => :mountain_lion
+  # depends_on :macos => :mountain_lion # error: unknown version 
   depends_on "pkg-config" => :build
   depends_on :xcode => :build
 
