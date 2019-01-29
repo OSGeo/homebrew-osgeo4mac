695a43cff1030c3d76b9913448444076f02a3e24
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-01-28 23:50:55 -0300

    add bottles

--- Formula/osgearth-qt4.rb
+++ Formula/osgearth-qt4.rb
@@ -1,18 +1,25 @@
 class OsgearthQt4 < Formula
   desc "Geospatial SDK and terrain engine for OpenSceneGraph"
   homepage "http://osgearth.org"
-  url "https://github.com/gwaldron/osgearth/archive/osgearth-2.7.tar.gz"
-  sha256 "945bd4d0bc65143a14caeb434b07384eccef1ba89ae11282fc499903a251ec18"
+  url "https://github.com/gwaldron/osgearth.git",
+    :branch => "2.7",
+    :commit => "dda0f0a92cedc83d6d40ed69cfb01140008f9911"
+  version "2.7.0"
+  # QGIS Globe Plugin does not support a larger version than OSGearh v2.7.
 
   # revision 1
 
   head "https://github.com/gwaldron/osgearth.git", :branch => "master"
 
   bottle do
-    cellar :any
-    sha256 "48cba11c49074ecbb6dda61a8a5a44881bb7aa121ecfb0eb9a61fb4eb5f05ad7" => :yosemite
-    sha256 "289d4169172f3a15c3e84b966fab8114bb0e92358af8274aecea8a098e923dda" => :mavericks
+    root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
+    sha256 "7080cdc8b0da8e250cc48fc3288236ed0366ad7beca38a638fca358a5986e551" => :mojave
+    sha256 "7080cdc8b0da8e250cc48fc3288236ed0366ad7beca38a638fca358a5986e551" => :high_sierra
+    sha256 "7080cdc8b0da8e250cc48fc3288236ed0366ad7beca38a638fca358a5986e551" => :sierra
   end
+
+  # The OSGearth 2.7 version is not being built with GDAL 2.x.
+  patch :DATA
 
   option "without-minizip", "Build without Google KMZ file access support"
   option "with-v8", "Build with Google's V8 JavaScript engine support"
@@ -20,17 +27,21 @@ class OsgearthQt4 < Formula
   option "with-docs-examples", "Build and install html documentation and examples"
 
   depends_on "cmake" => :build
-  depends_on "gdal"
+  # depends_on "gdal2"
   depends_on "sqlite"
   depends_on "qt-4"
   depends_on "minizip" => :recommended
   depends_on "v8" => :optional
   depends_on "tinyxml" => :optional
-  depends_on :macos => :mavericks
 
-  resource "sphinx" do
-    url "https://pypi.python.org/packages/source/S/Sphinx/Sphinx-1.2.1.tar.gz"
-    sha256 "182e5c81c3250e1752e744b6a35af4ef680bb6251276b49ef7d17f1d25e9ce70"
+  if (build.with? "docs-examples") && (!which("sphinx-build"))
+    # temporarily vendor a local sphinx install
+    sphinx_dir = prefix/"sphinx"
+    sphinx_site = sphinx_dir/"lib/python#{py_ver}/site-packages"
+    sphinx_site.mkpath
+    ENV.prepend_create_path "PYTHONPATH", sphinx_site
+    resource("Sphinx").stage { quiet_system "python#{py_ver}", "setup.py", "install", "--prefix=#{sphinx_dir}" }
+    ENV.prepend_path "PATH", sphinx_dir/"bin"
   end
 
   def install
@@ -46,9 +57,8 @@ class OsgearthQt4 < Formula
 
     args = std_cmake_args
 
-    args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.arch_64_bit}"
-
     args << "-DOSGEARTH_USE_QT=OFF"
+
     args << "-DWITH_EXTERNAL_TINYXML=ON" if build.with? "tinyxml"
 
     # v8 and minizip options should have empty values if not defined '--with'
@@ -93,3 +103,17 @@ class OsgearthQt4 < Formula
     system "#{bin}/osgearth_version"
   end
 end
+
+__END__
+
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -117,7 +117,7 @@
+ FIND_PACKAGE(OpenGL)
+
+ FIND_PACKAGE(CURL)
+-FIND_PACKAGE(GDAL)
++# FIND_PACKAGE(GDAL)
+ FIND_PACKAGE(GEOS)
+ FIND_PACKAGE(Sqlite3)
+ FIND_PACKAGE(ZLIB)
