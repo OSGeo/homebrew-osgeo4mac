b3d8841d0faf16b8cedd61936f7424bdf8549d2b
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-02-11 18:56:08 -0300

    fix travis deploy_key (#660)

--- Formula/whitebox-tools.rb
+++ Formula/whitebox-tools.rb
@@ -1,25 +1,11 @@
 class WhiteboxTools < Formula
   desc "An advanced geospatial data analysis platform"
-  homepage "https://www.uoguelph.ca/~hydrogeo/WhiteboxTools"
+  homepage "https://www.uoguelph.ca/~hydrogeo/WhiteboxTools" # https://github.com/jblindsay/whitebox-tools
   url "https://github.com/jblindsay/whitebox-tools/releases/download/v0.14.1/WhiteboxTools_darwin_amd64.zip"
-  # url "https://github.com/jblindsay/whitebox-tools.git",
-  #   :branch => "master",
-  #   :commit => "f0cf6af792de8f60dd8afb10e3f0a26df1c702c4"
-  version "0.14.0"
+  version "0.14.1"
   sha256 "1a96a037a83f3c1b83eff03c586ae6783bd6abcf04d8b0f9f0724cbe348352a8"
 
-  bottle do
-    root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "711ab8779ec3b1e0cdc2e1638a4bc93d840cc15bf13845ee50a5cc27ede7c866" => :mojave
-    sha256 "711ab8779ec3b1e0cdc2e1638a4bc93d840cc15bf13845ee50a5cc27ede7c866" => :high_sierra
-    sha256 "711ab8779ec3b1e0cdc2e1638a4bc93d840cc15bf13845ee50a5cc27ede7c866" => :sierra
-  end
-
-  revision 1
-
-  # head "https://github.com/jblindsay/whitebox-tools.git", :branch => "master"
+  revision 2
 
   def install
     cp_r buildpath.to_s, prefix.to_s
