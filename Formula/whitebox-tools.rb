9e195161abc1464a9d4126fa6743bd3040895054
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-02-11 20:12:37 -0300

    rebuild bottles for whitebox-tools

--- Formula/whitebox-tools.rb
+++ Formula/whitebox-tools.rb
@@ -1,11 +1,11 @@
 class WhiteboxTools < Formula
   desc "An advanced geospatial data analysis platform"
-  homepage "https://www.uoguelph.ca/~hydrogeo/WhiteboxTools" # https://github.com/jblindsay/whitebox-tools
+  homepage "https://www.uoguelph.ca/~hydrogeo/WhiteboxTools"
   url "https://github.com/jblindsay/whitebox-tools/releases/download/v0.14.1/WhiteboxTools_darwin_amd64.zip"
-  version "0.14.1"
   sha256 "1a96a037a83f3c1b83eff03c586ae6783bd6abcf04d8b0f9f0724cbe348352a8"
+  version "0.14.1"
 
-  revision 2
+  revision 3
 
   def install
     cp_r buildpath.to_s, prefix.to_s
