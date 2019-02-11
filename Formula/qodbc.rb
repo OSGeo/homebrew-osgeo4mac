c479b1dba6f649a8f9070aef164ae6cb5e1dc254
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-02-11 06:55:58 -0300

    Add QODBC formula

---
+++ Formula/qodbc.rb
@@ -0,0 +1,118 @@
+class Qodbc < Formula
+  desc "Qt SQL Database Driver - QODBC for Open Database Connectivity (ODBC)"
+  homepage "https://doc.qt.io/qt-5/sql-driver.html"
+  url "https://download.qt.io/official_releases/qt/5.12/5.12.0/single/qt-everywhere-src-5.12.0.tar.xz"
+  sha256 "356f42d9087718f22f03d13d0c2cdfb308f91dc3cf0c6318bed33f2094cd9d6c"
+
+  # revision 1
+
+  head "https://code.qt.io/qt/qt5.git", :branch => "5.12", :shallow => false
+
+  depends_on "pkg-config" => :build
+  depends_on "libiconv"
+  depends_on "gettext"
+  depends_on "libxml2"
+  depends_on "libxslt"
+  depends_on "zlib"
+  depends_on "qt"
+  depends_on "sqlite"
+  depends_on "openssl"
+  depends_on "unixodbc"
+  depends_on :xcode => :build
+
+  def install
+    qt_plugins = lib/"qt/plugins"
+    (qt_plugins/"sqldrivers").mkpath
+
+    chdir "#{buildpath}/qtbase/src/plugins/sqldrivers" do
+      system "#{Formula["qt"].opt_bin}/qmake", "--", "ODBC_PREFIX=#{Formula["unixodbc"].opt_prefix}"
+      system "make"
+
+      # copy libqsqlodbc.dylib
+      # libqsqlite.dylib from sqlite
+      cp_r "#{buildpath}/qtbase/src/plugins/sqldrivers/plugins/sqldrivers/libqsqlodbc.dylib", "#{lib}/qt/plugins/sqldrivers/"
+    end
+  end
+
+  def caveats; <<~EOS
+    Plugins generated are linked to the following directory:
+
+      #{HOMEBREW_PREFIX}/lib/qt/plugins/sqldrivers
+
+    EOS
+  end
+
+  test do
+    (testpath/"hello.pro").write <<~EOS
+      QT        += core sql
+      QT        -= gui
+      TARGET     = hello
+      CONFIG    += console
+      CONFIG    -= app_bundle
+      TEMPLATE   = app
+      HEADERS    = client.h
+      SOURCES   += client.cpp main.cpp
+    EOS
+
+    (testpath/"client.h").write <<~EOS
+    #ifndef CLIENT_H
+    #define CLIENT_H
+    #include <QObject>
+    #include <QSqlDatabase>
+    #include <QString>
+    class Client : public QObject
+    {
+      Q_OBJECT
+    public:
+      Client(QObject *parent = 0);
+    public slots:
+      void checkSqlDriver();
+    };
+    #endif // CLIENT_H
+    EOS
+
+    (testpath/"client.cpp").write <<~EOS
+    #include "client.h"
+    #include <QCoreApplication>
+    #include <QDebug>
+    #include <QSqlDatabase>
+    #include <QStringList>
+    Client::Client(QObject *parent)
+      : QObject(parent)
+    {
+    }
+    void Client::checkSqlDriver()
+    {
+      QCoreApplication::addLibraryPath("#{HOMEBREW_PREFIX}/lib/qt/plugins");
+      QCoreApplication::addLibraryPath("#{lib}/qt/plugins");
+      qDebug() << "QSqlDatabase::drivers(): " << QSqlDatabase::drivers().join(" ");
+      QCoreApplication::exit(!QSqlDatabase::isDriverAvailable("QODBC"));
+    }
+    EOS
+
+    (testpath/"main.cpp").write <<~EOS
+      #include <QCoreApplication>
+      #include <QDebug>
+      #include <QTimer>
+      #include "client.h"
+      int main(int argc, char *argv[])
+      {
+        QCoreApplication app(argc, argv);
+        Client c(app.instance());
+        qDebug() << "Running application";
+        QTimer::singleShot(1000, &c, SLOT(checkSqlDriver()));
+        return app.exec();
+      }
+    EOS
+
+    cd testpath do
+      system Formula["qt"].bin/"qmake", "hello.pro"
+      system "make"
+      assert_predicate testpath/"client.o", :exist?
+      assert_predicate testpath/"moc_client.o", :exist?
+      assert_predicate testpath/"main.o", :exist?
+      assert_predicate testpath/"hello", :exist?
+      system "./hello"
+    end
+  end
+end
