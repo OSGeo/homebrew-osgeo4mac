68309c783d021a726292db8b43345a466245fedf
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-02-12 15:41:43 -0300

    fix build

--- Formula/qgis-res.rb
+++ Formula/qgis-res.rb
@@ -807,10 +807,12 @@ class QgisRes < Formula
   def install
     # install python environment
     venv = virtualenv_create(libexec/'vendor', "#{Formula["python"].opt_bin}/python3")
-    res = resources.map(&:name).to_set - %w[pyRscript rpy2] # python-dateutil
+    res = resources.map(&:name).to_set - %w[Shapely Rtree pyRscript rpy2] # python-dateutil
 
     # fix pip._vendor.pep517.wrappers.BackendUnavailable
     system libexec/"vendor/bin/pip3", "install", "--upgrade", "-v", "setuptools", "pip<19.0.0", "wheel"
+    venv.pip_install_and_link "Shapely"
+    venv.pip_install_and_link "Rtree"
     # venv.pip_install_and_link "python-dateutil"
 
     res.each do |r|
