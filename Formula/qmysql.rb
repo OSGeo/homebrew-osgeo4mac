bf83ac563817a71789490a50a859d379499e1607
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-02-22 13:22:57 -0300

    Upgrade QMYSQL to 5.12.1 (#716)

--- Formula/qmysql.rb
+++ Formula/qmysql.rb
@@ -1,10 +1,10 @@
 class Qmysql < Formula
   desc "Qt SQL Database Driver - QMYSQL for MySQL 4 and higher"
   homepage "https://doc.qt.io/qt-5/sql-driver.html"
-  url "https://download.qt.io/official_releases/qt/5.12/5.12.0/single/qt-everywhere-src-5.12.0.tar.xz"
-  sha256 "356f42d9087718f22f03d13d0c2cdfb308f91dc3cf0c6318bed33f2094cd9d6c"
+  url "https://download.qt.io/official_releases/qt/5.12/5.12.1/single/qt-everywhere-src-5.12.1.tar.xz"
+  sha256 "caffbd625c7bc10ff8c5c7a27dbc7d84fa4de146975c0e1ffe904b514ccd6da4"
 
-  revision 1
+  # revision 1
 
   head "https://code.qt.io/qt/qt5.git", :branch => "5.12", :shallow => false
 
