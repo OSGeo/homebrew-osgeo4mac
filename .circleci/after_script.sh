170415282c8aad5d86b893104d30a51a9bf480a3
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-02-24 21:12:51 -0300

    fix

--- .circleci/after_script.sh
+++ .circleci/after_script.sh
@@ -14,6 +14,39 @@
 #   (at your option) any later version.                                   #
 #                                                                         #
 ###########################################################################
+set -e
+
+ls -lah /tmp/workspace/bottles/
+
+# Setup Git configuration
+COMMIT_USER=$(git log --format='%an' ${CIRCLE_SHA1}^\!)
+COMMIT_EMAIL=$(git log --format='%ae' ${CIRCLE_SHA1}^\!)
+git config user.name "Geo Ninja"
+git config user.email "qgisninja@gmail.com"
+REPO=$(git config remote.origin.url)
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+# Checkout on the proper branch
+# see https://gist.github.com/mitchellkrogza/a296ab5102d7e7142cc3599fca634203
+head_ref=$(git rev-parse HEAD)
+if [[ $? -ne 0 || ! $head_ref ]]; then
+    err "failed to get HEAD reference"
+    return 1
+fi
+branch_ref=$(git rev-parse "$CIRCLE_BRANCH")
+if [[ $? -ne 0 || ! $branch_ref ]]; then
+    err "failed to get $CIRCLE_BRANCH reference"
+    return 1
+fi
+if [[ $head_ref != $branch_ref ]]; then
+    msg "HEAD ref ($head_ref) does not match $CIRCLE_BRANCH ref ($branch_ref)"
+    err "someone may have pushed new commits before this build cloned the repo"
+    return 1
+fi
+if ! git checkout "$CIRCLE_BRANCH"; then
+    err "failed to checkout $CIRCLE_BRANCH"
+    return 1
+fi
 
 # Build the actual bottles
 # In Travis, this used to be part of the deploy phase, but now it needs to run as part of the original build process, but only on master.
@@ -65,3 +98,15 @@ pushd /tmp/bottles
     fi
   done
 popd
+
+# Now do the commit and push
+echo "Commit"
+git add -vA Formula/*.rb
+git commit -m "Updated bottles for: $BUILT_BOTTLES
+
+Committed for ${COMMIT_USER}<${COMMIT_EMAIL}>
+[ci skip]"
+
+echo "Push"
+# Now that we're all set up, we can push.
+git push $SSH_REPO $CIRCLE_BRANCH
--- .circleci/before_deploy.sh
+++ .circleci/before_deploy.sh
@@ -76,22 +76,22 @@ fi
 
 # Set up the keys
 # Decrypt the circle_deploy_key.enc key into /tmp/circle_deploy_key
-# echo "Decrypt key"
+echo "Decrypt key"
 # openssl aes-256-cbc -d -K $REPO_ENC_KEY -iv $REPO_ENC_IV -in circle_deploy_key.enc -out /tmp/circle_deploy_key
 # openssl aes-256-cbc -K $REPO_ENC_KEY -iv $REPO_ENC_IV -in circle_deploy_key.enc -out /tmp/circle_deploy_key -d
 # openssl aes-256-cbc -iv "$REPO_ENC_IV" -K "$REPO_ENC_KEY" -d -in circle_deploy_key.enc -out /tmp/circle_deploy_key
 # openssl aes-256-cbc -k $SSH_PASSPHRASE -d -in circle_deploy_key.enc -out /tmp/circle_deploy_key
-# openssl aes-256-cbc -d -in circle_deploy_key.enc -out /tmp/circle_deploy_key -k $SSH_PASSPHRASE
+# openssl aes-256-cbc -d -in circle_deploy_key.enc -out /tmp/circle_deploy_key -k $SSH_PASSPHRASE
 
 # Make sure only the current user can read the private key
-# chmod 600 /tmp/circle_deploy_key
+chmod 600 /tmp/circle_deploy_key
 # Create a script to return the passphrase environment variable to ssh-add
-# echo 'echo ${SSH_PASSPHRASE}' > /tmp/askpass && chmod +x /tmp/askpass
+echo 'echo ${SSH_PASSPHRASE}' > /tmp/askpass && chmod +x /tmp/askpass
 # Start the authentication agent
-# eval "$(ssh-agent -s)"
+eval "$(ssh-agent -s)"
 # Add the key to the authentication agent
-# brew install util-linux # for setsid
-# DISPLAY=":0.0" SSH_ASKPASS="/tmp/askpass" setsid ssh-add /tmp/circle_deploy_key </dev/null
+brew install util-linux # for setsid
+DISPLAY=":0.0" SSH_ASKPASS="/tmp/askpass" setsid ssh-add /tmp/circle_deploy_key </dev/null
 # ssh-add /tmp/circle_deploy_key
 # checkout, restore_cache, run: yarn install, save_cache, etc.
 # Run semantic-release after all the above is set.
