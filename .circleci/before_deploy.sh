0be394786df6ccb1cc791baaff42d5e57dfaa976
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-02-25 06:15:14 -0300

    test deploy

--- .circleci/before_deploy.sh
+++ .circleci/before_deploy.sh
@@ -18,10 +18,9 @@
 set -e
 
 pwd
-
 ls
 
-# ls -lah bottles/
+# ls -lah /tmp/workspace/bottles/
 
 # Setup Git configuration
 COMMIT_USER=$(git log --format='%an' ${CIRCLE_SHA1}^\!)
@@ -53,66 +52,50 @@ if ! git checkout "$CIRCLE_BRANCH"; then
     return 1
 fi
 
-BUILT_BOTTLES=
-mkdir -p bottles
-
-pushd bottles
-  ls
-  for f in ${CHANGED_FORMULAE};do
-    echo "Updating changed formula ${f} with new bottles..."
-
-    # Do Merge bottles with the formula
-    # Don't commit anything, we'll do that after updating all the formulae
-    # Catch the eror and store it to a variable
-    # brew bottle --merge --write --no-commit ${f}*.json
-    if result=$(brew bottle --merge --write --no-commit ${f}*.json 2>&1); then
-      BUILT_BOTTLES="$BUILT_BOTTLES ${f}"
-    else
-      # If there's an error, remove the json and bottle files, we don't want them anymore.
-      echo "Unable to bottle ${f}"
-      echo $result
-      rm ${f}*.json
-      rm ${f}*.tar.gz
-    fi
-  done
-  ls
-popd
+# BUILT_BOTTLES=
+# pushd /tmp/workspace/bottles/
+#   for f in ${CHANGED_FORMULAE};do
+#     echo "Updating changed formula ${f} with new bottles..."
+#
+#     # Do Merge bottles with the formula
+#     # Don't commit anything, we'll do that after updating all the formulae
+#     # Catch the eror and store it to a variable
+#     if result=$(brew bottle --merge --write --no-commit ${f}*.json 2>&1); then
+#       BUILT_BOTTLES="$BUILT_BOTTLES ${f}"
+#     else
+#      # If there's an error, remove the json and bottle files, we don't want them anymore.
+#      echo "Unable to bottle ${f}"
+#      echo $result
+#      rm ${f}*.json
+#      rm ${f}*.tar.gz
+#     fi
+#   done
+# popd
 
 # Set up the keys
 # Decrypt the circle_deploy_key.enc key into /tmp/circle_deploy_key
-# echo "Decrypt key"
 # openssl aes-256-cbc -d -K $REPO_ENC_KEY -iv $REPO_ENC_IV -in circle_deploy_key.enc -out /tmp/circle_deploy_key
-# openssl aes-256-cbc -K $REPO_ENC_KEY -iv $REPO_ENC_IV -in circle_deploy_key.enc -out /tmp/circle_deploy_key -d
-# openssl aes-256-cbc -iv "$REPO_ENC_IV" -K "$REPO_ENC_KEY" -d -in circle_deploy_key.enc -out /tmp/circle_deploy_key
-# openssl aes-256-cbc -k $SSH_PASSPHRASE -d -in circle_deploy_key.enc -out /tmp/circle_deploy_key
-# openssl aes-256-cbc -d -in circle_deploy_key.enc -out /tmp/circle_deploy_key -k $SSH_PASSPHRASE
 # Make sure only the current user can read the private key
 # chmod 600 /tmp/circle_deploy_key
 # Create a script to return the passphrase environment variable to ssh-add
-# echo 'echo ${SSH_PASSPHRASE}' > /tmp/askpass && chmod +x /tmp/askpass
+# echo 'echo ${SSH_PASSPHRASE}' > /tmp/askpass && chmod +x /tmp/askpass
 # Start the authentication agent
-# eval "$(ssh-agent -s)"
+# eval "$(ssh-agent -s)"
 # Add the key to the authentication agent
-# brew install util-linux # for setsid
-# DISPLAY=":0.0" SSH_ASKPASS="/tmp/askpass" setsid ssh-add /tmp/circle_deploy_key </dev/null
-# ssh-add /tmp/circle_deploy_key
+# brew install util-linux # for setsid
+# DISPLAY=":0.0" SSH_ASKPASS="/tmp/askpass" setsid ssh-add /tmp/circle_deploy_key </dev/null
 # checkout, restore_cache, run: yarn install, save_cache, etc.
 # Run semantic-release after all the above is set.
 
-cd ${HOMEBREW_REPOSITORY}/Library/Taps/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}
-
-# Add the mirror repository as a git remote
-# git remote set-url origin git@github.com:OSGeo/homebrew-osgeo4mac.git
+# cd ${HOMEBREW_REPOSITORY}/Library/Taps/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}
 
 # Now do the commit and push
-echo "Commit"
-git add -vA Formula/*.rb
-git commit -m "Updated bottles for: $BUILT_BOTTLES
 
-Committed for ${COMMIT_USER}<${COMMIT_EMAIL}>
-[ci skip]"
+# git add -vA Formula/*.rb
+# git commit -m "Updated bottles for: ${BUILT_BOTTLES}
 
-echo "Push"
+# Committed for ${COMMIT_USER}<${COMMIT_EMAIL}>
+# [ci skip]"
+
 # Now that we're all set up, we can push.
-git push $SSH_REPO $CIRCLE_BRANCH
-# git push --set-upstream $SSH_REPO $CIRCLE_BRANCH
+git push ${SSH_REPO} $CIRCLE_BRANCH
