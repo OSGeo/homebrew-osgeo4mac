3bbe494d2209dd0e56225d0b5f4c1f2a823c1bea
geo-ninja <qgisninja@gmail.com>
2019-02-27 22:39:34 -0800

    Updated bottles for:  whitebox-tools
      Committed for JF Perini<21315242+fjperini@users.noreply.github.com>
      [ci skip]

--- Formula/whitebox-tools.rb
+++ Formula/whitebox-tools.rb
@@ -10,8 +10,9 @@ class WhiteboxTools < Formula
   bottle do
     root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
     cellar :any_skip_relocation
-    sha256 "8b206d5f6411c0a2a083ff273252a07c792fdf12b83bd704301739fd24d85971" => :mojave
-    sha256 "8b206d5f6411c0a2a083ff273252a07c792fdf12b83bd704301739fd24d85971" => :high_sierra
+    rebuild 1
+    sha256 "8e22be2965d8acd98a66e4c8f97a5104cbb6b1fabd21c0af356e7f98a9832847" => :mojave
+    sha256 "8e22be2965d8acd98a66e4c8f97a5104cbb6b1fabd21c0af356e7f98a9832847" => :high_sierra
   end
 
   def install
