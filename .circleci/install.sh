49b69dfa101a3c1535538bfc21f65578eb081110
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-03-05 18:12:17 -0300

    fix tap

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -22,7 +22,6 @@ rm /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/pyqt.rb
 rm /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/qscintilla2.rb
 
 brew tap osgeo/osgeo4mac
-
 brew tap-pin osgeo/osgeo4mac
 
 for f in ${CHANGED_FORMULAE};do
