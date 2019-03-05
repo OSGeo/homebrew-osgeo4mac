4c5405646b2a36133aa92bcb5285d9bcce7f972a
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-03-04 22:52:21 -0300

    update

--- Formula/qt-4.rb
+++ Formula/qt-4.rb
@@ -1,4 +1,4 @@
-class Qt4 < Formula
+class QtAT4 < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
   url "https://download.qt.io/archive/qt/4.8/4.8.7/qt-everywhere-opensource-src-4.8.7.tar.gz"
@@ -106,9 +106,9 @@ class Qt4 < Formula
 
     args = %W[
       -prefix #{prefix}
-      -plugindir #{prefix}/lib/qt-4/plugins
-      -importdir #{prefix}/lib/qt-4/imports
-      -datadir #{prefix}/etc/qt-4
+      -plugindir #{prefix}/lib/qt4/plugins
+      -importdir #{prefix}/lib/qt4/imports
+      -datadir #{prefix}/etc/qt4
       -release
       -opensource
       -confirm-license
@@ -168,8 +168,8 @@ class Qt4 < Formula
     args << "-no-webkit" if build.without? "webkit"
 
     # Patch macdeployqt so it finds the plugin path
-    inreplace "tools/macdeployqt/macdeployqt/main.cpp", '"/Developer/Applications/Qt/plugins"', "\"#{HOMEBREW_PREFIX}/lib/qt-4/plugins\""
-    inreplace "tools/macdeployqt/macdeployqt/main.cpp", 'deploymentInfo.qtPath + "/plugins"', "\"#{HOMEBREW_PREFIX}/lib/qt-4/plugins\""
+    inreplace "tools/macdeployqt/macdeployqt/main.cpp", '"/Developer/Applications/Qt/plugins"', "\"#{HOMEBREW_PREFIX}/lib/qt4/plugins\""
+    inreplace "tools/macdeployqt/macdeployqt/main.cpp", 'deploymentInfo.qtPath + "/plugins"', "\"#{HOMEBREW_PREFIX}/lib/qt4/plugins\""
 
     system "./configure", *args
     system "make"
@@ -187,9 +187,9 @@ class Qt4 < Formula
     inreplace "configure", '=$QT_INSTALL_HEADERS"`', "=#{HOMEBREW_PREFIX}/include\"`"
     inreplace "configure", '=$QT_INSTALL_LIBS"`', "=#{HOMEBREW_PREFIX}/lib\"`"
     inreplace "configure", '=$QT_INSTALL_BINS"`', "=#{HOMEBREW_PREFIX}/bin\"`"
-    inreplace "configure", '=$QT_INSTALL_PLUGINS"`', "=#{HOMEBREW_PREFIX}/lib/qt-4/plugins\"`"
-    inreplace "configure", '=$QT_INSTALL_IMPORTS"`', "=#{HOMEBREW_PREFIX}/lib/qt-4/imports\"`"
-    inreplace "configure", '=$QT_INSTALL_DATA"`', "=#{HOMEBREW_PREFIX}/etc/qt-4\"`"
+    inreplace "configure", '=$QT_INSTALL_PLUGINS"`', "=#{HOMEBREW_PREFIX}/lib/qt4/plugins\"`"
+    inreplace "configure", '=$QT_INSTALL_IMPORTS"`', "=#{HOMEBREW_PREFIX}/lib/qt4/imports\"`"
+    inreplace "configure", '=$QT_INSTALL_DATA"`', "=#{HOMEBREW_PREFIX}/etc/qt4\"`"
     inreplace "configure", '=$QT_INSTALL_SETTINGS"`', "=#{HOMEBREW_PREFIX}\"`"
 
     # Run ./configure again, to rebuild qmake
@@ -212,17 +212,17 @@ class Qt4 < Formula
       include.install_symlink path => path.parent.basename(".framework")
     end
 
-    # Make `HOMEBREW_PREFIX/lib/qt-4/plugins` an additional plug-in search path
+    # Make `HOMEBREW_PREFIX/lib/qt4/plugins` an additional plug-in search path
     # for Qt Designer to support formulae that provide Qt Designer plug-ins.
     system "/usr/libexec/PlistBuddy",
-            "-c", "Add :LSEnvironment:QT_PLUGIN_PATH string \"#{HOMEBREW_PREFIX}/lib/qt-4/plugins\"",
+            "-c", "Add :LSEnvironment:QT_PLUGIN_PATH string \"#{HOMEBREW_PREFIX}/lib/qt4/plugins\"",
            "#{bin}/Designer.app/Contents/Info.plist"
 
     Pathname.glob("#{bin}/*.app") { |app| mv app, prefix }
   end
 
   def post_install
-    (HOMEBREW_PREFIX/"lib/qt-4/plugins/designer").mkpath
+    (HOMEBREW_PREFIX/"lib/qt4/plugins/designer").mkpath
   end
 
   def caveats; <<~EOS
@@ -231,7 +231,7 @@ class Qt4 < Formula
 
     Qt Designer no longer picks up changes to the QT_PLUGIN_PATH environment
     variable as it was tweaked to search for plug-ins provided by formulae in
-      #{HOMEBREW_PREFIX}/lib/qt-4/plugins
+      #{HOMEBREW_PREFIX}/lib/qt4/plugins
 
     Phonon is not supported on macOS Sierra or with Xcode 8.
     EOS
