d60ee354642f3b2d17093303e1cc39520e65cf6b
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-03-06 01:42:39 -0300

    Update qscintilla2.rb

--- Formula/qscintilla2.rb
+++ Formula/qscintilla2.rb
@@ -12,7 +12,7 @@ class Qscintilla2 < Formula
     sha256 "75ec91b6d960882cda9355ec94c35e2f2b2468b4ea389ff75e3cc0554794a0f5" => :sierra
   end
 
-  revision 1
+  revision 2
 
   depends_on "python"
   depends_on "python@2"
