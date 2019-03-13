ef3623fb02be3b6def36e7ebef8d997610cf8f9a
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-03-13 17:10:14 -0300

    Update osgeo-sip.rb [ci skip]

--- Formula/osgeo-sip.rb
+++ Formula/osgeo-sip.rb
@@ -4,7 +4,7 @@ class OsgeoSip < Formula
   url "https://www.riverbankcomputing.com/static/Downloads/sip/sip-4.19.14.tar.gz"
   sha256 "0ef3765dbcc3b8131f83e60239f49508f82205b33cae5408c405e2e2f2d0af87"
 
-  # revision 1
+  revision 1
 
   head "https://www.riverbankcomputing.com/hg/sip", :using => :hg
 
@@ -20,10 +20,11 @@ class OsgeoSip < Formula
       # build.py can use it to figure out a version number.
       ln_s cached_download/".hg", ".hg"
       # build.py doesn't run with python3
-      system "#{Formula["python"].opt_bin}/python3", "build.py", "prepare"
+      system "python", "build.py", "prepare"
     end
 
     ["#{Formula["python@2"].opt_bin}/python2", "#{Formula["python"].opt_bin}/python3"].each do |python|
+
       version = Language::Python.major_minor_version python
       system python, "configure.py",
                      "--deployment-target=#{MacOS.version}",
@@ -49,57 +50,10 @@ class OsgeoSip < Formula
   end
 
   test do
-    # system "#{Formula["python"].opt_bin}/python3", "-c", "import PyQt5.sip"
-    (testpath/"test.h").write <<~EOS
-      #pragma once
-      class Test {
-      public:
-        Test();
-        void test();
-      };
-    EOS
-    (testpath/"test.cpp").write <<~EOS
-      #include "test.h"
-      #include <iostream>
-      Test::Test() {}
-      void Test::test()
-      {
-        std::cout << "Hello World!" << std::endl;
-      }
-    EOS
-    (testpath/"test.sip").write <<~EOS
-      %Module test
-      class Test {
-      %TypeHeaderCode
-      #include "test.h"
-      %End
-      public:
-        Test();
-        void test();
-      };
-    EOS
-    (testpath/"generate.py").write <<~EOS
-      from sipconfig import SIPModuleMakefile, Configuration
-      m = SIPModuleMakefile(Configuration(), "test.build")
-      m.extra_libs = ["test"]
-      m.extra_lib_dirs = ["."]
-      m.generate()
-    EOS
-    (testpath/"run.py").write <<~EOS
-      from test import Test
-      t = Test()
-      t.test()
-    EOS
-    system ENV.cxx, "-shared", "-Wl,-install_name,#{testpath}/libtest.dylib",
-                    "-o", "libtest.dylib", "test.cpp"
-    system bin/"sip", "-b", "test.build", "-c", ".", "test.sip"
-
-    ["python2", "python3"].each do |python|
+    ["#{Formula["python@2"].opt_bin}/python2", "#{Formula["python"].opt_bin}/python3"].each do |python|
       version = Language::Python.major_minor_version python
       ENV["PYTHONPATH"] = lib/"python#{version}/site-packages"
-      system python, "generate.py"
-      system "make", "-j1", "clean", "all"
-      system python, "run.py"
+      system python, "-c", "import PyQt5.sip"
     end
   end
 end
