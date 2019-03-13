bd1a38a857a8220b198e877e8cac4aa7d9f2f0cd
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-03-13 01:55:45 -0300

    Update osgeo-laszip@2.rb [ci skip]

--- Formula/osgeo-laszip@2.rb
+++ Formula/osgeo-laszip@2.rb
@@ -4,7 +4,7 @@ class OsgeoLaszipAT2 < Formula
   url "https://github.com/LASzip/LASzip/archive/v2.2.0.tar.gz"
   sha256 "b8e8cc295f764b9d402bc587f3aac67c83ed8b39f1cb686b07c168579c61fbb2"
 
-  # revision 1
+  revision 1
 
   head "https://github.com/LASzip/LASzip.git", :tag => "v2.2.0"
 
