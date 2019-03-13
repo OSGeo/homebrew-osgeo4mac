b52988d3cf402b4b8dc5bb02a97d34b556df6b98
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-03-13 14:41:58 -0300

    Update osgeo-saga-lts.rb [ci skip]

--- Formula/osgeo-saga-lts.rb
+++ Formula/osgeo-saga-lts.rb
@@ -6,7 +6,7 @@ class OsgeoSagaLts < Formula
       :revision => "b6f474f8af4af7f0ff82548cc6f88c53547d91f5"
   version "2.3.2"
 
-  # revision 1
+  revision 1
 
   head "https://git.code.sf.net/p/saga-gis/code.git", :branch => "release-2-3-lts"
 
