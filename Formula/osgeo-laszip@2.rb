ac10b3fb7c767a69059b06cdccea3dd405127bb9
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-03-13 01:53:06 -0300

    Rename laszip@2.2.rb [ci skip]

--- Formula/osgeo-laszip@2.rb
+++ Formula/osgeo-laszip@2.rb
@@ -1,15 +1,12 @@
-class LaszipAT22 < Formula
+class OsgeoLaszipAT2 < Formula
   desc "Lossless LiDAR compression"
   homepage "https://www.laszip.org/"
   url "https://github.com/LASzip/LASzip/archive/v2.2.0.tar.gz"
   sha256 "b8e8cc295f764b9d402bc587f3aac67c83ed8b39f1cb686b07c168579c61fbb2"
 
-  bottle do
-    root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
-    cellar :any
-    sha256 "76b6613dc66169e90b3a8857f0c24bc2f7601a9659b186619074088ee47046e5" => :high_sierra
-    sha256 "76b6613dc66169e90b3a8857f0c24bc2f7601a9659b186619074088ee47046e5" => :sierra
-  end
+  # revision 1
+
+  head "https://github.com/LASzip/LASzip.git", :tag => "v2.2.0"
 
   keg_only :versioned_formula
 
