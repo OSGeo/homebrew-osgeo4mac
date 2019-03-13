25d7083ad4ea7f6347df080f2d39fbc2b9da40e2
geo-ninja <qgisninja@gmail.com>
2019-03-12 22:14:27 -0700

    Updated bottles for:  osgeo-laszip@2
      Committed for JF Perini<21315242+fjperini@users.noreply.github.com>
      [ci skip]

--- Formula/osgeo-laszip@2.rb
+++ Formula/osgeo-laszip@2.rb
@@ -8,6 +8,14 @@ class OsgeoLaszipAT2 < Formula
 
   head "https://github.com/LASzip/LASzip.git", :tag => "v2.2.0"
 
+  bottle do
+    root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
+    cellar :any
+    sha256 "a785cd4b90dc00fb0454badb64aecab0ad033c7bae47e40afaa77fbd548c1f11" => :mojave
+    sha256 "a785cd4b90dc00fb0454badb64aecab0ad033c7bae47e40afaa77fbd548c1f11" => :high_sierra
+    sha256 "03096e03a39bd9605f8efc66d43299a794b5596ab58d56ca6b8a72b0498d6e8a" => :sierra
+  end
+
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
