f111f201491e9bdb2afbbbd8b602e4e3972e2ec3
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-03-14 15:46:23 -0300

    Rename whitebox-tools.rb [ci skip]

---
+++ Formula/osgeo-whitebox-tools.rb
@@ -0,0 +1,19 @@
+class OsgeoWhiteboxTools < Formula
+  desc "An advanced geospatial data analysis platform"
+  homepage "https://www.uoguelph.ca/~hydrogeo/WhiteboxTools"
+  url "https://github.com/jblindsay/whitebox-tools/releases/download/v0.15/WhiteboxTools_darwin_amd64.zip"
+  sha256 "6e425ddf43f1c9cdcd9f00c9766a66fd42f5477aac30a5bc9b96784f9fb4c026"
+  version "0.14.1"
+
+  # revision 1
+
+  def install
+    cp_r buildpath.to_s, prefix.to_s
+    mkdir bin.to_s
+    ln_s "#{prefix}/WBT/whitebox_tools", "#{bin}/whitebox_tools"
+  end
+
+  test do
+    system "#{bin}/whitebox_tools", "--toolbox=Slope"
+  end
+end
