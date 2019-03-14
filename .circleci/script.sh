682a724e8e7b9f2722f400858abb3bb71302b065
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-03-13 21:58:09 -0300

    fix

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -19,28 +19,28 @@ set -e
 
 ulimit -n 1024
 
+# deps=$(brew deps --include-build ${f})
+
+# fix error: Unable to import PyQt5.QtCore
+# build qscintilla2
+# if [ "$(echo ${deps} | grep -c 'osgeo-pyqt')" != "0" ];then
+brew reinstall osgeo-pyqt
+brew unlink osgeo-pyqt && brew link osgeo-pyqt --force
+system "python2", "-c", '"import PyQt5.QtCore"'
+system "python3", "-c", '"import PyQt5.QtCore"'
+brew uninstall osgeo-pyqt
+# fi
+
+# fix error: 'libintl.h' file not found
+# build qgis with grass
+# if [ "$(echo ${deps} | grep -c 'osgeo-grass')" != "0" ];then
+brew reinstall gettext
+brew unlink gettext && brew link --force gettext
+# fi
+
 echo ${CHANGED_FORMULAE}
 
 for f in ${CHANGED_FORMULAE};do
-  deps=$(brew deps --include-build ${f})
-
-  # fix error: Unable to import PyQt5.QtCore
-  # build qscintilla2
-  # if [ "$(echo ${deps} | grep -c 'osgeo-pyqt')" != "0" ];then
-    brew reinstall osgeo-pyqt
-    brew unlink osgeo-pyqt && brew link osgeo-pyqt --force
-    system "python2", "-c", '"import PyQt5.QtCore"'
-    system "python3", "-c", '"import PyQt5.QtCore"'
-    brew uninstall osgeo-pyqt
-  # fi
-
-  # fix error: 'libintl.h' file not found
-  # build qgis with grass
-  # if [ "$(echo ${deps} | grep -c 'osgeo-grass')" != "0" ];then
-    brew reinstall gettext
-    brew unlink gettext && brew link --force gettext
-  # fi
-
   # if [[ $(brew list --versions ${f}) ]]; then
   #   echo "Clearing previously installed/cached formula ${f}..."
   #   brew uninstall --force --ignore-dependencies ${f} || true
--- travis/script.sh
+++ travis/script.sh
@@ -17,26 +17,26 @@
 
 set -e
 
+# deps=$(brew deps --include-build ${f})
+
+# fix error: Unable to import PyQt5.QtCore
+# build qscintilla2
+# if [ "$(echo ${deps} | grep -c 'osgeo-pyqt')" != "0" ];then
+brew reinstall osgeo-pyqt
+brew unlink osgeo-pyqt && brew link osgeo-pyqt --force
+system "python2", "-c", '"import PyQt5.QtCore"'
+system "python3", "-c", '"import PyQt5.QtCore"'
+brew uninstall osgeo-pyqt
+# fi
+
+# fix error: 'libintl.h' file not found
+# build qgis with grass
+# if [ "$(echo ${deps} | grep -c 'osgeo-grass')" != "0" ];then
+brew reinstall gettext
+brew unlink gettext && brew link --force gettext
+# fi
+
 for f in ${CHANGED_FORMULAE};do
-  deps=$(brew deps --include-build ${f})
-
-  # fix error: Unable to import PyQt5.QtCore
-  # build qscintilla2
-  # if [ "$(echo ${deps} | grep -c 'osgeo-pyqt')" != "0" ];then
-    brew reinstall osgeo-pyqt
-    brew unlink osgeo-pyqt && brew link osgeo-pyqt --force
-    system "python2", "-c", '"import PyQt5.QtCore"'
-    system "python3", "-c", '"import PyQt5.QtCore"'
-    brew uninstall osgeo-pyqt
-  # fi
-
-  # fix error: 'libintl.h' file not found
-  # build qgis with grass
-  # if [ "$(echo ${deps} | grep -c 'osgeo-grass')" != "0" ];then
-    brew reinstall gettext
-    brew unlink gettext && brew link --force gettext
-  # fi
-
   # if [[ $(brew list --versions ${f}) ]]; then
   #   echo "Clearing previously installed/cached formula ${f}..."
   #   brew uninstall --force --ignore-dependencies ${f} || true
