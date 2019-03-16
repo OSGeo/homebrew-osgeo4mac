130f62d8261c248d45c4f103d7478a9ea9d1c1f7
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-03-16 18:45:50 -0300

    Update osgeo-insighttoolkit.rb [ci skip]

--- Formula/osgeo-insighttoolkit.rb
+++ Formula/osgeo-insighttoolkit.rb
@@ -6,6 +6,14 @@ class OsgeoInsighttoolkit < Formula
 
   revision 1
 
+  bottle do
+    root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
+    cellar :any
+    sha256 "821a66bfa2f00ac11d6ee2b58215714593033cc06b452b2485ffa8820b1732e4" => :mojave
+    sha256 "821a66bfa2f00ac11d6ee2b58215714593033cc06b452b2485ffa8820b1732e4" => :high_sierra
+    sha256 "821a66bfa2f00ac11d6ee2b58215714593033cc06b452b2485ffa8820b1732e4" => :sierra
+  end
+
   head "git://itk.org/ITK.git"
 
   option :cxx11
@@ -24,10 +32,11 @@ class OsgeoInsighttoolkit < Formula
   depends_on "python" => :recommended
   depends_on "fftw" => :recommended
   depends_on "hdf5" => [:recommended] + cxx11dep
-  depends_on "jpeg" => :recommended
+  # depends_on "jpeg" => :recommended
+  depends_on "libjpeg-turbo" => :recommended
   depends_on "libpng" => :recommended
   depends_on "libtiff" => :recommended
-  depends_on "gdcm" => [:recommended] + cxx11dep
+  depends_on "gdcm" => [:optional] + cxx11dep
   depends_on "expat" unless OS.mac?
 
   if build.with? "python"
@@ -38,7 +47,33 @@ class OsgeoInsighttoolkit < Formula
     depends_on "osgeo-vtk" => [:build] + cxx11dep
   end
 
+  # JAVA_VERSION = "1.8" # "1.10+"
+  depends_on :java => ["1.8", :build] # JAVA_VERSION
+
+  depends_on "zlib"
+  depends_on "bison"
+  depends_on "libpng"
+  depends_on "tcl-tk"
+  depends_on "pcre"
+  depends_on "swig"
+  depends_on "castxml"
+  depends_on "git"
+  # depends_on "ruby"
+  # depends_on "perl"
+
   def install
+    # Warning: python modules have explicit framework links
+    # These python extension modules were linked directly to a Python
+    # framework binary. They should be linked with -undefined dynamic_lookup
+    # instead of -lpython or -framework Python
+    # ENV["PYTHON_LIBS"] = "-undefined dynamic_lookup"
+    # PYTHON_LDFLAGS=-undefined dynamic_lookup
+    # PYTHON_EXTRA_LIBS=-undefined dynamic_lookup
+    # PYTHON_EXTRA_LDFLAGS=-undefined dynamic_lookup
+
+    # cmd = Language::Java.java_home_cmd("1.8") # JAVA_VERSION
+    # ENV["JAVA_HOME"] = Utils.popen_read(cmd).chomp
+
     dylib = OS.mac? ? "dylib" : "so"
 
     args = std_cmake_args + %W[
@@ -52,6 +87,7 @@ class OsgeoInsighttoolkit < Formula
       -DCMAKE_INSTALL_NAME_DIR:STRING=#{lib}
       -DModule_SCIFIO=ON
     ]
+
     args << ".."
     args << "-DBUILD_EXAMPLES=" + (build.include?("examples") ? "ON" : "OFF")
     args << "-DModule_ITKVideoBridgeOpenCV=" + (build.with?("opencv") ? "ON" : "OFF")
@@ -59,7 +95,7 @@ class OsgeoInsighttoolkit < Formula
 
     args << "-DITK_USE_SYSTEM_FFTW=ON" << "-DITK_USE_FFTWF=ON" << "-DITK_USE_FFTWD=ON" if build.with? "fftw"
     args << "-DITK_USE_SYSTEM_HDF5=ON" if build.with? "hdf5"
-    args << "-DITK_USE_SYSTEM_JPEG=ON" if build.with? "jpeg"
+    args << "-DITK_USE_SYSTEM_JPEG=ON" if build.with? "libjpeg-turbo" # jpeg
     args << "-DITK_USE_SYSTEM_PNG=ON" if build.with? :libpng
     args << "-DITK_USE_SYSTEM_TIFF=ON" if build.with? "libtiff"
     args << "-DITK_USE_SYSTEM_GDCM=ON" if build.with? "gdcm"
@@ -72,8 +108,19 @@ class OsgeoInsighttoolkit < Formula
     args << "-DVCL_INCLUDE_CXX_0X=ON" if build.cxx11?
     ENV.cxx11 if build.cxx11?
 
+    args << "-DITK_USE_SYSTEM_LIBRARIES=ON"
+    args << "-DITK_USE_SYSTEM_SWIG=ON"
+    args << "-DITK_USE_SYSTEM_CASTXML=ON"
+    args << "-DITK_LEGACY_SILENT=ON"
+    args << "-DModule_ITKIOMINC=ON"
+    args << "-DModule_ITKIOTransformMINC=ON"
+    # args << "-DITK_WRAP_TCL=ON"
+    # args << "-DITK_WRAP_JAVA=ON"
+    # args << "-DITK_WRAP_RUBY=ON"
+    # args << "-DITK_WRAP_PERL=ON"
+
     mkdir "itk-build" do
-      if build.with?("python") || build.with?("python3")
+      if build.with?("python") || build.with?("python2")
         python_executable = `which python2`.strip if build.with? "python2"
         python_executable = `which python3`.strip if build.with? "python"
 
@@ -84,6 +131,12 @@ class OsgeoInsighttoolkit < Formula
         args << "-DITK_WRAP_PYTHON=ON"
         args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
         args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
+
+        # if PYTHON_EXECUTABLE
+        # does not match Python's prefix
+        # Python site-packages directory to install Python bindings
+        # PY_SITE_PACKAGES_PATH
+
         # CMake picks up the system's python dylib, even if we have a brewed one.
         if File.exist? "#{python_prefix}/Python"
           args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
