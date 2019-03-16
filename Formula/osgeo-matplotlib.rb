8430fb7cc28f523b634dd6a73d144efb904cee1e
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-03-15 21:54:31 -0300

    Add matplotlib.rb

---
+++ Formula/osgeo-matplotlib.rb
@@ -0,0 +1,138 @@
+class NoExternalPyCXXPackage < Requirement
+  fatal false
+
+  satisfy do
+    !quiet_system "python3", "-c", "import CXX"
+  end
+
+  def message; <<~EOS
+    *** Warning, PyCXX detected! ***
+    On your system, there is already a PyCXX version installed, that will
+    probably make the build of Matplotlib fail. In python you can test if that
+    package is available with `import CXX`. To get a hint where that package
+    is installed, you can:
+        python3 -c "import os; import CXX; print(os.path.dirname(CXX.__file__))"
+    See also: https://github.com/Homebrew/homebrew-python/issues/56
+  EOS
+  end
+end
+
+class OsgeoMatplotlib < Formula
+  desc "Python 2D plotting library"
+  homepage "https://matplotlib.org"
+  url "https://github.com/matplotlib/matplotlib/archive/v3.0.3.tar.gz"
+  sha256 "d384a58409c1c23b8760b6f3d84ebed2c936df7dcb82797e983faaf30ae4d69d"
+
+  head "https://github.com/matplotlib/matplotlib.git", :branch => "master"
+
+  depends_on NoExternalPyCXXPackage => :build
+  depends_on "pkg-config" => :build
+  depends_on "gcc" => :build # for gfortran
+  depends_on "python" => :build
+  depends_on "swig" => :build
+  depends_on "libagg"
+  depends_on "freetype"
+  depends_on "libpng"
+  depends_on "qhull"
+  depends_on "tcl-tk"
+  depends_on "zlib"
+
+  depends_on "openblas"
+  depends_on "numpy"
+  depends_on "scipy"
+  depends_on "cairo"
+  depends_on "py3cairo"
+  depends_on "gtk+3"
+  depends_on "pygobject3"
+  depends_on "pygtk"
+  depends_on "pygobject"
+  depends_on "osgeo-pyqt"
+  depends_on "wxpython"
+  depends_on "rsync"
+  depends_on "git"
+  depends_on "ffmpeg"
+  depends_on "imagemagick"
+  depends_on "ghostscript"
+  # depends_on "inkscape" => :optional
+
+  depends_on "openjpeg" # for Pillow
+
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/c2/f7/c7b501b783e5a74cf1768bc174ee4fb0a8a6ee5af6afa92274ff964703e0/setuptools-40.8.0.zip"
+    sha256 "6e4eec90337e849ade7103723b9a99631c1f0d19990d6e8412dc42f5ae8b304d"
+  end
+
+  resource "Pillow" do
+    url "https://files.pythonhosted.org/packages/3c/7e/443be24431324bd34d22dd9d11cc845d995bcd3b500676bcf23142756975/Pillow-5.4.1.tar.gz"
+    sha256 "5233664eadfa342c639b9b9977190d64ad7aca4edc51a966394d7e08e7f38a9f"
+  end
+
+  resource "cycler" do
+    url "https://files.pythonhosted.org/packages/c2/4b/137dea450d6e1e3d474e1d873cd1d4f7d3beed7e0dc973b06e8e10d32488/cycler-0.10.0.tar.gz"
+    sha256 "cd7b2d1018258d7247a71425e9f26463dfb444d411c39569972f4ce586b0c9d8"
+  end
+
+  resource "kiwisolver" do
+    url "https://files.pythonhosted.org/packages/31/60/494fcce70d60a598c32ee00e71542e52e27c978e5f8219fae0d4ac6e2864/kiwisolver-1.0.1.tar.gz"
+    sha256 "ce3be5d520b4d2c3e5eeb4cd2ef62b9b9ab8ac6b6fedbaa0e39cdb6f50644278"
+  end
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/b9/b8/6b32b3e84014148dcd60dd05795e35c2e7f4b72f918616c61fdce83d27fc/pyparsing-2.3.1.tar.gz"
+    sha256 "66c9268862641abcac4a96ba74506e594c884e3f57690a696d21ad8210ed667a"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/ad/99/5b2e99737edeb28c71bcbec5b5dda19d0d9ef3ca3e92e3e925e7c0bb364c/python-dateutil-2.8.0.tar.gz"
+    sha256 "c89805f6f4d64db21ed966fda138f8a5ed7a4fdbc1a8ee329ce1b74e3c74da9e"
+  end
+
+  resource "pytz" do
+    url "https://files.pythonhosted.org/packages/af/be/6c59e30e208a5f28da85751b93ec7b97e4612268bb054d0dff396e758a90/pytz-2018.9.tar.gz"
+    sha256 "d5f05e487007e29e03409f9398d074e158d920d36eb82eaf66fb1136b0c5374c"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b6994648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
+    sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
+  end
+
+  resource "tornado" do
+    url "https://files.pythonhosted.org/packages/e6/78/6e7b5af12c12bdf38ca9bfe863fcaf53dc10430a312d0324e76c1e5ca426/tornado-5.1.1.tar.gz"
+    sha256 "4e5158d97583502a7e2739951553cbd88a72076f152b4b11b64b9a10c4c49409"
+  end
+
+  # resource "numpy" do
+  #   url "https://files.pythonhosted.org/packages/2b/26/07472b0de91851b6656cbc86e2f0d5d3a3128e7580f23295ef58b6862d6c/numpy-1.16.1.zip"
+  #   sha256 "31d3fe5b673e99d33d70cfee2ea8fe8dccd60f265c3ed990873a88647e3dd288"
+  # end
+
+  def install
+    if DevelopmentTools.clang_build_version >= 900
+      ENV.delete "SDKROOT"
+      ENV.delete "HOMEBREW_SDKROOT"
+    end
+
+    inreplace "setupext.py",
+              "'darwin': ['/usr/local/'",
+              "'darwin': ['#{HOMEBREW_PREFIX}'"
+
+    xy = Language::Python.major_minor_version "python3"
+    site_packages = libexec/"lib/python#{xy}/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", site_packages
+
+    resources.each do |r|
+      r.stage do
+        system "python3", *Language::Python.setup_install_args(libexec)
+      end
+    end
+    (lib/"python#{xy}/site-packages/homebrew-matplotlib.pth").write "#{site_packages}\n"
+
+    system "python3", *Language::Python.setup_install_args(prefix)
+  end
+
+  test do
+    ENV["PYTHONDONTWRITEBYTECODE"] = "1"
+    system "python3", "-c", "import matplotlib"
+  end
+end
