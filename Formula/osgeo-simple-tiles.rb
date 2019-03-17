951254d5578dd3ba542ac11436f9232d8d106ae4
geo-ninja <qgisninja@gmail.com>
2019-03-17 14:13:49 -0700

    Updated bottles for:  osgeo-simple-tiles
      Committed for JF Perini<21315242+fjperini@users.noreply.github.com>
      [ci skip]

--- Formula/osgeo-simple-tiles.rb
+++ Formula/osgeo-simple-tiles.rb
@@ -4,6 +4,14 @@ class OsgeoSimpleTiles < Formula
   url "https://github.com/propublica/simple-tiles/archive/v0.6.1.tar.gz"
   sha256 "2391b2f727855de28adfea9fc95d8c7cbaca63c5b86c7286990d8cbbcd640d6f"
 
+  bottle do
+    root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
+    cellar :any
+    sha256 "cf3b55ed85ec7e3c84d487633831fe92a87382b024ddc7cb506f335daacc3c4a" => :mojave
+    sha256 "cf3b55ed85ec7e3c84d487633831fe92a87382b024ddc7cb506f335daacc3c4a" => :high_sierra
+    sha256 "4cecd6dd82c66d4648fa72e6220a21be6d6cbb0c820d39cd8974f4d3718258e3" => :sierra
+  end
+
   # revision 1
 
   head "https://github.com/propublica/simple-tiles.git", :branch => "master"
