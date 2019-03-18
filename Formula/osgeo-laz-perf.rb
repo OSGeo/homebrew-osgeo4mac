5c422cb9125792076f73701c49a8e4b4b0999107
geo-ninja <qgisninja@gmail.com>
2019-03-18 05:45:24 -0700

    Updated bottles for:  osgeo-laz-perf
      Committed for JF Perini<21315242+fjperini@users.noreply.github.com>
      [ci skip]

--- Formula/osgeo-laz-perf.rb
+++ Formula/osgeo-laz-perf.rb
@@ -4,6 +4,14 @@ class OsgeoLazPerf < Formula
   url "https://github.com/hobu/laz-perf/archive/1.3.0.tar.gz"
   sha256 "9d4273206557e091a4faf7faa4867ecd7da55a116015fcbfb58d30e88570958e"
 
+  bottle do
+    root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
+    cellar :any_skip_relocation
+    sha256 "7b02a955661953ec73411efaa516147d67e2376b3a81ce735fa7006bf3aee116" => :mojave
+    sha256 "7b02a955661953ec73411efaa516147d67e2376b3a81ce735fa7006bf3aee116" => :high_sierra
+    sha256 "b185161a85e97816ae429781856862ef8ddd330716369132ae2b8d0ae0e0bd9d" => :sierra
+  end
+
   # revision 1
 
   head "https://github.com/hobu/laz-perf.git", :branch => "master"
