3fc56cc8277b8eab6002d3975bbb6e2d59c47dee
geo-ninja <qgisninja@gmail.com>
2019-03-19 12:26:13 -0700

    Updated bottles for:  osgeo-pointcloud
      Committed for JF Perini<21315242+fjperini@users.noreply.github.com>
      [ci skip]

--- Formula/osgeo-pointcloud.rb
+++ Formula/osgeo-pointcloud.rb
@@ -3,6 +3,14 @@ class OsgeoPointcloud < Formula
   homepage "https://github.com/pgpointcloud/pointcloud"
   url "https://github.com/pgpointcloud/pointcloud/archive/v1.2.0.tar.gz"
   sha256 "8542a4c714b4d0c67f10d092291a43b5650871b4ec8caf831e492810f25bb93c"
+  bottle do
+    root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
+    cellar :any
+    sha256 "7c256b3bcdad665e88b18d54975658e63e71852374aa3340465e7b8740b8125e" => :mojave
+    sha256 "7c256b3bcdad665e88b18d54975658e63e71852374aa3340465e7b8740b8125e" => :high_sierra
+    sha256 "60bc28e7274904d6729e7cf2fd82c2388907a26d1ba07d559ff7785164de4b17" => :sierra
+  end
+
   # url "https://github.com/pgpointcloud/pointcloud/archive/v1.0.1.tar.gz"
   # sha256 "3fac2efe1263b0876c26fc77e28f3664b56aa1e142c92383f9eb5b828999d0e7"
 
