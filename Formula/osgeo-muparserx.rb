3b5c3317cdd7ae8a1de828099d1b6f38a7f2d4ab
geo-ninja <qgisninja@gmail.com>
2019-03-30 21:32:53 -0700

    Updated bottles for:  osgeo-muparserx
      Committed for JF Perini<21315242+fjperini@users.noreply.github.com>
      [ci skip]

--- Formula/osgeo-muparserx.rb
+++ Formula/osgeo-muparserx.rb
@@ -4,6 +4,14 @@ class OsgeoMuparserx < Formula
   url "https://github.com/beltoforion/muparserx/archive/v4.0.8.tar.gz"
   sha256 "5913e0a4ca29a097baad1b78a4674963bc7a06e39ff63df3c73fbad6fadb34e1"
 
+  bottle do
+    root_url "https://bottle.download.osgeo.org"
+    cellar :any_skip_relocation
+    sha256 "5af85a06844fecba30fb23c997031958ba9f8b7be00245a26330755f81b71112" => :mojave
+    sha256 "5af85a06844fecba30fb23c997031958ba9f8b7be00245a26330755f81b71112" => :high_sierra
+    sha256 "ded4ce5df774c51f10a8137b7b23b98317ecdef59183bba0375815201699a127" => :sierra
+  end
+
   # revision 1
 
   head "https://github.com/beltoforion/muparserx.git", :branch => "master"
