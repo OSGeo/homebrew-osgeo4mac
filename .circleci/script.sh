48a22ac11e434bf065e3677790d28bb236d71061
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-04-02 16:57:38 -0300

    fix postgresql

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -56,6 +56,7 @@ for f in ${CHANGED_FORMULAE};do
   # initdb: could not create directory "/usr/local/var/postgresql": Operation not permitted
   if [ "${f}" == "osgeo-libpqxx" ];then
     initdb /usr/local/var/postgresql -E utf8 --locale=en_US.UTF-8
+    pg_ctl -D /usr/local/var/postgresql -l logfile start
     # system "psql", "-h", "localhost", "-d", "postgres"
     createdb circleci
   fi
