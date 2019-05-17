228f2dde9498523e8d63380deb5f89f0c70f7508
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-05-17 01:36:00 -0300

    osgeo-vtk.rb: use proj5 and gdal2 (#1128)

--- Formula/osgeo-vtk.rb
+++ Formula/osgeo-vtk.rb
@@ -5,7 +5,7 @@ class OsgeoVtk < Formula
   url "https://www.vtk.org/files/release/8.2/VTK-8.2.0.tar.gz"
   sha256 "34c3dc775261be5e45a8049155f7228b6bd668106c72a3c435d95730d17d57bb"
 
-  revision 6
+  revision 7
 
   head "https://github.com/Kitware/VTK.git", :branch => "master"
 
