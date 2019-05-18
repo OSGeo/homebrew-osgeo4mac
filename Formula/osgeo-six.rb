4c74f1a00666ca931b8e1c133ac3f76e69562cbc
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-05-18 17:32:54 -0300

    Update osgeo-six.rb (#1150)

--- Formula/osgeo-six.rb
+++ Formula/osgeo-six.rb
@@ -4,7 +4,7 @@ class OsgeoSix < Formula
   url "https://github.com/benjaminp/six/archive/1.12.0.tar.gz"
   sha256 "0ce7aef70d066b8dda6425c670d00c25579c3daad8108b3e3d41bef26003c852"
 
-  revision 1
+  revision 2
 
   head "https://github.com/benjaminp/six.git", :branch => "master"
 
@@ -22,13 +22,13 @@ class OsgeoSix < Formula
   depends_on "tcl-tk"
 
   resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/c2/f7/c7b501b783e5a74cf1768bc174ee4fb0a8a6ee5af6afa92274ff964703e0/setuptools-40.8.0.zip"
-    sha256 "6e4eec90337e849ade7103723b9a99631c1f0d19990d6e8412dc42f5ae8b304d"
+    url "https://files.pythonhosted.org/packages/1d/64/a18a487b4391a05b9c7f938b94a16d80305bf0369c6b0b9509e86165e1d3/setuptools-41.0.1.zip"
+    sha256 "a222d126f5471598053c9a77f4b5d4f26eaa1f150ad6e01dcf1a42e185d05613"
   end
 
   resource "pytest" do
-    url "https://files.pythonhosted.org/packages/41/f8/507d1f6121293a0392f5d0850c138d9c7dac6d22f575734078da2d0f447c/pytest-4.2.0.tar.gz"
-    sha256 "65aeaa77ae87c7fc95de56285282546cfa9c886dc8e5dc78313db1c25e21bc07"
+    url "https://files.pythonhosted.org/packages/88/04/f2ae104dffcd6b2e3c7ed35773b760971c1bacbe4447250966f927cf5efd/pytest-4.5.0.tar.gz"
+    sha256 "1a8aa4fa958f8f451ac5441f3ac130d9fc86ea38780dd2715e6d5c5882700b24"
   end
 
   def install
