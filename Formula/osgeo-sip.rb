5f71b0b891a3fc26e47d1f4d70b1424b8dd752f6
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-05-18 17:43:23 -0300

    Update osgeo-sip.rb [ci skip]

--- Formula/osgeo-sip.rb
+++ Formula/osgeo-sip.rb
@@ -1,10 +1,30 @@
+class Unlinked < Requirement
+  fatal true
+
+  satisfy(:build_env => false) { !core_sip_linked }
+
+  def core_sip_linked
+    Formula["sip"].linked_keg.exist?
+  rescue
+    return false
+  end
+
+  def message
+    s = "\033[31mYou have other linked versions!\e[0m\n\n"
+
+    s += "Unlink with \e[32mbrew unlink sip\e[0m or remove with brew \e[32muninstall --ignore-dependencies sip\e[0m\n\n" if core_sip_linked
+    s
+  end
+end
+
+
 class OsgeoSip < Formula
   desc "Tool to create Python bindings for C and C++ libraries"
   homepage "https://www.riverbankcomputing.com/software/sip/intro"
   url "https://www.riverbankcomputing.com/static/Downloads/sip/4.19.17/sip-4.19.17.tar.gz"
   sha256 "12bcd8f4d5feefc105bc075d12c5090ee783f7380728563c91b8b95d0ec45df3"
 
-  # revision 1
+  revision 1
 
   head "https://www.riverbankcomputing.com/hg/sip", :using => :hg
 
@@ -16,6 +36,10 @@ class OsgeoSip < Formula
     sha256 "6885c242ee0e799289eae650d788d65f64effaebe0f06d2f96905b7289c34ead" => :high_sierra
     sha256 "cf0a51b2595e2e2f1dddd6d4740ddb422e0502413fafe229b74fb7aa270cad9e" => :sierra
   end
+
+  # keg_only "sip" is already provided by homebrew/core"
+  # we will verify that other versions are not linked
+  depends_on Unlinked
 
   depends_on "python"
   depends_on "python@2"
