8e420ca1d26ce0b718b447174ffa1cec374103f3
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-05-21 08:21:20 -0300

    Update osgeo-orfeo.rb (#1170)

--- Formula/osgeo-orfeo.rb
+++ Formula/osgeo-orfeo.rb
@@ -4,14 +4,14 @@ class OsgeoOrfeo < Formula
   url "https://github.com/orfeotoolbox/OTB/archive/6.6.2.tar.gz"
   sha256 "39c2a9e202e1a2e2b12f2fb96b60a99123700cd169b69a8fb021137f5612b89c"
 
-  # revision 1
+  revision 1
 
   bottle do
     root_url "https://bottle.download.osgeo.org"
     cellar :any
-    sha256 "b0bb5b051b4a0c64c67e1a6917229f27d107b08bfc82fafb804213e72cf73e9f" => :mojave
-    sha256 "b0bb5b051b4a0c64c67e1a6917229f27d107b08bfc82fafb804213e72cf73e9f" => :high_sierra
-    sha256 "fbdea683ddc16897e068951f93dd6cc8d0e4a3b4d167719f6bd8c94b89323025" => :sierra
+    sha256 "be41ed265d0a0eb849763a0fbf6500a86e93520d8952e1fe08bf73f44ae43f91" => :mojave
+    sha256 "be41ed265d0a0eb849763a0fbf6500a86e93520d8952e1fe08bf73f44ae43f91" => :high_sierra
+    sha256 "33215fbefa9ddb036f419653d6396460dea0d7fc5dc62e16f7e1111296f625d4" => :sierra
   end
 
   head "https://gitlab.orfeo-toolbox.org/orfeotoolbox/otb.git", :branch => "master"
