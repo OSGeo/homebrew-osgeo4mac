560b56f00d6728fd5da4a44584c0f3690b2bc26d
geo-ninja <qgisninja@gmail.com>
2019-05-21 18:33:09 -0700

    Updated bottles for:  osgeo-simple-tiles
      Committed for JF Perini<21315242+fjperini@users.noreply.github.com>
      [ci skip]

--- Formula/osgeo-simple-tiles.rb
+++ Formula/osgeo-simple-tiles.rb
@@ -7,9 +7,10 @@ class OsgeoSimpleTiles < Formula
   bottle do
     root_url "https://bottle.download.osgeo.org"
     cellar :any
-    sha256 "cf3b55ed85ec7e3c84d487633831fe92a87382b024ddc7cb506f335daacc3c4a" => :mojave
-    sha256 "cf3b55ed85ec7e3c84d487633831fe92a87382b024ddc7cb506f335daacc3c4a" => :high_sierra
-    sha256 "4cecd6dd82c66d4648fa72e6220a21be6d6cbb0c820d39cd8974f4d3718258e3" => :sierra
+    rebuild 1
+    sha256 "fec2a26451465b4e4d895e2ef383b20240af7f6956d57b5ea70fd7fcf059d47e" => :mojave
+    sha256 "fec2a26451465b4e4d895e2ef383b20240af7f6956d57b5ea70fd7fcf059d47e" => :high_sierra
+    sha256 "e1d77cc94e155ce5067b5f84d3b5dc781b3f5014eab0a5d18ce77681cbb9ecaa" => :sierra
   end
 
   revision 2
