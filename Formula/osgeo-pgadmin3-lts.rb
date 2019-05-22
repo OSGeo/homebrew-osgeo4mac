a045e791058054c45b0bb484f8c91d94581b0652
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-05-22 02:44:45 -0300

    Update osgeo-pgadmin3-lts.rb [ci skip]

--- Formula/osgeo-pgadmin3-lts.rb
+++ Formula/osgeo-pgadmin3-lts.rb
@@ -6,7 +6,7 @@ class OsgeoPgadmin3Lts < Formula
   sha256 "65de9fb0d2bc43bfa0931832c344cfb894ae862acefefc64fbc8cd7d0f6cb7ff"
   version "1.22.3"
 
-  revision 1
+  revision 2
 
   head "https://github.com/pgcentral/pgadmin3-lts.git", :branch => "master"
 
@@ -32,6 +32,7 @@ class OsgeoPgadmin3Lts < Formula
   depends_on "libxml2"
   depends_on "libgcrypt"
   depends_on "openssl"
+  depends_on "libssh2"
   depends_on "zlib"
   depends_on "osgeo-libpqxx"
   depends_on "imagemagick"
@@ -49,10 +50,42 @@ class OsgeoPgadmin3Lts < Formula
   #   sha256 "c7658aab75c920288a8cf6f09f244c6cfdae30d82d803ac1634d9f223a80ca08"
   # end
 
-  resource "pg11" do
-    url "https://gist.githubusercontent.com/fjperini/712d8605f59590a8caefdb6e45b4b97e/raw/40cffed9c69802d861ae410ce299d72d61209e86/pgadmin3.diff"
-    sha256 "eeafe9bb1f6aae02677a267af85a12a04d64a05fad544c55ca67044111a23f50"
+  if build.with?("pg10")
+    # PG 10 support, adapted from:
+    # https://bitbucket.org/openscg/pgadmin3-lts/commits/ea8a31af779b101248fc13242cb7a34e252cf49e/raw
+    # https://bitbucket.org/openscg/pgadmin3-lts/commits/1d65e012f7bb88d3b6c5a08946a2456617213c35/raw
+    resource "pg10" do
+      url "https://gist.githubusercontent.com/fjperini/204ec325671a30e9ddb46d77ea8e1289/raw/f383173056b93731d3e2e8a90614373948274ca6/pgadmin3-pg10.diff"
+      sha256 "74e127952d9674f4c09bbf8805f43f6a85821133fd9a84db094e8aab837c7d03"
+    end
+  else
+    # PG 11 support, adapted from:
+    # https://abdulyadi.wordpress.com/2018/11/03/pgadmin3-adjustment-for-postgresql-11-0/
+    resource "pg11" do
+      url "https://gist.githubusercontent.com/fjperini/9b22ecd9bda767e51a446749472f8e94/raw/7bed1afe383d915c408c3cc735a25b192b3e0108/pgadmin3-pg11.diff"
+      sha256 "7dc526b80eb61540e9bf6d46a329f45eac683646caaa68dec90f9743723dd7d3"
+    end
   end
+
+  # 1.14.2-cflags
+  # resource "cflags" do
+  #   url "https://gist.githubusercontent.com/fjperini/5e9c519e88dd1aebd975edb89f2bd680/raw/03679e513f0ca67d5283d2560b0338d15730c0aa/pgadmin3-1.14.2-cflags.diff"
+  #   sha256 "1ab6db05896684cac42b4a29a422a3cd29d12b98f8f7d092c7f7647ea0fffc76"
+  # end
+
+  # Move this == null check to a static function.  This works, but I opted
+  # for the compiler flag since it "fixes" all cases, and pgadmin4 is on the
+  # way.
+  resource "nullthis" do
+    url "https://gist.githubusercontent.com/fjperini/d5b129bad455a6d945ff80d17b91887a/raw/96531fd54ee7a67b7fd42eac97fedf6c8c27bb07/pgadmin3-nullthis.diff"
+    sha256 "3b785ee8a2857f02b7d1983a3b8d699032494438096f3321576a8267c823b0c8"
+  end
+
+  # Fix failure to use EVP_CIPHER_CTX_new()
+  # resource "ssh2" do
+  #   url "https://gist.githubusercontent.com/fjperini/2bc12464cc37c0c324cc2a6a61a1f3af/raw/b28e587dc265f0094f0ed23d7ff210bb24f77128/pgadmin3-ssh2.diff"
+  #   sha256 "71a7b90989ab35793b4435d6bc1ad54581f47649a5265021622fddb61ee83054"
+  # end
 
   def install
     # venv = virtualenv_create(libexec, "#{Formula["python@2"].opt_bin}/python2")
@@ -61,14 +94,42 @@ class OsgeoPgadmin3Lts < Formula
     #   venv.pip_install resource(r)
     # end
 
-    resource("pg11").stage do
-      cp_r "./pgadmin3.diff", "#{buildpath}"
+    if build.with?("pg10")
+      resource("pg10").stage do
+        cp_r "./pgadmin3-pg10.diff", "#{buildpath}"
+      end
+      system "patch", "-p1", "-i", "#{buildpath}/pgadmin3-pg10.diff"
+    else
+      resource("pg11").stage do
+        cp_r "./pgadmin3-pg11.diff", "#{buildpath}"
+      end
+      system "patch", "-p1", "-i", "#{buildpath}/pgadmin3-pg11.diff"
     end
-    system "patch", "-p1", "-i", "#{buildpath}/pgadmin3.diff"
+
+    # resource("cflags").stage do
+    #   cp_r "./pgadmin3-1.14.2-cflags.diff", "#{buildpath}"
+    # end
+    # system "patch", "-p1", "-i", "#{buildpath}/pgadmin3-1.14.2-cflags.diff"
+
+    resource("nullthis").stage do
+      cp_r "./pgadmin3-nullthis.diff", "#{buildpath}"
+    end
+    # system "patch", "-p0", "-b", ".nullthis"
+    system "patch", "-p1", "-i", "#{buildpath}/pgadmin3-nullthis.diff"
+
+    # use the libssh2 embedded with pgadmin3
+    # global use_embedded 1
+    # remove embedded libssh2
+    # system "rm", "-rf", "#{buildpath}/pgadmin/libssh2", "#{buildpath}/pgadmin/include/libssh2"
+    # resource("ssh2").stage do
+    #   cp_r "./pgadmin3-ssh2.diff", "#{buildpath}"
+    # end
+    # # system "patch", "-p0", "-b", ".ssh2f"
+    # system "patch", "-p1", "-i", "#{buildpath}/pgadmin3-ssh2.diff"
 
     args = [
-      "--with-wx=#{Formula["wxmac"].opt_prefix}",
-      "--with-wx-version=3.0",
+      "--with-wx=#{Formula["wxmac"].opt_prefix}", # #{HOMEBREW_PREFIX}
+      "--with-wx-version=3.0", # #{Formula["wxmac"].version}
       "--with-libxml2=#{Formula["libxml2"].opt_prefix}",
       "--with-libxslt=#{Formula["libxslt"].opt_prefix}",
       "--prefix=#{prefix}",
@@ -94,6 +155,8 @@ class OsgeoPgadmin3Lts < Formula
     # pgAdmin3.rsrc: No such file or directory
     # args << "--enable-appbundle"
 
+    ENV.append "CXXFLAGS", "-fno-delete-null-pointer-checks -Wno-unused-local-typedefs"
+
     if build.with?("pg10")
       args << "--with-pgsql=#{Formula["osgeo-postgresql@10"].opt_prefix}"
     else
@@ -104,8 +167,8 @@ class OsgeoPgadmin3Lts < Formula
 
     system "./bootstrap"
     # [ -f Makefile ] ||  ./configure --prefix=/usr --with-wx-version=3.0
-    system "./configure", *args
-    system "make"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", *args
+    system "make", "all"
     system "make", "install"
 
     if build.with? "app"
