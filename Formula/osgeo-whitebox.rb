5fbe3c6d9c6f5418bdd96727a4b1853a5dbca341
FJ Perini <21315242+fjperini@users.noreply.github.com>
2019-05-23 20:48:24 -0300

    Update osgeo-whitebox.rb [ci skip]

--- Formula/osgeo-whitebox.rb
+++ Formula/osgeo-whitebox.rb
@@ -1,11 +1,11 @@
-class Whitebox < Formula
+class OsgeoWhitebox < Formula
   desc "Geographic information system (GIS) and remote sensing package intended for advanced geospatial analysis and data visualization"
   homepage "https://www.uoguelph.ca/~hydrogeo/Whitebox"
   url "https://www.uoguelph.ca/~hydrogeo/Whitebox/WhiteboxGAT-mac.zip"
   sha256 "32b2a75dab883e97d271621c60e7ad254558587b8bda0f1013bb9562077eea34"
   version "3.4.0"
 
-  revision 4
+  revision 5
 
   bottle do
     root_url "https://dl.bintray.com/homebrew-osgeo/osgeo-bottles"
