4ba05439af2d0e22a93eef48717b8ed72b3cf12c
JF Perini <21315242+fjperini@users.noreply.github.com>
2019-11-05 19:02:55 -0300

    Update osgeo-libgeotiff.rb (#1273)

--- Formula/osgeo-libgeotiff.rb
+++ Formula/osgeo-libgeotiff.rb
@@ -20,8 +20,12 @@ end
 class OsgeoLibgeotiff < Formula
   desc "Library and tools for dealing with GeoTIFF"
   homepage "https://geotiff.osgeo.org/"
-  url "https://github.com/OSGeo/libgeotiff/releases/download/1.5.1/libgeotiff-1.5.1.tar.gz"
-  sha256 "f9e99733c170d11052f562bcd2c7cb4de53ed405f7acdde4f16195cd3ead612c"
+  # url "https://github.com/OSGeo/libgeotiff/releases/download/1.5.1/libgeotiff-1.5.1.tar.gz"
+  # sha256 "f9e99733c170d11052f562bcd2c7cb4de53ed405f7acdde4f16195cd3ead612c"
+  url "https://github.com/OSGeo/libgeotiff.git",
+    :branch => "master",
+    :commit => "cfe0433bce6811520bc8e22ca5d14d63a37cf717"
+  version "1.5.1"
 
   bottle do
     root_url "https://bottle.download.osgeo.org"
@@ -43,20 +47,23 @@ class OsgeoLibgeotiff < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "gettext" => :build
+  depends_on "pkgconfig" => :build
   depends_on "jpeg"
   depends_on "zlib"
   depends_on "libtiff"
   depends_on "osgeo-proj"
 
   def install
-    # cd "libgeotiff" do
-      # system "./autogen.sh"
+    cd "libgeotiff" do
+      # autoreconf -fvi
+      system "./autogen.sh"
       system "./configure", "--disable-dependency-tracking",
                             "--prefix=#{prefix}",
                             "--with-jpeg", "--with-zlib"
       system "make" # Separate steps or install fails
       system "make", "install"
-    # end
+    end
   end
 
   test do
