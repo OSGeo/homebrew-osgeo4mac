bc20dc37589677bea1151eb2eff6b67ee728d91e
JF Perini <21315242+fjperini@users.noreply.github.com>
2020-01-11 10:15:25 -0300

    Upgrade PROJ to 6.3.0 (#1298)

--- .circleci/after_script.sh
+++ .circleci/after_script.sh
@@ -15,6 +15,12 @@
 #                                                                         #
 ###########################################################################
 
+# OS X version
+# sw_vers -productVersion
+# sw_vers -productVersion | cut -d '.' -f 1,2
+# MAJOR_MAC_VERSION=$(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}')
+# system_profiler SPSoftwareDataType
+
 # Build the actual bottles
 # In Travis, this used to be part of the deploy phase, but now it needs
 # to run as part of the original build process, but only on master.
@@ -26,6 +32,13 @@ pushd /tmp/bottles
   for f in ${CHANGED_FORMULAE};do
     echo "Bottling changed formula ${f}..."
     brew bottle --verbose --json --root-url=${BOTTLE_ROOT} osgeo/osgeo4mac/${f}
+
+    for art in ${f}*.sierra.bottle.*; do
+        # Remove double dashes introduced by the latest changes to Homebrew bottling.
+        # This may need to be reverted later, but this at least normalizes the bottle names with what's in the json files.
+        # Move the sierra bottle and json file
+        mv ${art} ${art/--/-}
+    done
 
     # temporary duplication of 10.3-Xcode-10.x-built bottles to 10.4 bottles
     # Do the bottle duplication per formula, so we can merge the changes
@@ -39,6 +52,20 @@ pushd /tmp/bottles
     done
     for json in ${f}*.mojave.bottle*.json; do
       sed -i '' s@high_sierra@mojave@g ${json}
+    done
+
+    # temporary duplication
+    # Do the bottle duplication per formula, so we can merge the changes
+    for art in ${f}*.high_sierra.bottle.*; do
+      new_name=${art/.high_sierra./.catalina.}
+      # Remove double dashes introduced by the latest changes to Homebrew bottling.
+      # This may need to be reverted later, but this at least normalizes the bottle names with what's in the json files.
+      cp -a ${art} ${new_name/--/-}
+      # Move the high_sierra bottle and json file
+      mv ${art} ${art/--/-}
+    done
+    for json in ${f}*.catalina.bottle*.json; do
+      sed -i '' s@high_sierra@catalina@g ${json}
     done
   done
   ls
