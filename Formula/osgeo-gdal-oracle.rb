cec7a307f3f88a62be3e7ea2eb38ce55fe5c3436
FJ Perini <21315242+fjperini@users.noreply.github.com>
2020-01-31 16:45:33 -0300

    Update gdal plugins [ci skip]

--- Formula/osgeo-gdal-oracle.rb
+++ Formula/osgeo-gdal-oracle.rb
@@ -1,8 +1,8 @@
 class OsgeoGdalOracle < Formula
   desc "GDAL/OGR 2.x plugin for Oracle Spatial driver"
   homepage "http://www.gdal.org/drv_oci.html"
-  url "https://github.com/OSGeo/gdal/releases/download/v3.0.1/gdal-3.0.1.tar.gz"
-  sha256 "37fd5b61fabc12b4f13a556082c680025023f567459f7a02590600344078511c"
+  url "https://github.com/OSGeo/gdal/releases/download/v3.0.4/gdal-3.0.4.tar.gz"
+  sha256 "fc15d2b9107b250305a1e0bd8421dd9ec1ba7ac73421e4509267052995af5e83"
 
   # revision 1
 
