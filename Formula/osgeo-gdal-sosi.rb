d750b788f97b7048e4b94ad16a6fa7b581fb92af
FJ Perini <21315242+fjperini@users.noreply.github.com>
2020-01-31 00:18:24 -0300

    Update osgeo-gdal-sosi.rb

--- Formula/osgeo-gdal-sosi.rb
+++ Formula/osgeo-gdal-sosi.rb
@@ -1,8 +1,8 @@
 class OsgeoGdalSosi < Formula
   desc "GDAL/OGR 2.x plugin for SOSI driver"
   homepage "https://trac.osgeo.org/gdal/wiki/SOSI"
-  url "https://github.com/OSGeo/gdal/releases/download/v3.0.2/gdal-3.0.2.tar.gz"
-  sha256 "787cf150346e58bff0ccf8c131b333139273e35d2abd590ad7196a9ee08f0039"
+  url "https://github.com/OSGeo/gdal/releases/download/v3.0.4/gdal-3.0.4.tar.gz"
+  sha256 "fc15d2b9107b250305a1e0bd8421dd9ec1ba7ac73421e4509267052995af5e83"
 
   # revision 1
 
