2746456c35784d9b15bdcc748d1ffe831a679c06
FJ Perini <21315242+fjperini@users.noreply.github.com>
2020-02-17 02:19:35 -0300

    osgeo-pcl: fix jpeg-turbo

--- .circleci/before_install.sh
+++ .circleci/before_install.sh
@@ -113,7 +113,7 @@ for f in ${CHANGED_FORMULAE};do
     brew unlink osgeo-grass
   fi
 
-  if [ "${f}" == "osgeo-vtk" ] || [ "${f}" == "osgeo-insighttoolkit" ];then
+  if [ "${f}" == "osgeo-vtk" ] || [ "${f}" == "osgeo-insighttoolkit" ] || [ "${f}" == "osgeo-pcl" ];then
     echo "Installing jpeg-turbo"
     # osgeo-vtk: Java 1.8 is required to install this formula.
     # Install AdoptOpenJDK 8 with Homebrew Cask:
