e3b7adc75d9c298e14e68165889865927e4a7420
Alberto Lázaro-López <alazarolop@gmail.com>
2020-03-16 13:35:34 +0100

    New formulas postgresql@11 and postgis@2.5

---
+++ Formula/osgeo-postgis@2.5.rb
@@ -0,0 +1,236 @@
+class OsgeoPostgis < Formula
+  desc "Adds support for geographic objects to PostgreSQL"
+  homepage "https://postgis.net/"
+  url "https://github.com/postgis/postgis/archive/2.5.4.tar.gz"
+  sha256 "ee547d902bfd758fc89e9e04d5bf3d0f08ebf449d0c63a5a1a8827249fe22c21"
+
+  #bottle do
+  #  root_url "https://bottle.download.osgeo.org"
+  #  cellar :any
+  #  sha256 "7f726ad8efddf119507b78c7e5d277e12a02bd26345926259fe983810338fcd7" => :catalina
+  #  sha256 "7f726ad8efddf119507b78c7e5d277e12a02bd26345926259fe983810338fcd7" => :mojave
+  #  sha256 "7f726ad8efddf119507b78c7e5d277e12a02bd26345926259fe983810338fcd7" => :high_sierra
+  #end
+
+  #revision 2
+
+  head "https://github.com/postgis/postgis.git", :branch => "master"
+
+  option "with-html-docs", "Generate multi-file HTML documentation"
+  option "with-api-docs", "Generate developer API documentation (long process)"
+  option "with-pg11", "Build with PostgreSQL 11 client"
+
+  # keg_only "postgis is already provided by homebrew/core"
+  # we will verify that other versions are not linked
+  depends_on Unlinked
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "gpp" => :build
+  depends_on "pkg-config" => :build
+  depends_on "geos"
+  depends_on "json-c" # for GeoJSON and raster handling
+  depends_on "libiconv"
+  depends_on "libxml2"
+  depends_on "libxslt"
+  depends_on "pcre"
+  depends_on "osgeo-proj"
+  depends_on "sfcgal" # for advanced 2D/3D functions
+  depends_on "protobuf-c" #  Geobuf and Mapbox Vector Tile support
+  depends_on "osgeo-gdal" # for GeoJSON and raster handling
+
+  if build.with? "pg11"
+    depends_on "osgeo-postgresql@11"
+  else
+    depends_on "osgeo-postgresql"
+  end
+
+  depends_on "gtk+" # for gui
+
+  if build.with? "html-docs"
+    depends_on "imagemagick"
+    depends_on "docbook-xsl" # docbook-xsl-nons
+  end
+
+  if build.with? "api-docs"
+    depends_on "graphviz"
+    depends_on "doxygen"
+  end
+
+  def install
+    # Follow the PostgreSQL linked keg back to the active Postgres installation
+    # as it is common for people to avoid upgrading Postgres.
+    # postgres_realpath = Formula["postgresql"].opt_prefix.realpath
+    ENV.append "CFLAGS", "-Diconv=libiconv -Diconv_open=libiconv_open -Diconv_close=libiconv_close"
+    ENV.append "LDFLAGS", "-L#{Formula["libiconv"].opt_lib} -liconv" # ICONV_LDFLAGS
+
+    ENV.deparallelize
+
+    args = [
+      "--with-libiconv=#{Formula["libiconv"].opt_prefix}",
+      "--with-xml2config=#{Formula["libxml2"].opt_bin}/xml2-config",
+      "--with-geosconfig=#{Formula["geos"].opt_bin}/geos-config",
+      "--with-sfcgal=#{Formula["sfcgal"].opt_bin}/sfcgal-config",
+      "--with-projdir=#{Formula["osgeo-proj"].opt_prefix}",
+      "--with-jsondir=#{Formula["json-c"].opt_prefix}",
+      "--with-protobufdir=#{Formula["protobuf-c"].opt_prefix}",
+      "--with-pcredir=#{Formula["pcre"].opt_prefix}",
+      "--with-gdalconfig=#{Formula["osgeo-gdal"].opt_bin}/gdal-config",
+      "--with-gui",
+      "--with-raster",
+    ]
+
+    # By default PostGIS will try to detect gettext support and compile with it,
+    # how ever if your un into incompatibility issues that cause breakage of loader,
+    # you can disable it entirely with this command. Refer to ticket
+    # http://trac.osgeo.org/postgis/ticket/748 for an example issue solved by
+    # configuring with this. NOTE: that you aren’t missing much by turning this off.
+    # This is used for international help/label support for the GUI loader which is not
+    # yet documented and still experimental.
+    args << "--with-gettext=no" # or PATH
+
+    # Unfortunately, NLS support causes all kinds of headaches because
+    # PostGIS gets all of its compiler flags from the PGXS makefiles. This
+    # makes it nigh impossible to tell the buildsystem where our keg-only
+    # gettext installations are.
+    args << "--disable-nls"
+
+    # Wagyu will only be necessary if protobuf is present to build MVTs
+    # args << "--with-wagyu"
+
+    # Disable topology support.
+    # There is no corresponding library as all logic needed for
+    # topology is in postgis- 3.0.2 library.
+    # args << "--without-topology"
+
+    # Disable the address_standardizer extension
+    # args << "--without-address-standardizer"
+
+    # specify the dtd path for mathml2.dtd
+    # args << "--with-mathmldtd=PATH"
+
+    args << "--with-xsldir=#{Formula["docbook-xsl"].opt_prefix}/docbook-xsl" if build.with? "html-docs" # /docbook-xsl-nons
+
+    if build.with?("pg10")
+      args << "--with-pgconfig=#{Formula["osgeo-postgresql@10"].opt_bin}/pg_config"
+    else
+      args << "--with-pgconfig=#{Formula["osgeo-postgresql"].opt_bin}/pg_config"
+    end
+
+    system "./autogen.sh"
+    system "./configure", *args
+    system "make"
+
+    if build.with? "html-docs"
+      cd "doc" do
+        ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+        system "make", "chunked-html"
+        doc.install "html"
+      end
+    end
+
+    if build.with? "api-docs"
+      cd "doc" do
+        system "make", "doxygen"
+        doc.install "doxygen/html" => "api"
+      end
+    end
+
+    # PostGIS includes the PGXS makefiles and so will install __everything__
+    # into the Postgres keg instead of the PostGIS keg. Unfortunately, some
+    # things have to be inside the Postgres keg in order to be function. So, we
+    # install everything to a staging directory and manually move the pieces
+    # into the appropriate prefixes.
+    mkdir "stage"
+    system "make", "install", "DESTDIR=#{buildpath}/stage"
+
+    # Install PostGIS plugin libraries into the Postgres keg so that they can
+    # be loaded and so PostGIS databases will continue to function even if
+    # PostGIS is removed.
+    # (postgres_realpath/"lib").install Dir["stage/**/*.so"]
+
+    # Install extension scripts to the Postgres keg.
+    # `CREATE EXTENSION postgis;` won't work if these are located elsewhere.
+    # (postgres_realpath/"share/postgresql/extension").install Dir["stage/**/extension/*"]
+
+    bin.install Dir["stage/**/bin/*"]
+    lib.install Dir["stage/**/lib/*"]
+    include.install Dir["stage/**/include/*"]
+    pkgshare.install Dir["stage/**/contrib/postgis-*/*"]
+    (share/"doc/postgresql/extension").install Dir["stage/**/share/doc/postgresql/extension/*"]
+    (share/"postgresql/extension").install Dir["stage/**/share/postgresql/extension/*"]
+    (share/"postgresql/contrib/postgis-3.0").install Dir["stage/**/contrib/postgis-*/*"]
+    (share/"postgis_topology").install Dir["stage/**/contrib/postgis_topology-*/*"]
+
+    # Extension scripts
+    bin.install %w[
+      utils/create_undef.pl
+      utils/postgis_proc_upgrade.pl
+      utils/postgis_restore.pl
+      utils/profile_intersects.pl
+      utils/test_estimation.pl
+      utils/test_geography_estimation.pl
+      utils/test_geography_joinestimation.pl
+      utils/test_joinestimation.pl
+    ]
+
+    man1.install Dir["doc/**/*.1"]
+  end
+
+  def caveats
+    <<~EOS
+      To create a spatially-enabled database, see the documentation:
+        https://postgis.net/docs/manual-3.0/postgis_installation.html#create_new_db_extensions
+      If you are currently using PostGIS 2.0+, you can go the soft upgrade path:
+        ALTER EXTENSION postgis UPDATE TO "#{version}";
+      Users of 1.5 and below will need to go the hard-upgrade path, see here:
+        https://postgis.net/docs/manual-3.0/postgis_installation.html#upgrading
+
+      PostGIS SQL scripts installed to:
+        #{HOMEBREW_PREFIX}/share/postgresql/contrib/postgis-3.0
+      PostGIS plugin libraries installed to:
+        #{HOMEBREW_PREFIX}/lib
+      PostGIS extension modules installed to:
+        #{HOMEBREW_PREFIX}/share/postgresql/extension
+    EOS
+  end
+
+  test do
+    require "base64"
+    (testpath/"brew.shp").write ::Base64.decode64 <<~EOS
+      AAAnCgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAoOgDAAALAAAAAAAAAAAAAAAA
+      AAAAAADwPwAAAAAAABBAAAAAAAAAFEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      AAAAAAAAAAAAAAAAAAEAAAASCwAAAAAAAAAAAPA/AAAAAAAA8D8AAAAAAAAA
+      AAAAAAAAAAAAAAAAAgAAABILAAAAAAAAAAAACEAAAAAAAADwPwAAAAAAAAAA
+      AAAAAAAAAAAAAAADAAAAEgsAAAAAAAAAAAAQQAAAAAAAAAhAAAAAAAAAAAAA
+      AAAAAAAAAAAAAAQAAAASCwAAAAAAAAAAAABAAAAAAAAAAEAAAAAAAAAAAAAA
+      AAAAAAAAAAAABQAAABILAAAAAAAAAAAAAAAAAAAAAAAUQAAAAAAAACJAAAAA
+      AAAAAEA=
+    EOS
+    (testpath/"brew.dbf").write ::Base64.decode64 <<~EOS
+      A3IJGgUAAABhAFsAAAAAAAAAAAAAAAAAAAAAAAAAAABGSVJTVF9GTEQAAEMA
+      AAAAMgAAAAAAAAAAAAAAAAAAAFNFQ09ORF9GTEQAQwAAAAAoAAAAAAAAAAAA
+      AAAAAAAADSBGaXJzdCAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgIFBvaW50ICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgU2Vjb25kICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgICBQb2ludCAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgIFRoaXJkICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgICAgUG9pbnQgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICBGb3VydGggICAgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgICAgIFBvaW50ICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgQXBwZW5kZWQgICAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAgICAgICBQb2ludCAgICAgICAgICAgICAgICAgICAgICAg
+      ICAgICAgICAgICAg
+    EOS
+    (testpath/"brew.shx").write ::Base64.decode64 <<~EOS
+      AAAnCgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARugDAAALAAAAAAAAAAAAAAAA
+      AAAAAADwPwAAAAAAABBAAAAAAAAAFEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      AAAAAAAAAAAAAAAAADIAAAASAAAASAAAABIAAABeAAAAEgAAAHQAAAASAAAA
+      igAAABI=
+    EOS
+    result = shell_output("#{bin}/shp2pgsql #{testpath}/brew.shp")
+    assert_match(/Point/, result)
+    assert_match(/AddGeometryColumn/, result)
+  end
+end
---
+++ Formula/osgeo-postgresql@11.rb
@@ -0,0 +1,280 @@
+class OsgeoPostgresql < Formula
+  desc "Object-relational database system"
+  homepage "https://www.postgresql.org/"
+  url "https://github.com/postgres/postgres/archive/REL_11_7.tar.gz"
+  sha256 "8c427e10a5f8b6be76353e83c7cf0171ac0e85308d352b8c129612002bb342eb"
+
+  #bottle do
+  #  root_url "https://bottle.download.osgeo.org"
+  #  sha256 "7d47e98b0e6ebe6d8a5968e8b1ff5828bdc7ab91644d41feb2847225f2d96684" => :catalina
+  #  sha256 "7d47e98b0e6ebe6d8a5968e8b1ff5828bdc7ab91644d41feb2847225f2d96684" => :mojave
+  #  sha256 "7d47e98b0e6ebe6d8a5968e8b1ff5828bdc7ab91644d41feb2847225f2d96684" => :high_sierra
+  #end
+
+  # revision 1
+
+  head "https://github.com/postgres/postgres.git", :branch => "master"
+
+  option "with-cellar", "Use /Cellar in the path configuration (necessary for migration)"
+
+  # keg_only "postgresql is already provided by homebrew/core"
+  # we will verify that other versions are not linked
+  depends_on Unlinked
+
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "icu4c"
+  depends_on "openldap" # libldap
+  depends_on "openssl"
+  depends_on "readline"
+  depends_on "tcl-tk"
+  depends_on "krb5"
+  depends_on "libxml2"
+  depends_on "python"
+  depends_on "perl"
+  depends_on "zlib"
+  # depends_on "e2fsprogs"
+
+  # others: pam
+
+  def install
+    # avoid adding the SDK library directory to the linker search path
+    # XML2_CONFIG=:
+    ENV["XML2_CONFIG"] = "xml2-config --exec-prefix=/usr"
+
+    # As of Xcode/CLT 10.x the Perl headers were moved from /System
+    # to inside the SDK, so we need to use `-iwithsysroot` instead
+    # of `-I` to point to the correct location.
+    # https://www.postgresql.org/message-id/153558865647.1483.573481613491501077%40wrigleys.postgresql.org
+    ENV.prepend "LDFLAGS", "-L#{Formula["openssl"].opt_lib} -L#{Formula["readline"].opt_lib} -R#{lib}/postgresql"
+    ENV.prepend "CPPFLAGS", "-I#{Formula["openssl"].opt_include} -I#{Formula["readline"].opt_include}"
+
+    # ENV["PYTHON"] = which("python3")
+    ENV["PYTHON"] = "#{Formula["python"].opt_bin}/python3"
+
+    args = %W[
+      --disable-debug
+      --enable-thread-safety
+      --with-bonjour
+      --with-gssapi
+      --with-icu
+      --with-ldap
+      --with-libxml
+      --with-libxslt
+      --with-openssl
+      --with-pam
+      --with-perl
+      --with-uuid=e2fs
+      --enable-dtrace
+      --enable-nls
+      --with-python
+    ]
+
+    if build.with? "cellar"
+      args += [
+        "--prefix=#{prefix}",
+        # "bindir=#{bin}", # if define this, will refer to /Cellar
+        "--datadir=#{share}/postgresql",
+        "--libdir=#{lib}/postgresql",
+        "--sysconfdir=#{etc}",
+        "--docdir=#{doc}/postgresql",
+        "--includedir=#{include}/postgresql"
+      ]
+    else
+      # this is to not have the reference to /Cellar in the files
+      args += [
+        "--prefix=#{prefix}",
+        # "--bindir=#{HOMEBREW_PREFIX}/bin",
+        "--sysconfdir=#{HOMEBREW_PREFIX}/etc",
+        "--libdir=#{HOMEBREW_PREFIX}/lib",
+        "--datadir=#{HOMEBREW_PREFIX}/share/postgresql",
+        "--docdir=#{HOMEBREW_PREFIX}/share/doc/postgresql",
+        "--localstatedir=#{HOMEBREW_PREFIX}/var",
+        "--includedir=#{HOMEBREW_PREFIX}/include",
+        "--datarootdir=#{HOMEBREW_PREFIX}/share",
+        "--localedir=#{HOMEBREW_PREFIX}/share/locale",
+        "--mandir=#{HOMEBREW_PREFIX}/share/man",
+      ]
+
+      # args << "--with-system-tzdata=#{HOMEBREW_PREFIX}/share/zoneinfo" # use system time zone data in DIR
+    end
+
+    dirs = [
+      # "bindir=#{bin}",
+      "datadir=#{share}/postgresql", # #{pkgshare}
+      "libdir=#{lib}",
+      "pkglibdir=#{lib}/postgresql", # #{lib}
+      "pkgincludedir=#{include}/postgresql",
+      "sysconfdir=#{etc}",
+      "includedir=#{include}",
+      "localedir=#{share}/locale",
+      "mandir=#{man}",
+      "docdir=#{share}/doc/postgresql",
+    ]
+
+    # The CLT is required to build Tcl support on 10.7 and 10.8 because
+    # tclConfig.sh is not part of the SDK
+    args << "--with-tcl"
+    if File.exist?("#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework/tclConfig.sh")
+      args << "--with-tclconfig=#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework"
+    end
+
+    # Add include and library directories of dependencies, so that
+    # they can be used for compiling extensions.  Superenv does this
+    # when compiling this package, but won't record it for pg_config.
+    deps = %w[gettext icu4c openldap openssl readline tcl-tk]
+    with_includes = deps.map { |f| Formula[f].opt_include }.join(":")
+    with_libraries = deps.map { |f| Formula[f].opt_lib }.join(":")
+    args << "--with-includes=#{with_includes}"
+    args << "--with-libraries=#{with_libraries}"
+
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    system "./configure", *args
+    system "make"
+
+    # Temporarily disable building/installing the documentation.
+    # Postgresql seems to "know" the build system has been altered and
+    # tries to regenerate the documentation when using `install-world`.
+    # This results in the build failing:
+    #  `ERROR: `osx' is missing on your system.`
+    # Attempting to fix that by adding a dependency on `open-sp` doesn't
+    # work and the build errors out on generating the documentation, so
+    # for now let's simply omit it so we can package Postgresql for Mojave.
+    # if DevelopmentTools.clang_build_version >= 1000
+    system "make", "all"
+    system "make", "-C", "contrib", "install", "all", *dirs
+    system "make", "install", "all", *dirs
+    # else
+    #   system "make", "install-world", *dirs
+    # end
+  end
+
+  def post_install
+    # (var/"log").mkpath
+    # (var/"postgresql").mkpath
+    # unless File.exist? "#{var}/postgresql/PG_VERSION"
+    #   system "#{bin}/initdb", "#{var}/postgresql"
+    # end
+
+    if build.with? "cellar"
+      if File.exists?(File.join("#{HOMEBREW_PREFIX}/Cellar", "osgeo-postgis@2.5"))
+        unless File.exists?("#{HOMEBREW_PREFIX}/opt/osgeo-postgresql/lib/postgresql/postgis-2.5.so")
+          # copy postgis 2.5.x to postgresql 11.x
+          FileUtils.cp_r "#{Formula["osgeo-postgis@2.5"].opt_share}/postgresql/.", "#{share}/postgresql/"
+          FileUtils.cp_r "#{Formula["osgeo-postgis@2.5"].opt_lib}/postgresql/.", "#{lib}/postgresql/"
+          # FileUtils.cp_r "#{Formula["osgeo-postgis@2.5"].opt_lib}/postgresql/rtpostgis-2.5.so", "#{lib}/postgresql/"
+          # FileUtils.cp_r "#{Formula["osgeo-postgis@2.5"].opt_lib}/postgresql/postgis-2.5.so", "#{lib}/postgresql/"
+          # FileUtils.cp_r "#{Formula["osgeo-postgis@2.5"].opt_lib}/postgresql/postgis_topology-2.5.so", "#{lib}/postgresql/"
+        end
+      end
+
+      # if File.exists?(File.join("#{HOMEBREW_PREFIX}/Cellar", "osgeo-postgis"))
+      #   unless File.exists?("#{HOMEBREW_PREFIX}/opt/osgeo-postgresql/lib/postgresql/postgis-2.5.so")
+      #     # install postgis 2.5.x to postgresql 11.x
+      #     FileUtils.cp_r "#{Formula["osgeo-postgis"].opt_share}/postgresql/.", "#{share}/postgresql/"
+      #     # FileUtils.cp_r "#{Formula["osgeo-postgis"].opt_lib}/postgresql/.", "#{lib}/postgresql/"
+      #     FileUtils.cp_r "#{Formula["osgeo-postgis"].opt_lib}/postgresql/rtpostgis-2.5.so", "#{lib}/postgresql/"
+      #     FileUtils.cp_r "#{Formula["osgeo-postgis"].opt_lib}/postgresql/postgis-2.5.so", "#{lib}/postgresql/"
+      #     FileUtils.cp_r "#{Formula["osgeo-postgis"].opt_lib}/postgresql/postgis_topology-2.5.so", "#{lib}/postgresql/"
+      #   end
+      # end
+    end
+  end
+
+  def caveats; <<~EOS
+
+    1 - If you need to link "#{name}":
+
+          \e[32m$ brew link #{name} --force\e[0m
+
+        Previously unlink any other version that you have installed.
+
+    2 - If you need to init postgresql just execute the following command:
+
+          \e[32m$ initdb #{HOMEBREW_PREFIX}/var/postgresql -E utf8 --locale=en_US.UTF-8\e[0m
+
+        If the file "#{HOMEBREW_PREFIX}/var/postgresql/PG_VERSION" exists,
+        it is because you already created this in postinstall or a previous installation.
+
+    3 - Start using:
+
+          \e[32m$ pg_ctl start -D /usr/local/var/postgresql\e[0m
+
+    4 - Connecting to our new database
+
+          \e[32m$ psql -h localhost -d postgres\e[0m
+
+    Note:
+
+      - Services doesn't start properly, add to \e[32mhomebrew.mxcl.osgeo-postgresql.plist\e[0m:
+
+          \e[32m<key>EnvironmentVariables</key>\e[0m
+          \e[32m<dict>\e[0m
+            \e[32m<key>LC_ALL</key>\e[0m
+            \e[32m<string>en_US.UTF-8</string>\e[0m
+          \e[32m</dict>\e[0m
+
+          issue: \e[32mhttps://github.com/OSGeo/homebrew-osgeo4mac/issues/1075#issuecomment-490052517\e[0m
+
+      - Could not bind ipv6 address database system was not properly shut:
+
+          \e[32m$ sudo lsof -i :5432\e[0m (search PID)
+
+          \e[32m$ kill PID\e[0m
+
+      - To migrate existing data from a previous major version of PostgreSQL run:
+
+          \e[32m$ brew postgresql-upgrade-database\e[0m
+
+      - For more information see our page with documentation:
+
+          \e[32mhttps://osgeo.github.io/homebrew-osgeo4mac\e[0m
+    EOS
+  end
+
+  plist_options :manual => "pg_ctl -D #{HOMEBREW_PREFIX}/var/postgresql start"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/postgres</string>
+        <string>-D</string>
+        <string>#{var}/postgresql</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/postgresql.log</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/postgresql.log</string>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    ENV["LC_ALL"]="en_US.UTF-8"
+    ENV["LC_CTYPE"]="en_US.UTF-8"
+    system "#{bin}/initdb", testpath/"test"
+    if build.with? "cellar"
+      assert_equal (share/"postgresql").to_s, shell_output("#{bin}/pg_config --sharedir").chomp
+      assert_equal (lib/"postgresql").to_s, shell_output("#{bin}/pg_config --libdir").chomp
+      assert_equal (lib/"postgresql").to_s, shell_output("#{bin}/pg_config --pkglibdir").chomp
+    else
+      assert_equal "#{HOMEBREW_PREFIX}/share/postgresql", shell_output("#{bin}/pg_config --sharedir").chomp
+      assert_equal "#{HOMEBREW_PREFIX}/lib", shell_output("#{bin}/pg_config --libdir").chomp
+      assert_equal "#{HOMEBREW_PREFIX}/lib/postgresql", shell_output("#{bin}/pg_config --pkglibdir").chomp
+    end
+  end
+end
