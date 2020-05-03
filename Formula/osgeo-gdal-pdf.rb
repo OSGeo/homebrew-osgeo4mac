9f478dd60ae734ee862e05435f50262cef41f9d6
Alberto Lázaro-López <alazarolop@gmail.com>
2020-05-03 10:28:57 +0200

    Revision of gdal-pdf

--- Formula/osgeo-gdal-pdf.rb
+++ Formula/osgeo-gdal-pdf.rb
@@ -4,16 +4,16 @@ class OsgeoGdalPdf < Formula
   url "https://github.com/OSGeo/gdal/releases/download/v3.0.4/gdal-3.0.4.tar.gz"
   sha256 "fc15d2b9107b250305a1e0bd8421dd9ec1ba7ac73421e4509267052995af5e83"
 
-  revision 2
+  revision 3
   
   head "https://github.com/OSGeo/gdal.git", :branch => "master"
 
-  #bottle do
-  #  root_url "https://bottle.download.osgeo.org"
-  #  sha256 "713a9db5d91e16b168858af96eccba1f91369e1046d59fd9a6f7af3891817823" => :catalina
-  #  sha256 "713a9db5d91e16b168858af96eccba1f91369e1046d59fd9a6f7af3891817823" => :mojave
-  #  sha256 "713a9db5d91e16b168858af96eccba1f91369e1046d59fd9a6f7af3891817823" => :high_sierra
-  #end
+  bottle do
+    root_url "https://bottle.download.osgeo.org"
+    sha256 "713a9db5d91e16b168858af96eccba1f91369e1046d59fd9a6f7af3891817823" => :catalina
+    sha256 "713a9db5d91e16b168858af96eccba1f91369e1046d59fd9a6f7af3891817823" => :mojave
+    sha256 "713a9db5d91e16b168858af96eccba1f91369e1046d59fd9a6f7af3891817823" => :high_sierra
+  end
 
   depends_on "pkg-config" => :build
   depends_on "cairo"
