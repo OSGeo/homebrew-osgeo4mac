d938423f08d0d7df1d67bc3705f28608e9196621
Alberto Lázaro-López <alazarolop@gmail.com>
2020-05-03 11:32:03 +0200

    Revision of postgis@2.4

--- Formula/osgeo-postgis@2.4.rb
+++ Formula/osgeo-postgis@2.4.rb
@@ -43,7 +43,7 @@ class OsgeoPostgisAT24 < Formula
     sha256 "c7beee0ddd6337a6f521b44a9b5cfbf57f824c20201b75d72dc115e379bcd509" => :sierra
   end
 
-  revision 1
+  revision 2
 
   head "https://github.com/postgis/postgis.git", :branch => "svn-2.4"
 
