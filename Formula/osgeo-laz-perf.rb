5be532d48723289ebd107f791639a318a51026d0
Alberto Lázaro-López <alazarolop@posteo.me>
2020-06-03 09:16:14 +0200

    Upgrade laz-perf

--- Formula/osgeo-laz-perf.rb
+++ Formula/osgeo-laz-perf.rb
@@ -1,8 +1,12 @@
 class OsgeoLazPerf < Formula
   desc "Alternative LAZ implementation for C++ and JavaScript"
   homepage "https://github.com/hobu/laz-perf"
-  url "https://github.com/hobu/laz-perf/archive/1.3.0.tar.gz"
-  sha256 "9d4273206557e091a4faf7faa4867ecd7da55a116015fcbfb58d30e88570958e"
+  url "https://github.com/hobu/laz-perf/archive/1.4.4.zip"
+  sha256 "9801e671ac7122bfa67436d8ed3b202323c4f05f467882fe54ae1f20c4f0df88"
+  #url "https://github.com/hobu/laz-perf.git",
+  #  :branch => "master",
+  #  :commit => "834629e362d8ff90669dcec60bef5cf555d197e2"
+  #version "1.4.4"
 
   bottle do
     root_url "https://bottle.download.osgeo.org"
@@ -12,7 +16,7 @@ class OsgeoLazPerf < Formula
     sha256 "c7cdfafb3568e4381c0322f56af26cfe9660c796d15dc78f7e46931a18305aae" => :high_sierra
   end
 
-  revision 1
+  #revision 1
 
   head "https://github.com/hobu/laz-perf.git", :branch => "master"
 
